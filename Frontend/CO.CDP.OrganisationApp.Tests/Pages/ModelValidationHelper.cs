f1f7955bdddf67b175713bdbc1f5f488cd2a9c9c
Dharm <dharmendraverma82@gmail.com>
2024-03-19 15:51:46 +0000

    Model validation of details page

---
+++ Frontend/CO.CDP.OrganisationApp.Tests/Pages/ModelValidationHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CO.CDP.OrganisationApp.Tests.Pages;
+
+public class ModelValidationHelper
+{
+	public static IList<ValidationResult> Validate(object model)
+	{
+		var results = new List<ValidationResult>();
+		var validationContext = new ValidationContext(model, null, null);
+		Validator.TryValidateObject(model, validationContext, results, true);
+		if (model is IValidatableObject result)
+		{
+			result.Validate(validationContext);
+		}
+		return results;
+	}
+}
--- Frontend/CO.CDP.OrganisationApp/Pages/Registration/YourDetails.cshtml.cs
+++ Frontend/CO.CDP.OrganisationApp/Pages/Registration/YourDetails.cshtml.cs
@@ -1,19 +1,39 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace CO.CDP.OrganisationApp.Pages.SupplierRegistration
+namespace CO.CDP.OrganisationApp.Pages.Registration;
+
+public class YourDetailsModel : PageModel
 {
+	[BindProperty]
+	[DisplayName("First name")]
+	[Required(ErrorMessage = "Enter your first name")]
+	public string? FirstName { get; set; }
 
-    public class YourDetailsModel : PageModel
-    {
-        private readonly ILogger<YourDetailsModel> _logger;
+	[BindProperty]
+	[DisplayName("Last name")]
+	[Required(ErrorMessage = "Enter your last name")]
+	public string? LastName { get; set; }
 
-        public YourDetailsModel(ILogger<YourDetailsModel> logger)
-        {
-            _logger = logger;
-        }
+	[BindProperty]
+	[DisplayName("Email address")]
+	[Required(ErrorMessage = "Enter your email address")]
+	[EmailAddress(ErrorMessage = "Enter an email address in the correct format, like name@example.com")]
+	public string? Email { get; set; }
 
-        public void OnGet()
-        {
-        }
-    }
+	public void OnGet()
+	{
+	}
+
+	public IActionResult OnPost()
+	{
+		if (!ModelState.IsValid)
+		{
+			return Page();
+		}
+
+		return Page();
+	}
 }
