39d211dd58d95101f04fe2978a6b72c3cc3c98b2
Jawwad Baig <jawwad.baig@goaco.com>
2024-03-19 13:42:09 +0000

    #dp-11 organisation identification page validation with tests

--- Frontend/CO.CDP.OrganisationApp/Pages/Registration/OrganisationIdentification.cshtml.cs
+++ Frontend/CO.CDP.OrganisationApp/Pages/Registration/OrganisationIdentification.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CO.CDP.OrganisationApp.Pages.Registration
@@ -5,7 +6,7 @@ namespace CO.CDP.OrganisationApp.Pages.Registration
     public class OrganisationIdentificationModel : PageModel
     {
         private readonly ILogger<OrganisationIdentificationModel> _logger;
-
+        public bool HasError { get; set; }
         public OrganisationIdentificationModel(ILogger<OrganisationIdentificationModel> logger)
         {
             _logger = logger;
@@ -13,6 +14,30 @@ namespace CO.CDP.OrganisationApp.Pages.Registration
 
         public void OnGet()
         {
+            HasError = false;
         }
+
+        public IActionResult OnPost(string organisationIdentification, IDictionary<string, string> organisationNumbers)
+        {
+
+            if (organisationIdentification == "otherNone" || ValidateOrganisationNumber(organisationIdentification, organisationNumbers))
+            {
+                return RedirectToPage("./OrganisationRegisteredAddress");
+            }
+
+            HasError = true;
+            return Page();
+        }
+
+        private bool ValidateOrganisationNumber(string organisationIdentification, IDictionary<string, string> organisationNumbers)
+        {
+            if (organisationNumbers.TryGetValue(organisationIdentification, out var number))
+            {
+                return !string.IsNullOrEmpty(number);
+            }
+            HasError = true;
+            return false;
+        }
+
     }
 }
