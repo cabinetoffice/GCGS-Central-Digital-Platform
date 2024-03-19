383f52f8ff82d3740bb431220cda8aaf942ed892
Jawwad Baig <jawwad.baig@goaco.com>
2024-03-19 14:47:36 +0000

    #dp-11 adding organisation identification number into session and unit tests

--- Frontend/CO.CDP.OrganisationApp.Tests/Registration/OrganisationIdentificationTests.cs
+++ Frontend/CO.CDP.OrganisationApp.Tests/Registration/OrganisationIdentificationTests.cs
@@ -1,5 +1,7 @@
 ﻿using CO.CDP.OrganisationApp.Pages.Registration;
+using CO.CDP.OrganisationApp.Tests.Common;
 using FluentAssertions;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -9,13 +11,40 @@ namespace CO.CDP.OrganisationApp.Tests.Registration
 {
     public class OrganisationIdentificationModelTests
     {
-        private readonly Mock<ILogger<OrganisationIdentificationModel>> _loggerMock;
         private readonly OrganisationIdentificationModel _pageModel;
+        private readonly Session _session;
+        private readonly DefaultHttpContext _httpContext;
+        private readonly Mock<ILogger<OrganisationIdentificationModel>> _loggerMock;
 
         public OrganisationIdentificationModelTests()
         {
             _loggerMock = new Mock<ILogger<OrganisationIdentificationModel>>();
-            _pageModel = new OrganisationIdentificationModel(_loggerMock.Object);
+
+            _httpContext = new DefaultHttpContext
+            {
+                Session = new MockHttpSession()
+            };
+            var httpContextAccessor = new HttpContextAccessor { HttpContext = _httpContext };
+
+            _session = new Session(httpContextAccessor);
+
+            _pageModel = new OrganisationIdentificationModel(_loggerMock.Object, _session);
+        }
+
+        [Fact]
+        public void ValidIdentificationNumber_ShouldStoreInSession()
+        {
+            // Arrange
+            var validOrganisationId = "companiesHouseNumber";
+            var validNumber = "12345678";
+            var organisationNumbers = new Dictionary<string, string> { { validOrganisationId, validNumber } };
+
+            // Act
+            _pageModel.OnPost(validOrganisationId, organisationNumbers);
+
+            // Assert            
+            string storedNumber = _session.Get<string>(validOrganisationId);
+            storedNumber.Should().Be(validNumber);
         }
 
         [Theory]
