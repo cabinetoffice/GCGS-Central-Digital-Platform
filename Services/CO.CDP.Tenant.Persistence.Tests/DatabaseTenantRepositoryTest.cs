f3716d9e5bc126d5f98fe77f44c390858500cbfc
Jakub Zalas <jakub@zalas.pl>
2024-03-21 21:01:06 +0000

    Remove the InMemoryTenantRepository implementation
    We ended up not using it for now.

--- Services/CO.CDP.Tenant.Persistence.Tests/DatabaseTenantRepositoryTest.cs
+++ Services/CO.CDP.Tenant.Persistence.Tests/DatabaseTenantRepositoryTest.cs
@@ -1,12 +1,84 @@
 using CO.CDP.Testcontainers.PostgreSql;
+using FluentAssertions;
 using Microsoft.EntityFrameworkCore;
 
 namespace CO.CDP.Tenant.Persistence.Tests;
 
-public class DatabaseTenantRepositoryTest(PostgreSqlFixture postgreSql) :
-    TenantRepositoryContractTest, IClassFixture<PostgreSqlFixture>
+public class DatabaseTenantRepositoryTest(PostgreSqlFixture postgreSql) : IClassFixture<PostgreSqlFixture>
 {
-    protected override ITenantRepository TenantRepository()
+    [Fact]
+    public async Task ItFindsSavedTenant()
+    {
+        using var repository = TenantRepository();
+
+        var tenant = GivenTenant(guid: Guid.NewGuid());
+
+        repository.Save(tenant);
+
+        var found = await repository.Find(tenant.Guid);
+
+        found.Should().Be(tenant);
+        found.As<Tenant>().Id.Should().BePositive();
+    }
+
+    [Fact]
+    public async Task ItReturnsNullIfTenantIsNotFound()
+    {
+        using var repository = TenantRepository();
+
+        var found = await repository.Find(Guid.NewGuid());
+
+        found.Should().BeNull();
+    }
+
+    [Fact]
+    public void ItRejectsTwoTenantsWithTheSameName()
+    {
+        using var repository = TenantRepository();
+
+        var tenant1 = GivenTenant(guid: Guid.NewGuid(), name: "Bob");
+        var tenant2 = GivenTenant(guid: Guid.NewGuid(), name: "Bob");
+
+        repository.Save(tenant1);
+
+        repository.Invoking(r => r.Save(tenant2))
+            .Should().Throw<ITenantRepository.TenantRepositoryException.DuplicateTenantException>()
+            .WithMessage($"Tenant with name `Bob` already exists.");
+    }
+
+    [Fact]
+    public void ItRejectsTwoTenantsWithTheSameGuid()
+    {
+        using var repository = TenantRepository();
+
+        var guid = Guid.NewGuid();
+        var tenant1 = GivenTenant(guid: guid, name: "Alice");
+        var tenant2 = GivenTenant(guid: guid, name: "Sussan");
+
+        repository.Save(tenant1);
+
+        repository.Invoking((r) => r.Save(tenant2))
+            .Should().Throw<ITenantRepository.TenantRepositoryException.DuplicateTenantException>()
+            .WithMessage($"Tenant with guid `{guid}` already exists.");
+    }
+
+    [Fact]
+    public async Task ItUpdatesAnExistingTenant()
+    {
+        using var repository = TenantRepository();
+
+        var tenant = GivenTenant(guid: Guid.NewGuid(), name: "Olivia");
+
+        repository.Save(tenant);
+        tenant.Name = "Hannah";
+        repository.Save(tenant);
+
+        var found = await repository.Find(tenant.Guid);
+
+        found.Should().Be(tenant);
+    }
+
+    private ITenantRepository TenantRepository()
     {
         return new DatabaseTenantRepository(TenantContext());
     }
@@ -17,5 +89,25 @@ public class DatabaseTenantRepositoryTest(PostgreSqlFixture postgreSql) :
         context.Database.Migrate();
         context.SaveChanges();
         return context;
+    }
+
+    private static Tenant GivenTenant(
+        Guid? guid = null,
+        string? name = null,
+        string email = "stefan@example.com",
+        string phone = "07925123123")
+    {
+        var theGuid = guid ?? Guid.NewGuid();
+        var theName = name ?? $"Stefan {theGuid}";
+        return new Tenant
+        {
+            Guid = theGuid,
+            Name = theName,
+            ContactInfo = new Tenant.TenantContactInfo
+            {
+                Email = email,
+                Phone = phone
+            }
+        };
     }
 }
--- Services/CO.CDP.Tenant.Persistence.Tests/InMemoryTenantRepositoryTest.cs
+++
@@ -1,9 +0,0 @@
-namespace CO.CDP.Tenant.Persistence.Tests;
-
-public class InMemoryTenantRepositoryTest : TenantRepositoryContractTest
-{
-    protected override ITenantRepository TenantRepository()
-    {
-        return new InMemoryTenantRepository();
-    }
-}
