3160f277fba9a6f4c6fe65c113db3b1877e0541f
Jawwad Baig <jawwad.baig@goaco.com>
2024-03-21 13:54:13 +0000

    #DP-11 refactored all code according to PR reviews

--- Frontend/CO.CDP.OrganisationApp/Pages/Registration/OrganisationDetails.cshtml.cs
+++ Frontend/CO.CDP.OrganisationApp/Pages/Registration/OrganisationDetails.cshtml.cs
@@ -1,6 +1,7 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.ComponentModel.DataAnnotations;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace CO.CDP.OrganisationApp.Pages.Registration;
 
@@ -9,7 +10,7 @@ public class OrganisationDetailModel : PageModel
 {
     [BindProperty]
     [DisplayName("Organisation name")]
-    [Required(ErrorMessage = "Enter your organisation name")]    
+    [Required(ErrorMessage = "Enter your organisation name")]
     public string? OrganisationName { get; set; }
 
     [BindProperty]
