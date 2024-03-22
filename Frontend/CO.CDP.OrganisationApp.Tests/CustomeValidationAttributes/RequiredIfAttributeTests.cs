4f80ff094f023bb4f29891e203b3ba8090a46e00
Jawwad Baig <jawwad.baig@goaco.com>
2024-03-22 12:54:47 +0000

    #dp-11 updated the file-scope

--- Frontend/CO.CDP.OrganisationApp.Tests/CustomeValidationAttributes/RequiredIfAttributeTests.cs
+++ Frontend/CO.CDP.OrganisationApp.Tests/CustomeValidationAttributes/RequiredIfAttributeTests.cs
@@ -4,51 +4,51 @@ using System.ComponentModel.DataAnnotations;
 
 namespace CO.CDP.OrganisationApp.Tests.CustomeValidationAttributes;
 
-    public class RequiredIfAttributeTests
+public class RequiredIfAttributeTests
+{
+    public class RequiredIfAttributeValidationTests
     {
-        public class RequiredIfAttributeValidationTests
-        {
-            [Fact]
-            public void Should_Fail_Validation_When_OrganisationType_Matches_And_IdentificationNumber_IsNull()
-            {
-                var model = new OrganisationTestModel { OrganisationType = "CCNI" };
-                var validationContext = new ValidationContext(model, null, null)
-                {
-                    MemberName = nameof(OrganisationTestModel.CharityCommissionNorthernIrelandNumber)
-                };
-                var attribute = new RequiredIfAttribute("OrganisationType", "CCNI");
-
-                var result = attribute.GetValidationResult(null, validationContext);
-
-                result.Should().NotBeNull()
-                      .And.BeOfType<ValidationResult>()
-                      .Which.ErrorMessage.Should().Be("CharityCommissionNorthernIrelandNumber is required");
-            }
-        }
-
         [Fact]
-        public void Should_Pass_All_Validation_When_OrganisationType_Matches_And_IdentificationNumber_IsProvided()
+        public void Should_Fail_Validation_When_OrganisationType_Matches_And_IdentificationNumber_IsNull()
         {
-            var model = new OrganisationTestModel
-            {
-                OrganisationType = "CCNI",
-                CharityCommissionNorthernIrelandNumber = "12345"
-            };
-            var context = new ValidationContext(model, null, null)
+            var model = new OrganisationTestModel { OrganisationType = "CCNI" };
+            var validationContext = new ValidationContext(model, null, null)
             {
                 MemberName = nameof(OrganisationTestModel.CharityCommissionNorthernIrelandNumber)
             };
             var attribute = new RequiredIfAttribute("OrganisationType", "CCNI");
 
-            var result = attribute.GetValidationResult(model.CharityCommissionNorthernIrelandNumber, context);
+            var result = attribute.GetValidationResult(null, validationContext);
 
-            result.Should().BeNull("because the identification number is provided when the organisation type matches");
-        }
-
-        private class OrganisationTestModel
-        {
-            public string? OrganisationType { get; set; }
-            public string? CharityCommissionNorthernIrelandNumber { get; set; }
+            result.Should().NotBeNull()
+                  .And.BeOfType<ValidationResult>()
+                  .Which.ErrorMessage.Should().Be("CharityCommissionNorthernIrelandNumber is required");
         }
     }
+
+    [Fact]
+    public void Should_Pass_All_Validation_When_OrganisationType_Matches_And_IdentificationNumber_IsProvided()
+    {
+        var model = new OrganisationTestModel
+        {
+            OrganisationType = "CCNI",
+            CharityCommissionNorthernIrelandNumber = "12345"
+        };
+        var context = new ValidationContext(model, null, null)
+        {
+            MemberName = nameof(OrganisationTestModel.CharityCommissionNorthernIrelandNumber)
+        };
+        var attribute = new RequiredIfAttribute("OrganisationType", "CCNI");
+
+        var result = attribute.GetValidationResult(model.CharityCommissionNorthernIrelandNumber, context);
+
+        result.Should().BeNull("because the identification number is provided when the organisation type matches");
+    }
+
+    private class OrganisationTestModel
+    {
+        public string? OrganisationType { get; set; }
+        public string? CharityCommissionNorthernIrelandNumber { get; set; }
+    }
 }
+
