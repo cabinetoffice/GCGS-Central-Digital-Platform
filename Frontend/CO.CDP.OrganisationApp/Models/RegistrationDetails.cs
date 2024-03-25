d9c5d10880e06e28ef3e770f8e22792c769bc1e7
dpatel017 <dpatel017@outlook.com>
2024-03-25 14:54:59 +0000

    Remember organisation details in the session (#29)
    * Remember organisation details in the session
    
    * Added OnGet and test for OnGet
    
    * changes to remove GitGub warning for code
    
    * merged from main

--- Frontend/CO.CDP.OrganisationApp/Models/RegistrationDetails.cs
+++ Frontend/CO.CDP.OrganisationApp/Models/RegistrationDetails.cs
@@ -8,7 +8,13 @@ namespace CO.CDP.OrganisationApp.Models
 
         public string? Email { get; set; }
 
+        public string? OrganisationName { get; set; }
+
         public string? OrganisationType { get; set; }
+
+        public string? OrganisationEmailAddress { get; set; }
+
+        public string? OrganisationTelephoneNumber { get; set; }
 
         public string? OrganisationIdentificationNumber { get; set; }
     }
