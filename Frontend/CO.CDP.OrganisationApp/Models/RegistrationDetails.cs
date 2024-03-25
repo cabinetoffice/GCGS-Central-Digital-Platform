7b6e97c2476f91a2d53fac74ad7614fe067cafe7
dpatel017 <dpatel017@outlook.com>
2024-03-25 19:11:45 +0000

    file scop for namesapce

--- Frontend/CO.CDP.OrganisationApp/Models/RegistrationDetails.cs
+++ Frontend/CO.CDP.OrganisationApp/Models/RegistrationDetails.cs
@@ -1,31 +1,30 @@
-namespace CO.CDP.OrganisationApp.Models
+namespace CO.CDP.OrganisationApp.Models;
+
+public class RegistrationDetails
 {
-    public class RegistrationDetails
-    {
-        public string? FirstName { get; set; }
+    public string? FirstName { get; set; }
 
-        public string? LastName { get; set; }
+    public string? LastName { get; set; }
 
-        public string? Email { get; set; }
+    public string? Email { get; set; }
 
-        public string? OrganisationName { get; set; }
+    public string? OrganisationName { get; set; }
 
-        public string? OrganisationType { get; set; }
+    public string? OrganisationType { get; set; }
 
-        public string? OrganisationEmailAddress { get; set; }
+    public string? OrganisationEmailAddress { get; set; }
 
-        public string? OrganisationTelephoneNumber { get; set; }
+    public string? OrganisationTelephoneNumber { get; set; }
 
-        public string? OrganisationIdentificationNumber { get; set; }
+    public string? OrganisationIdentificationNumber { get; set; }
 
-        public string? OrganisationAddressLine1 { get; set; }
+    public string? OrganisationAddressLine1 { get; set; }
 
-        public string? OrganisationAddressLine2 { get; set; }
+    public string? OrganisationAddressLine2 { get; set; }
 
-        public string? OrganisationCityOrTown { get; set; }
+    public string? OrganisationCityOrTown { get; set; }
 
-        public string? OrganisationPostcode { get; set; }
+    public string? OrganisationPostcode { get; set; }
 
-        public string? OrganisationCountry { get; set; }
-    }
+    public string? OrganisationCountry { get; set; }
 }
--- Frontend/CO.CDP.OrganisationApp/Pages/Registration/OrganisationDetailsSummary.cshtml.cs
+++ Frontend/CO.CDP.OrganisationApp/Pages/Registration/OrganisationDetailsSummary.cshtml.cs
@@ -1,19 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace CO.CDP.OrganisationApp.Pages.SupplierRegistration
+namespace CO.CDP.OrganisationApp.Pages.SupplierRegistration;
+
+public class OrganisationDetailsSummaryModel : PageModel
 {
-    public class OrganisationDetailsSummaryModel : PageModel
+    private readonly ILogger<OrganisationDetailsSummaryModel> _logger;
+
+    public OrganisationDetailsSummaryModel(ILogger<OrganisationDetailsSummaryModel> logger)
     {
-        private readonly ILogger<OrganisationDetailsSummaryModel> _logger;
+        _logger = logger;
+    }
 
-        public OrganisationDetailsSummaryModel(ILogger<OrganisationDetailsSummaryModel> logger)
-        {
-            _logger = logger;
-        }
-
-        public void OnGet()
-        {
-        }
+    public void OnGet()
+    {
     }
 }
