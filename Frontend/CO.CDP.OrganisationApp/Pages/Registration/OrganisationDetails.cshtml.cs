ef98efaf79566748593dbe65333f1c9ebc4365ed
dpatel017 <dpatel017@outlook.com>
2024-03-25 14:53:23 +0000

    merged from main

--- Frontend/CO.CDP.OrganisationApp/Pages/Registration/OrganisationDetails.cshtml.cs
+++ Frontend/CO.CDP.OrganisationApp/Pages/Registration/OrganisationDetails.cshtml.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel;
 using CO.CDP.OrganisationApp.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace CO.CDP.OrganisationApp.Pages.Registration;
 
