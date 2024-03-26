4eb7505e2fdc4adae269bf74657c7606c1611f84
dpatel017 <dpatel017@outlook.com>
2024-03-26 09:53:36 +0000

    DP-49: Supplier provides organisation's registered address (#42)
    * Organisation address and storing in session
    
    * file scop for namesapce

--- Frontend/CO.CDP.OrganisationApp/Pages/Registration/OrganisationRegisteredAddress.cshtml.cs
+++ Frontend/CO.CDP.OrganisationApp/Pages/Registration/OrganisationRegisteredAddress.cshtml.cs
@@ -1,19 +1,78 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using CO.CDP.OrganisationApp.Models;
+using System.Net.Mail;
 
-namespace CO.CDP.OrganisationApp.Pages.SupplierRegistration
+namespace CO.CDP.OrganisationApp.Pages.Registration;
+
+public class OrganisationRegisteredAddressModel(ISession session) : PageModel
 {
-    public class OrganisationRegisteredAddressModel : PageModel
+    [BindProperty]
+    [DisplayName("Address line 1")]
+    [Required(ErrorMessage = "Enter your address line 1")]
+    public string? AddressLine1 { get; set; }
+
+    [BindProperty]
+    [DisplayName("Address line 2")]
+    [Required(ErrorMessage = "Enter your address line 2")]
+    public string? AddressLine2 { get; set; }
+
+    [BindProperty]
+    [DisplayName("City or town")]
+    [Required(ErrorMessage = "Enter your city or town")]
+    public string? CityOrTown { get; set; }
+
+    [BindProperty]
+    [DisplayName("Postcode or ZIP Code")]
+    [Required(ErrorMessage = "Enter your postcode or zip Code")]
+    public string? Postcode { get; set; }
+
+    [BindProperty]
+    [DisplayName("Country")]
+    [Required(ErrorMessage = "Enter your country")]
+    public string? Country { get; set; }
+
+    public void OnGet()
     {
-        private readonly ILogger<OrganisationRegisteredAddressModel> _logger;
+        var registrationDetails = VerifySession();
 
-        public OrganisationRegisteredAddressModel(ILogger<OrganisationRegisteredAddressModel> logger)
+        AddressLine1 = registrationDetails.OrganisationAddressLine1;
+        AddressLine2 = registrationDetails.OrganisationAddressLine2;
+        CityOrTown = registrationDetails.OrganisationCityOrTown;
+        Postcode = registrationDetails.OrganisationPostcode;
+        Country = registrationDetails.OrganisationCountry;
+    }
+
+    public IActionResult OnPost() {
+        if (!ModelState.IsValid)
         {
-            _logger = logger;
+            return Page();
         }
 
-        public void OnGet()
+        var registrationDetails = VerifySession();
+
+        registrationDetails.OrganisationAddressLine1 = AddressLine1;
+        registrationDetails.OrganisationAddressLine2 = AddressLine2;
+        registrationDetails.OrganisationCityOrTown = CityOrTown;
+        registrationDetails.OrganisationPostcode = Postcode;
+        registrationDetails.OrganisationCountry = Country;
+
+        session.Set(Session.RegistrationDetailsKey, registrationDetails);
+
+        return RedirectToPage("OrganisationDetailsSummary");
+
+    }
+
+    private RegistrationDetails VerifySession()
+    {
+        var registrationDetails = session.Get<RegistrationDetails>(Session.RegistrationDetailsKey);
+        if (registrationDetails == null)
         {
+            //show error page (Once we finalise)
+            throw new Exception("Shoudn't be here");
         }
+        return registrationDetails;
     }
 }
--- Frontend/CO.CDP.OrganisationApp/Session.cs
+++ Frontend/CO.CDP.OrganisationApp/Session.cs
@@ -1,41 +1,40 @@
-﻿using System.Text.Json;
+using System.Text.Json;
 
-namespace CO.CDP.OrganisationApp
+namespace CO.CDP.OrganisationApp;
+
+public class Session(IHttpContextAccessor httpContextAccessor) : ISession
 {
-    public class Session(IHttpContextAccessor httpContextAccessor) : ISession
+    public const string RegistrationDetailsKey = "RegistrationDetails";
+
+    public T? Get<T>(string key)
     {
-        public const string RegistrationDetailsKey = "RegistrationDetails";
+        var value = GetSession().GetString(key);
+        return value == null ? default : JsonSerializer.Deserialize<T>(value);
+    }
 
-        public T? Get<T>(string key)
-        {
-            var value = GetSession().GetString(key);
-            return value == null ? default : JsonSerializer.Deserialize<T>(value);
-        }
+    public void Set<T>(string key, T value)
+    {
+        GetSession().SetString(key, JsonSerializer.Serialize(value));
+    }
 
-        public void Set<T>(string key, T value)
-        {
-            GetSession().SetString(key, JsonSerializer.Serialize(value));
-        }
+    public void Remove(string key)
+    {
+        GetSession().Remove(key);
+    }
 
-        public void Remove(string key)
+    private Microsoft.AspNetCore.Http.ISession GetSession()
+    {
+        try
         {
-            GetSession().Remove(key);
-        }
-
-        private Microsoft.AspNetCore.Http.ISession GetSession()
-        {
-            try
+            if (httpContextAccessor.HttpContext?.Session == null)
             {
-                if (httpContextAccessor.HttpContext?.Session == null)
-                {
-                    throw new Exception("Session is not available");
-                }
-                return httpContextAccessor.HttpContext!.Session;
+                throw new Exception("Session is not available");
             }
-            catch (InvalidOperationException cause)
-            {
-                throw new Exception("Session is not available", cause);
-            }
+            return httpContextAccessor.HttpContext!.Session;
+        }
+        catch (InvalidOperationException cause)
+        {
+            throw new Exception("Session is not available", cause);
         }
     }
 }
