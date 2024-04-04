e02fa42a2ef6ed49fbf73fdc3a6219b8a704343a
dpatel017 <dpatel017@outlook.com>
2024-04-04 16:48:40 +0100

    Organisation summury list.  Populate your detail page from session. (#53)

--- Frontend/CO.CDP.OrganisationApp/Pages/Registration/OrganisationDetailsSummary.cshtml.cs
+++ Frontend/CO.CDP.OrganisationApp/Pages/Registration/OrganisationDetailsSummary.cshtml.cs
@@ -1,18 +1,26 @@
+using CO.CDP.OrganisationApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace CO.CDP.OrganisationApp.Pages.SupplierRegistration;
+namespace CO.CDP.OrganisationApp.Pages.Registration;
 
-public class OrganisationDetailsSummaryModel : PageModel
+public class OrganisationDetailsSummaryModel(ISession session) : PageModel
 {
-    private readonly ILogger<OrganisationDetailsSummaryModel> _logger;
-
-    public OrganisationDetailsSummaryModel(ILogger<OrganisationDetailsSummaryModel> logger)
-    {
-        _logger = logger;
-    }
+    public RegistrationDetails RegistrationDetailModel { get; set; }
 
     public void OnGet()
     {
+        RegistrationDetailModel = VerifySession();
+    }
+
+    private RegistrationDetails VerifySession()
+    {
+        var registrationDetails = session.Get<RegistrationDetails>(Session.RegistrationDetailsKey);
+        if (registrationDetails == null)
+        {
+            //show error page (Once we finalise)
+            throw new Exception("Shoudn't be here");
+        }
+        return registrationDetails;
     }
 }
