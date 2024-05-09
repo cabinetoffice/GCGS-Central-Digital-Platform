967f220073ed46bbac1bfb7140b854cf8b4d2726
Jakub Zalas <jakub@zalas.pl>
2024-05-09 10:25:49 +0100

    Add user principal property to the Person

---
+++ Services/CO.CDP.OrganisationInformation.Persistence/Migrations/20240509092447_AddUserPrincipalToPerson.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CO.CDP.OrganisationInformation.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserPrincipalToPerson : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserPrincipal",
+                table: "Persons",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserPrincipal",
+                table: "Persons");
+        }
+    }
+}
--- Services/CO.CDP.OrganisationInformation.Persistence/Person.cs
+++ Services/CO.CDP.OrganisationInformation.Persistence/Person.cs
@@ -11,4 +11,5 @@ public class Person
     public required string FirstName { get; set; }
     public required string LastName { get; set; }
     public required string Email { get; set; }
+    public string? UserPrincipal { get; set; }
 }
