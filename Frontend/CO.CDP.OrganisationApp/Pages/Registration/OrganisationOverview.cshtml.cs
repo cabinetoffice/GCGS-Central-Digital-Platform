845464f80878396af87d40aae322f3d30c2a269d
dpatel017 <dpatel017@outlook.com>
2024-05-10 16:10:53 +0100

    code tidy up

--- Frontend/CO.CDP.OrganisationApp/Pages/Registration/OrganisationOverview.cshtml.cs
+++ Frontend/CO.CDP.OrganisationApp/Pages/Registration/OrganisationOverview.cshtml.cs
@@ -1,13 +1,7 @@
 using CO.CDP.Organisation.WebApiClient;
-using CO.CDP.OrganisationApp.Models;
-using CO.CDP.Person.WebApiClient;
-using CO.CDP.Tenant.WebApiClient;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 using OrganisationWebApiClient = CO.CDP.Organisation.WebApiClient;
-using PersonWebApiClient = CO.CDP.Person.WebApiClient;
 
 namespace CO.CDP.OrganisationApp.Pages.Registration;
 
