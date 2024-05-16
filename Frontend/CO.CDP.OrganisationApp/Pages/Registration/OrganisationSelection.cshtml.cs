a4662fd6dd6f7fec2e425bf02714649d1526cb86
Dharmendra Verma <64859911+dharmverma@users.noreply.github.com>
2024-05-16 15:17:11 +0100

    Registered Organisation list (#117)
    Fix broken flow

--- Frontend/CO.CDP.OrganisationApp/Pages/Registration/OrganisationSelection.cshtml.cs
+++ Frontend/CO.CDP.OrganisationApp/Pages/Registration/OrganisationSelection.cshtml.cs
@@ -1,3 +1,5 @@
+using CO.CDP.Organisation.WebApiClient;
+using CO.CDP.OrganisationApp.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -5,10 +7,31 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace CO.CDP.OrganisationApp.Pages.Registration;
 
 [Authorize]
-public class OrganisationSelectionModel() : PageModel
+public class OrganisationSelectionModel(
+    IOrganisationClient organisationClient,
+    ISession session) : PageModel
 {
+    public IEnumerable<Organisation.WebApiClient.Organisation> Organisations { get; set; } = [];
+
+    public async Task<IActionResult> OnGet()
+    {
+        var details = VerifySession();
+
+        Organisations = await organisationClient.ListOrganisationsAsync(details.UserUrn);
+
+        return Page();
+    }
+
     public IActionResult OnPost()
     {
         return RedirectToPage("OrganisationType");
+    }
+
+    private RegistrationDetails VerifySession()
+    {
+        var details = session.Get<RegistrationDetails>(Session.RegistrationDetailsKey)
+            ?? throw new Exception("Shoudn't be here"); // show error page?
+
+        return details;
     }
 }
