98743c8b04a4542e65952d5b6a6ab4dd1cf705f5
Jakub Zalas <jakub@zalas.pl>
2024-05-20 14:15:55 +0100

    Fix a typo

--- Services/CO.CDP.DataSharing.WebApi/Model/Identifier.cs
+++ Services/CO.CDP.DataSharing.WebApi/Model/Identifier.cs
@@ -8,7 +8,7 @@ internal record Identifier
     [Required] public required string Scheme { get; init; }
     /// <example>"5a360be7-e1d3-4214-9f72-0e1d6b57b85d"</example>
     [Required] public required string Id { get; init; }
-    /// <example>Acme Corporation Ltd."</example>
+    /// <example>"Acme Corporation Ltd."</example>
     [Required(AllowEmptyStrings = true)] public required string LegalName { get; init; }
     /// <example>"https://cdp.cabinetoffice.gov.uk/organisations/5a360be7-e1d3-4214-9f72-0e1d6b57b85d"</example>
     [Required(AllowEmptyStrings = true)] public required string Uri { get; init; }
