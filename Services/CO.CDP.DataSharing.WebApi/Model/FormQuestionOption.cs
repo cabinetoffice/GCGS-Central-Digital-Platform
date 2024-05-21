dc4601a4d5dab61c77edb5c66f0ba16f24f36da2
Jakub Zalas <jakub@zalas.pl>
2024-05-21 10:49:04 +0100

    Use the Id of the option for its value

--- Services/CO.CDP.DataSharing.WebApi/Model/FormQuestionOption.cs
+++ Services/CO.CDP.DataSharing.WebApi/Model/FormQuestionOption.cs
@@ -5,6 +5,6 @@ namespace CO.CDP.DataSharing.WebApi.Model;
 internal record FormQuestionOption
 {
     [Required] public required Guid Id { get; init; }
-    /// <example>"Option 1"</example>
+    /// <example>"3fa85f64-5717-4562-b3fc-2c963f66afa6"</example>
     public required string Value { get; init; }
 }
