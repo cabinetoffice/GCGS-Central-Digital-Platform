cb69ca2c4d4fc82de42c4e85e48ac017e2fa0c36
Jawwad Baig <jawwad.baig@goaco.com>
2024-05-31 13:38:15 +0100

    #dp-139 added base url to configuration also updated the documentation

--- Prototype/CompanyHouseApi.Integration/ExternalServices/CompaniesHouse/CompaniesHouseService.cs
+++ Prototype/CompanyHouseApi.Integration/ExternalServices/CompaniesHouse/CompaniesHouseService.cs
@@ -6,19 +6,20 @@ namespace CompanyHouseApi.Integration.ExternalServices.CompaniesHouse;
 public class CompaniesHouseService : ICompaniesHouseService
 {
     private readonly string _apiKey;
+    private readonly string _baseUrl;
     private readonly HttpClient httpClient;
 
     public CompaniesHouseService(HttpClient httpClient, IConfiguration configuration)
     {
         this.httpClient = httpClient;
         _apiKey = configuration["CompaniesHouse:ApiKey"] ?? throw new ArgumentNullException(nameof(configuration), "API Key is missing in configuration.");
+        _baseUrl = configuration["CompaniesHouse:BaseUrl"] ?? throw new ArgumentNullException(nameof(configuration), "Base URL is missing in configuration.");
     }
 
     public async Task<CompanyHouseDetails> GetCompanyAsync(string companyNumber)
     {
-        var request = new HttpRequestMessage(HttpMethod.Get, $"https://api.company-information.service.gov.uk/company/{companyNumber}");
-        var byteArray = new UTF8Encoding().GetBytes($"{_apiKey}:");
-        request.Headers.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+        var request = new HttpRequestMessage(HttpMethod.Get, $"{_baseUrl}/company/{companyNumber}");
+        request.Headers.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(new UTF8Encoding().GetBytes($"{_apiKey}:")));
 
         var response = await httpClient.SendAsync(request);
 
