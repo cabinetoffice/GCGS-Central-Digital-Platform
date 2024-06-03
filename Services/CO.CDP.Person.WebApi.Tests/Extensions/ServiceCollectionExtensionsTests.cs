5c58c3a90de1058c80bc1b158f0180e1b1064104
Gavin Lilley <gavin.lilley@goaco.com>
2024-06-03 11:30:22 +0100

    DP-131 lint

--- Services/CO.CDP.Person.WebApi.Tests/Extensions/ServiceCollectionExtensionsTests.cs
+++ Services/CO.CDP.Person.WebApi.Tests/Extensions/ServiceCollectionExtensionsTests.cs
@@ -8,52 +8,52 @@ namespace CO.CDP.Person.WebApi.Tests.Extensions;
 public class ServiceCollectionExtensionsTests
 {
     [Fact]
-        public void MapException_Should_Return_UnprocessableEntity_For_BadHttpRequestException()
-        {
-            var exception = new BadHttpRequestException("Bad request");
-            var result = ServiceCollectionExtensions.MapException(exception);
+    public void MapException_Should_Return_UnprocessableEntity_For_BadHttpRequestException()
+    {
+        var exception = new BadHttpRequestException("Bad request");
+        var result = ServiceCollectionExtensions.MapException(exception);
 
-            Assert.Equal(StatusCodes.Status422UnprocessableEntity, result.status);
-            Assert.Equal("UNPROCESSABLE_ENTITY", result.error);
-        }
+        Assert.Equal(StatusCodes.Status422UnprocessableEntity, result.status);
+        Assert.Equal("UNPROCESSABLE_ENTITY", result.error);
+    }
 
-        [Fact]
-        public void MapException_Should_Return_NotFound_For_UnknownPersonException()
-        {
-            var exception = new DuplicatePersonException("Duplicate person");
-            var result = ServiceCollectionExtensions.MapException(exception);
+    [Fact]
+    public void MapException_Should_Return_NotFound_For_UnknownPersonException()
+    {
+        var exception = new DuplicatePersonException("Duplicate person");
+        var result = ServiceCollectionExtensions.MapException(exception);
 
-            Assert.Equal(StatusCodes.Status404NotFound, result.status);
-            Assert.Equal("PERSON_DOES_NOT_EXIST", result.error);
-        }
+        Assert.Equal(StatusCodes.Status404NotFound, result.status);
+        Assert.Equal("PERSON_DOES_NOT_EXIST", result.error);
+    }
 
-        [Fact]
-        public void MapException_Should_Return_BadRequest_For_ArgumentNullException()
-        {
-            var exception = new ArgumentNullException();
-            var result = ServiceCollectionExtensions.MapException(exception);
+    [Fact]
+    public void MapException_Should_Return_BadRequest_For_ArgumentNullException()
+    {
+        var exception = new ArgumentNullException();
+        var result = ServiceCollectionExtensions.MapException(exception);
 
-            Assert.Equal(StatusCodes.Status400BadRequest, result.status);
-            Assert.Equal("ARGUMENT_NULL", result.error);
-        }
+        Assert.Equal(StatusCodes.Status400BadRequest, result.status);
+        Assert.Equal("ARGUMENT_NULL", result.error);
+    }
 
-        [Fact]
-        public void MapException_Should_Return_BadRequest_For_InvalidOperationException()
-        {
-            var exception = new InvalidOperationException();
-            var result = ServiceCollectionExtensions.MapException(exception);
+    [Fact]
+    public void MapException_Should_Return_BadRequest_For_InvalidOperationException()
+    {
+        var exception = new InvalidOperationException();
+        var result = ServiceCollectionExtensions.MapException(exception);
 
-            Assert.Equal(StatusCodes.Status400BadRequest, result.status);
-            Assert.Equal("INVALID_OPERATION", result.error);
-        }
+        Assert.Equal(StatusCodes.Status400BadRequest, result.status);
+        Assert.Equal("INVALID_OPERATION", result.error);
+    }
 
-        [Fact]
-        public void MapException_Should_Return_InternalServerError_For_GenericException()
-        {
-            var exception = new Exception();
-            var result = ServiceCollectionExtensions.MapException(exception);
+    [Fact]
+    public void MapException_Should_Return_InternalServerError_For_GenericException()
+    {
+        var exception = new Exception();
+        var result = ServiceCollectionExtensions.MapException(exception);
 
-            Assert.Equal(StatusCodes.Status500InternalServerError, result.status);
-            Assert.Equal("GENERIC_ERROR", result.error);
-        }
+        Assert.Equal(StatusCodes.Status500InternalServerError, result.status);
+        Assert.Equal("GENERIC_ERROR", result.error);
+    }
 }
