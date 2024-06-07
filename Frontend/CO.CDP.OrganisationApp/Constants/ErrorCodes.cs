d08c0c7e786e211877b776866a0a095cf101a691
Jawwad Baig <jawwad.baig@goaco.com>
2024-06-07 09:26:56 +0100

    #dp-168 removed any comments

--- Frontend/CO.CDP.OrganisationApp/Constants/ErrorCodes.cs
+++ Frontend/CO.CDP.OrganisationApp/Constants/ErrorCodes.cs
@@ -2,18 +2,14 @@ namespace CO.CDP.OrganisationApp.Constants;
 
 public static class ErrorCodes
 {
-    // Status400
     public const string ORGANISATION_ALREADY_EXISTS = "ORGANISATION_ALREADY_EXISTS";
     public const string INVALID_OPERATION = "INVALID_OPERATION";
     public const string ARGUMENT_NULL = "ARGUMENT_NULL";
     public const string PERSON_ALREADY_EXISTS = "PERSON_ALREADY_EXISTS";
 
-    // Status404
     public const string PERSON_DOES_NOT_EXIST = "PERSON_DOES_NOT_EXIST";
 
-    // Status422
     public const string UNPROCESSABLE_ENTITY = "UNPROCESSABLE_ENTITY";
 
-    // Status500
     public const string GENERIC_ERROR = "GENERIC_ERROR";
 }
