7e16493679031b6910fbaf8d94bf6da086b989fa
dpatel017 <dpatel017@outlook.com>
2024-06-13 11:23:52 +0100

    Non UK address navigation to summary bug

--- Frontend/CO.CDP.OrganisationApp/Pages/Registration/OrganisationNonUKAddress.cshtml.cs
+++ Frontend/CO.CDP.OrganisationApp/Pages/Registration/OrganisationNonUKAddress.cshtml.cs
@@ -75,20 +75,16 @@ public class OrganisationNonUKAddressModel(ISession session) : RegistrationStepM
 
         session.Set(Session.RegistrationDetailsKey, RegistrationDetails);
 
-        if (RegistrationDetails.OrganisationType == OrganisationType.Buyer)
-        {
-            if (RedirectToSummary == true)
-            {
-                return RedirectToPage("OrganisationDetailsSummary");
-            }
-            else
-            {
-                return RedirectToPage("BuyerOrganisationType");
-            }
-        }
-        else
+        if (RedirectToSummary == true)
         {
             return RedirectToPage("OrganisationDetailsSummary");
         }
+
+        if (RegistrationDetails.OrganisationType == OrganisationType.Buyer)
+        {
+            return RedirectToPage("BuyerOrganisationType");
+        }
+
+        return RedirectToPage("OrganisationDetailsSummary");
     }
 }
