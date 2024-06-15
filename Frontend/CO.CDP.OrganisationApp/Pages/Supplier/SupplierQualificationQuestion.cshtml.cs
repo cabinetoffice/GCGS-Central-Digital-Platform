b36faf60227ffa21d687322848bfb242fb5ec041
Jawwad Baig <jawwad.baig@goaco.com>
2024-06-15 12:38:52 +0100

    dp-152 supplier qualification question screen

---
+++ Frontend/CO.CDP.OrganisationApp/Pages/Supplier/SupplierQualificationQuestion.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CO.CDP.OrganisationApp.Pages.Supplier;
+
+public class SupplierQualificationQuestionModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
