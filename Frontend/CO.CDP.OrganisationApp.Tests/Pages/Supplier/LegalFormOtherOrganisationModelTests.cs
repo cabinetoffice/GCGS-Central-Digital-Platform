7b970fdbd1f31eb687cf7bf982094a5e1a4039f9
dpatel017 <dpatel017@outlook.com>
2024-07-01 18:45:22 +0100

    Bug fix for following... (#249)
    * Bug fix for following...
    
    - Qualification
    - Legal form
    
    Fixed back link for Check answer page Trade assurance
    
    * Code tidy up

--- Frontend/CO.CDP.OrganisationApp.Tests/Pages/Supplier/LegalFormOtherOrganisationModelTests.cs
+++ Frontend/CO.CDP.OrganisationApp.Tests/Pages/Supplier/LegalFormOtherOrganisationModelTests.cs
@@ -10,15 +10,31 @@ public class LegalFormOtherOrganisationModelTests
 {
     private readonly Mock<ITempDataService> _mockTempDataService;
     private readonly LegalFormOtherOrganisationModel _model;
+    private readonly Mock<Organisation.WebApiClient.IOrganisationClient> _mockOrganisationClient;
 
     public LegalFormOtherOrganisationModelTests()
     {
         _mockTempDataService = new Mock<ITempDataService>();
-        _model = new LegalFormOtherOrganisationModel(_mockTempDataService.Object);
+        _mockOrganisationClient = new Mock<Organisation.WebApiClient.IOrganisationClient>();
+        _model = new LegalFormOtherOrganisationModel(_mockTempDataService.Object, _mockOrganisationClient.Object);
     }
 
     [Fact]
-    public void OnGet_SetsOtherOrganisation_FromTempData()
+    public async Task OnGet_OrganisationNotFound_ShouldRedirectToPageNotFound()
+    {
+        _model.Id = Guid.NewGuid();
+
+        _mockOrganisationClient.Setup(client => client.GetOrganisationAsync(_model.Id))
+            .ThrowsAsync(new Organisation.WebApiClient.ApiException("Unexpected error", 404, "", default, null));
+
+        var result = await _model.OnGet(_model.Id);
+
+        result.Should().BeOfType<RedirectResult>()
+            .Which.Url.Should().Be("/page-not-found");
+    }
+
+    [Fact]
+    public async Task OnGet_SetsOtherOrganisation_FromTempData()
     {
         var id = Guid.NewGuid();
         var legalForm = new LegalForm
@@ -27,7 +43,7 @@ public class LegalFormOtherOrganisationModelTests
         };
         _mockTempDataService.Setup(s => s.PeekOrDefault<LegalForm>(LegalForm.TempDataKey)).Returns(legalForm);
 
-        var result = _model.OnGet(id);
+        var result = await _model.OnGet(id);
 
         _model.OtherOrganisation.Should().Be("Some Legal Form");
         result.Should().BeOfType<PageResult>();
@@ -53,9 +69,7 @@ public class LegalFormOtherOrganisationModelTests
         var legalForm = new LegalForm();
         _mockTempDataService.Setup(s => s.PeekOrDefault<LegalForm>(LegalForm.TempDataKey)).Returns(legalForm);
 
-
         var result = _model.OnPost();
-
 
         legalForm.RegisteredLegalForm.Should().Be("Some Legal Form");
         _mockTempDataService.Verify(s => s.Put(LegalForm.TempDataKey, legalForm), Times.Once);
