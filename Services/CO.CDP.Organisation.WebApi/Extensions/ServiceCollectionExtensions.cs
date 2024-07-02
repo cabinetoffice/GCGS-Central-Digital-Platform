2aa9f8698bcb72e3550815495f86b6b7950c6118
Jakub Zalas <jakub@zalas.pl>
2024-07-02 08:31:46 +0100

    DP-186: Document error responses (#246)
    * Generalise mapping exceptions to status/error codes
    
    * Generate multiple examples according to the passed error code list
    
    * Enable error responses documentation for the organisation service
    
    * DP-186: Update error responces for Person and Tenant API endpoints
    
    * Simplify OpenAPI example output and remove redundant code
    
    ---------
    
    Co-authored-by: Gavin Lilley <gavin.lilley@goaco.com>
    Co-authored-by: glgoaco <158280526+glgoaco@users.noreply.github.com>
    Co-authored-by: dpatel017 <dpatel017@outlook.com>

--- Services/CO.CDP.Organisation.WebApi/Extensions/ServiceCollectionExtensions.cs
+++ Services/CO.CDP.Organisation.WebApi/Extensions/ServiceCollectionExtensions.cs
@@ -1,11 +1,26 @@
 using CO.CDP.Organisation.WebApi.Model;
-using static CO.CDP.Organisation.WebApi.UseCase.RegisterOrganisationUseCase.RegisterOrganisationException;
 using static CO.CDP.OrganisationInformation.Persistence.IOrganisationRepository.OrganisationRepositoryException;
+using static CO.CDP.Organisation.WebApi.UseCase.RegisterOrganisationUseCase.RegisterOrganisationException;
 
 namespace CO.CDP.Organisation.WebApi.Extensions;
 
 public static class ServiceCollectionExtensions
 {
+    private static readonly Dictionary<Type, (int, string)> ExceptionMap = new()
+    {
+        { typeof(BadHttpRequestException), (StatusCodes.Status422UnprocessableEntity, "UNPROCESSABLE_ENTITY") },
+        { typeof(DuplicateOrganisationException), (StatusCodes.Status400BadRequest, "ORGANISATION_ALREADY_EXISTS") },
+        { typeof(UnknownPersonException), (StatusCodes.Status404NotFound, "PERSON_DOES_NOT_EXIST") },
+        { typeof(SupplierInfoNotExistException), (StatusCodes.Status422UnprocessableEntity, "UNPROCESSABLE_ENTITY") },
+        { typeof(ArgumentNullException), (StatusCodes.Status400BadRequest, "ARGUMENT_NULL") },
+        { typeof(InvalidOperationException), (StatusCodes.Status400BadRequest, "INVALID_OPERATION") },
+        { typeof(UnknownOrganisationException), (StatusCodes.Status404NotFound, "UNKNOWN_ORGANISATION") },
+        { typeof(BuyerInfoNotExistException), (StatusCodes.Status404NotFound, "BUYER_INFO_NOT_EXISTS") },
+        { typeof(InvalidUpdateBuyerInformationCommand), (StatusCodes.Status400BadRequest, "INVALID_BUYER_INFORMATION_UPDATE_ENTITY") },
+        { typeof(InvalidUpdateSupplierInformationCommand), (StatusCodes.Status400BadRequest, "INVALID_SUPPLIER_INFORMATION_UPDATE_ENTITY") },
+        { typeof(InvalidQueryException), (StatusCodes.Status400BadRequest, "MISSING_QUERY_PARAMETERS") },
+    };
+
     public static IServiceCollection AddOrganisationProblemDetails(this IServiceCollection services)
     {
         services.AddProblemDetails(options =>
@@ -27,30 +42,15 @@ public static class ServiceCollectionExtensions
 
     public static (int status, string error) MapException(Exception? exception)
     {
-        switch (exception)
+        if (ExceptionMap.TryGetValue(exception?.GetType() ?? typeof(Exception), out (int, string) code))
         {
-            case BadHttpRequestException:
-                return (StatusCodes.Status422UnprocessableEntity, "UNPROCESSABLE_ENTITY");
-            case DuplicateOrganisationException:
-                return (StatusCodes.Status400BadRequest, "ORGANISATION_ALREADY_EXISTS");
-            case UnknownPersonException:
-                return (StatusCodes.Status404NotFound, "PERSON_DOES_NOT_EXIST");
-            case ArgumentNullException:
-                return (StatusCodes.Status400BadRequest, "ARGUMENT_NULL");
-            case InvalidOperationException:
-                return (StatusCodes.Status400BadRequest, "INVALID_OPERATION");
-            case UnknownOrganisationException:
-                return (StatusCodes.Status404NotFound, "UNKNOWN_ORGANISATION");
-            case BuyerInfoNotExistException:
-                return (StatusCodes.Status404NotFound, "BUYER_INFO_NOT_EXISTS");
-            case InvalidUpdateBuyerInformationCommand:
-                return (StatusCodes.Status400BadRequest, "INVALID_BUYER_INFORMATION_UPDATE_ENTITY");
-            case InvalidUpdateSupplierInformationCommand:
-                return (StatusCodes.Status400BadRequest, "INVALID_SUPPLIER_INFORMATION_UPDATE_ENTITY");
-            case InvalidQueryException:
-                return (StatusCodes.Status400BadRequest, "MISSING_QUERY_PARAMETERS");
-            default:
-                return (StatusCodes.Status500InternalServerError, "GENERIC_ERROR");
+            return code;
         }
+        return (StatusCodes.Status500InternalServerError, "GENERIC_ERROR");
     }
+
+    public static Dictionary<string, List<string>> ErrorCodes() =>
+        ExceptionMap.Values
+            .GroupBy(s => s.Item1)
+            .ToDictionary(k => k.Key.ToString(), v => v.Select(i => i.Item2).Distinct().ToList());
 }
