b3136cebeb462ab29817ac4192cda5c03edcd86d
Gavin Lilley <gavin.lilley@goaco.com>
2024-07-10 13:30:33 +0100

    DP-309 refactor FormsEngine page to simplify partial view rendering

--- Frontend/CO.CDP.OrganisationApp/Pages/Forms/DynamicFormsPage.cshtml.cs
+++ Frontend/CO.CDP.OrganisationApp/Pages/Forms/DynamicFormsPage.cshtml.cs
@@ -8,6 +8,14 @@ public class DynamicFormsPageModel(IFormsEngine formsEngine) : PageModel
 {
     public SectionQuestionsResponse? SectionWithQuestions { get; set; }
 
+    private static readonly Dictionary<FormQuestionType, string> FormQuestionPartials = new()
+    {
+        { FormQuestionType.NoInput, "_FormElementNoInput" },
+        { FormQuestionType.YesOrNo, "_FormElementYesNoInput" },
+        { FormQuestionType.FileUpload, "_FormElementFileUpload" },
+        { FormQuestionType.Date, "_FormElementDateInput" }
+    };
+
     public async Task OnGetAsync(Guid formId, Guid sectionId)
     {
         SectionWithQuestions = await formsEngine.LoadFormSectionAsync(formId, sectionId);
@@ -16,5 +24,10 @@ public class DynamicFormsPageModel(IFormsEngine formsEngine) : PageModel
     public IActionResult OnPost()
     {
         return RedirectToPage("/Forms/Confirmation");
+    }
+
+    public string? GetPartialViewName(FormQuestionType questionType)
+    {
+        return FormQuestionPartials.TryGetValue(questionType, out var partialView) ? partialView : null;
     }
 }
