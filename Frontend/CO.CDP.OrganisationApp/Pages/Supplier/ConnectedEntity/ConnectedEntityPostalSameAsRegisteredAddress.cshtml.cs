5190c88ea9cc3aa7e6223ccb4c079852f1222fa3
dpatel017 <dpatel017@outlook.com>
2024-07-15 21:07:07 +0100

    Connected Person (#294)
    * DP-252 -> Connected Persons - Is your organisation registered on Companies House?
    DP-286 -> Connected Persons - Organisation, Individual or Trustee
    
    * address page integration draft
    
    * Test
    
    * Test
    
    * Postal address same as registered address
    
    * Added following page and unit test..
     - Organisation Ctegory
     - Organisation Name
    
    * Refactor
    
    * page redirect fix
    
    * Fixed test
    
    * warning fix
    
    ---------
    
    Co-authored-by: Dharm <dharmendraverma82@gmail.com>

---
+++ Frontend/CO.CDP.OrganisationApp/Pages/Supplier/ConnectedEntity/ConnectedEntityPostalSameAsRegisteredAddress.cshtml.cs
@@ -0,0 +1,95 @@
+using CO.CDP.OrganisationApp.Constants;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace CO.CDP.OrganisationApp.Pages.Supplier.ConnectedEntity;
+
+[Authorize]
+public class ConnectedEntityPostalSameAsRegisteredAddressModel(ISession session) : PageModel
+{
+    [BindProperty(SupportsGet = true)]
+    public Guid Id { get; set; }
+
+    [BindProperty(SupportsGet = true)]
+    public Guid? ConnectedEntityId { get; set; }
+
+    [BindProperty]
+    [Required(ErrorMessage = "Please select an option")]
+    public bool? DifferentThanRegiseterdAddress { get; set; }
+
+    public string? Caption { get; set; }
+
+    public string? OrganisationName { get; set; }
+
+    public IActionResult OnGet(bool? selected)
+    {
+        var (valid, state) = ValidatePage();
+        if (!valid)
+        {
+            return RedirectToPage(ConnectedEntityId.HasValue ?
+                "ConnectedEntityCheckAnswers" : "ConnectedEntitySupplierCompanyQuestion", new { Id, ConnectedEntityId });
+        }
+
+        DifferentThanRegiseterdAddress = selected.HasValue ? selected : AreSameAddress(state.RegisteredAddress, state.PostalAddress);
+
+        InitModal(state);
+        return Page();
+    }
+
+    public IActionResult OnPost()
+    {
+        var (valid, state) = ValidatePage();
+        if (!valid)
+        {
+            return RedirectToPage(ConnectedEntityId.HasValue ?
+                "ConnectedEntityCheckAnswers" : "ConnectedEntitySupplierCompanyQuestion", new { Id, ConnectedEntityId });
+        }
+
+        InitModal(state);
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+
+        if (DifferentThanRegiseterdAddress == false)
+        {
+            state.PostalAddress = state.RegisteredAddress;
+            session.Set(Session.ConnectedPersonKey, state);
+
+            //TODO 5: Next page link??;
+            return RedirectToPage("ConnectedEntityDeclaration", new { Id });
+        }
+
+        return RedirectToPage("ConnectedEntityAddress",
+            new { Id, ConnectedEntityId, AddressType = AddressType.Postal, UkOrNonUk = "uk" });
+    }
+
+    private (bool valid, ConnectedEntityState state) ValidatePage()
+    {
+        var cp = session.Get<ConnectedEntityState>(Session.ConnectedPersonKey);
+        if (cp == null ||
+            cp.SupplierOrganisationId != Id ||
+            (ConnectedEntityId.HasValue && cp.ConnectedEntityId.HasValue && cp.ConnectedEntityId != ConnectedEntityId))
+        {
+            return (false, new());
+        }
+        return (true, cp);
+    }
+
+    private static bool? AreSameAddress(ConnectedEntityState.Address? registeredAddress, ConnectedEntityState.Address? postalAddress)
+    {
+        if (registeredAddress == null || postalAddress == null) return null;
+        return registeredAddress.AddressLine1 == postalAddress.AddressLine1
+                && registeredAddress.TownOrCity == postalAddress.TownOrCity
+                && registeredAddress.Postcode == postalAddress.Postcode
+                && registeredAddress.Country == postalAddress.Country;
+    }
+
+    private void InitModal(ConnectedEntityState state)
+    {
+        OrganisationName = state.OrganisationName;
+        Caption = state.GetCaption();
+    }
+}
