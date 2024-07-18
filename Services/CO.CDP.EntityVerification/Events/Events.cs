d980a71366f2d70c5b87ff6cf6629e0eb0f6159b
Jakub Zalas <jakub@zalas.pl>
2024-07-18 12:39:53 +0100

    Include identifiers in the OrganisationRegistered event

--- Services/CO.CDP.EntityVerification/Events/Events.cs
+++ Services/CO.CDP.EntityVerification/Events/Events.cs
@@ -4,5 +4,15 @@ public interface IEvent;
 
 public record OrganisationRegistered : IEvent
 {
-    public required string Name { get; set;}
+    /// <example>"d2dab085-ec23-481c-b970-ee6b372f9f57"</example>
+    public required Guid Id { get; init; }
+
+    /// <example>"Acme Corporation"</example>
+    public required string Name { get; init; }
+
+    public required Identifier Identifier { get; init; }
+
+    public List<Identifier> AdditionalIdentifiers { get; init; } = [];
+
+    public required List<string> Roles { get; init; }
 }
