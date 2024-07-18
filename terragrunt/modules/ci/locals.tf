c5ff9b4c0dd3bd37615850950d8d836b9e25a742
Ali Bahman <abn@webit4.me>
2024-07-18 08:20:45 +0100

    DP-190 Add pipeline to provision entire account (#304)

--- terragrunt/modules/ci/locals.tf
+++ terragrunt/modules/ci/locals.tf
@@ -2,4 +2,7 @@ locals {
   name_prefix = var.product.resource_name
 
   orchestrator_account_id = var.account_ids["orchestrator"]
+
+  update_account_cb_name     = "update-account"
+  update_ecs_service_cb_name = "update-ecs-services"
 }
