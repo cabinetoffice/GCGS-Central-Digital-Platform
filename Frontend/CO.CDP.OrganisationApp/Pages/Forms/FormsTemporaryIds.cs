aa073454d0f231dd58c2d77c07c08396f349df16
Gavin Lilley <gavin.lilley@goaco.com>
2024-07-19 11:35:06 +0100

    DP-313 Unblock test with fixed Form and Section guids in FE

--- Frontend/CO.CDP.OrganisationApp/Pages/Forms/FormsTemporaryIds.cs
+++ Frontend/CO.CDP.OrganisationApp/Pages/Forms/FormsTemporaryIds.cs
@@ -3,6 +3,6 @@ namespace CO.CDP.OrganisationApp.Pages.Forms;
 public static class FormsTemporaryIds
 {
     // TODO: This is just temporary - need to delete once we have APIs returning proper IDs
-    public const string FORMID = "6e593748-a047-403e-9ce8-6c41230410d2";
-    public const string SECTIONID = "03854031-4e1e-49a6-a950-8a5b0cd72057";
+    public const string FORMID = "0618b13e-eaf2-46e3-a7d2-6f2c44be7022";
+    public const string SECTIONID = "13511cb1-9ed4-4d72-ba9e-05b4a0be880c";
 }
