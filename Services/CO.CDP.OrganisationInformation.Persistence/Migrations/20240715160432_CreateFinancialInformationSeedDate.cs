19a848891d61b2b002f3de3972a3c41fdd72292e
glgoaco <158280526+glgoaco@users.noreply.github.com>
2024-07-19 12:34:00 +0100

    DP-313 Unblock test with fixed Form and Section guids in FE (#316)
    * DP-313 Unblock test with fixed Form and Section guids in FE
    
    * DP-313 Align NoInput content with original example, adding html

--- Services/CO.CDP.OrganisationInformation.Persistence/Migrations/20240715160432_CreateFinancialInformationSeedDate.cs
+++ Services/CO.CDP.OrganisationInformation.Persistence/Migrations/20240715160432_CreateFinancialInformationSeedDate.cs
@@ -36,35 +36,24 @@ namespace CO.CDP.OrganisationInformation.Persistence.Migrations
                     -- Insert into form_questions
                     INSERT INTO form_questions (guid, section_id, type, is_required, title, description, options)
                     VALUES
-                    ('{Guid.NewGuid()}', formId, {(int)FormQuestionType.NoInput}, TRUE, 'The financial information you will need.', 'You will need to upload accounts or statements for your 2 most recent financial years. If you do not have 2 years, you can upload your most recent financial year. You will need to enter the financial year end date for the information you upload.', '{{}}'),
-                    ('{Guid.NewGuid()}', formId, {(int)FormQuestionType.YesOrNo}, TRUE, 'Were your accounts audited?', NULL, '{{}}'),
-                    ('{Guid.NewGuid()}', formId, {(int)FormQuestionType.FileUpload}, TRUE, 'Upload your accounts', 'Upload your most recent 2 financial years. If you do not have 2, upload your most recent financial year.', '{{}}'),
-                    ('{Guid.NewGuid()}', formId, {(int)FormQuestionType.Date}, TRUE, 'What is the financial year end date for the information you uploaded?', NULL, '{{}}'),
-                    ('{Guid.NewGuid()}', formId, {(int)FormQuestionType.Text}, TRUE, 'Enter a description of your financial status.', 'Please provide detailed information about your financial status.', '{{}}'),
-                    ('{Guid.NewGuid()}', formId, {(int)FormQuestionType.SingleChoice}, TRUE, 'Were your accounts audited?.', NULL, '[""Yes"",""No""]'),
-                    ('{Guid.NewGuid()}', formId, {(int)FormQuestionType.MultipleChoice}, TRUE, 'Which of the following statements applies to your accounts or financial statements?.', NULL, '[""Accounts submitted for our last two financial years were required to be audited."",""Accounts submitted for only our most recent financial year were required to be audited."",""Financial information for our most recent financial year. Our accounts did not require an audit.""]'),
-                    ('{Guid.NewGuid()}', formId, {(int)FormQuestionType.CheckYourAnswers}, TRUE, 'Check your answers', NULL, '{{}}');
-                END $$;
+        ('{Guid.NewGuid()}', formId, {(int)FormQuestionType.NoInput}, TRUE, 'The financial information you will need.', '<p class=""govuk-body"">You will need to upload accounts or statements for your 2 most recent financial years.</p><p class=""govuk-body"">If you do not have 2 years, you can upload your most recent financial year.</p><p class=""govuk-body"">You will need to enter the financial year end date for the information you upload.</p>', '{{}}'),
+        ('{Guid.NewGuid()}', formId, {(int)FormQuestionType.YesOrNo}, TRUE, 'Were your accounts audited?', NULL, '{{}}'),
+        ('{Guid.NewGuid()}', formId, {(int)FormQuestionType.FileUpload}, TRUE, 'Upload your accounts', 'Upload your most recent 2 financial years. If you do not have 2, upload your most recent financial year.', '{{}}'),
+        ('{Guid.NewGuid()}', formId, {(int)FormQuestionType.Date}, TRUE, 'What is the financial year end date for the information you uploaded?', NULL, '{{}}'),
+        ('{Guid.NewGuid()}', formId, {(int)FormQuestionType.Text}, TRUE, 'Enter a description of your financial status.', 'Please provide detailed information about your financial status.', '{{}}'),
+        ('{Guid.NewGuid()}', formId, {(int)FormQuestionType.SingleChoice}, TRUE, 'Were your accounts audited?.', NULL, '{{""choices"": [{{""id"": ""2687c252-236d-4804-b3a4-e3be396aa949"", ""title"": ""Yes"", ""groupName"": null, ""hint"": null, ""value"": ""Yes""}}, {{""id"": ""a7167886-0017-4f9d-978b-a845bacac9f9"", ""title"": ""No"", ""groupName"": null, ""hint"": null, ""value"": ""No""}}], ""choiceProviderStrategy"": null}}'),
+        ('{Guid.NewGuid()}', formId, {(int)FormQuestionType.MultipleChoice}, TRUE, 'Which of the following statements applies to your accounts or financial statements?.', NULL, '{{""choices"": [{{""id"": ""df7f9ab0-7b59-4fee-8dd7-74fde0348039"", ""title"": ""Accounts submitted for our last two financial years were required to be audited."", ""groupName"": null, ""hint"": {{""title"": null, ""description"": ""This means your accounts were audited for the last two years.""}}, ""value"": ""accountsLastTwoYearsAudited""}}, {{""id"": ""cf94e70b-aa5e-457f-bfcd-bf5f3150e688"", ""title"": ""Accounts submitted for only our most recent financial year were required to be audited."", ""groupName"": null, ""hint"": {{""title"": null, ""description"": ""This means your accounts were audited only for the most recent year.""}}, ""value"": ""accountsRecentYearAudited""}}, {{""id"": ""b97d4dd8-1437-4ddb-8778-d6807de558ff"", ""title"": ""Financial information for our most recent financial year. Our accounts did not require an audit."", ""groupName"": null, ""hint"": {{""title"": null, ""description"": ""This means your accounts were not audited for the most recent year.""}}, ""value"": ""recentYearNotAudited""}}], ""choiceProviderStrategy"": null}}'),
+        ('{Guid.NewGuid()}', formId, {(int)FormQuestionType.CheckYourAnswers}, TRUE, 'Check your answers', NULL, '{{}}');
+    END $$;
             ");
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DeleteData(
-                table: "form_questions",
-                keyColumn: "Id",
-                keyValues: new object[] { 1, 2, 3, 4 });
-
-            migrationBuilder.DeleteData(
-                table: "form_sections",
-                keyColumn: "Id",
-                keyValue: 1);
-
-            migrationBuilder.DeleteData(
-                table: "forms",
-                keyColumn: "Id",
-                keyValue: 1);
+            migrationBuilder.Sql("TRUNCATE TABLE form_questions CASCADE;");
+            migrationBuilder.Sql("TRUNCATE TABLE form_sections CASCADE;");
+            migrationBuilder.Sql("TRUNCATE TABLE forms CASCADE;");
         }
     }
 }
