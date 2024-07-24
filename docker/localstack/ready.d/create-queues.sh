47a14c8afe77e58c00d41d72201070c3d8354da5
Jakub Zalas <jakub@zalas.pl>
2024-07-24 08:18:53 +0100

    Fix the env var name

--- docker/localstack/ready.d/create-queues.sh
+++ docker/localstack/ready.d/create-queues.sh
@@ -4,5 +4,5 @@ echo "Creating SQS queues..."
 
 for queue in ${QUEUES//,/ }
 do
-    awslocal sqs create-queue --queue-name "${queue}" --region "${DEFAULT_REGION:-eu-west-1}"
+    awslocal sqs create-queue --queue-name "${queue}" --region "${REGION:-eu-west-1}"
 done
