0239b979d60a4e33dc1e77ec798162c6adfdd2f0
maciej-goaco <maciej.gontarz@goaco.com>
2024-07-24 13:34:52 +0100

    DP-214 - Tests updated to address the CR feedback

--- Services/CO.CDP.OrganisationInformation.Tests/Serialisation/LowerCamelCaseEnumConverterTest.cs
+++ Services/CO.CDP.OrganisationInformation.Tests/Serialisation/LowerCamelCaseEnumConverterTest.cs
@@ -1,44 +1,39 @@
 using System.Text.Json;
+using FluentAssertions;
 
 namespace CO.CDP.OrganisationInformation.Tests.Serialisation;
 
 public class LowerCamelCaseEnumConverterTest
 {
-    private readonly string _serialisedSinglePartyRoleString = "[\"supplier\"]";
-    private readonly string _serialisedMultiplePartyRolesString = "[\"supplier\",\"procuringEntity\"]";
-
-    private readonly List<PartyRole> _singlePartyRoleList = [PartyRole.Supplier];
-    private readonly List<PartyRole> _multiplePartyRolesList = [PartyRole.Supplier, PartyRole.ProcuringEntity];
-
     [Fact]
     public void Serialize_ShouldReadSinglePartyRoleEnum_WhenProvidedSerialisedLowerCamelCasedPartyRoles()
     {
-        var jsonString = JsonSerializer.Serialize(_singlePartyRoleList);
+        var jsonString = JsonSerializer.Serialize(new List<PartyRole>() { PartyRole.Supplier });
 
-        Assert.Equal(_serialisedSinglePartyRoleString, jsonString);
+        jsonString.Should().BeEquivalentTo("[\"supplier\"]");
     }
 
     [Fact]
     public void Deserialize_ShouldSerialiseSinglePartyRole_WhenProvidedPartyRolesEnum()
     {
-        var partyRole = JsonSerializer.Deserialize<List<PartyRole>>(_serialisedSinglePartyRoleString);
+        var partyRole = JsonSerializer.Deserialize<List<PartyRole>>("[\"supplier\"]");
 
-        Assert.Equal(_singlePartyRoleList, partyRole);
+        partyRole.Should().BeEquivalentTo(new List<PartyRole>() { PartyRole.Supplier });
     }
 
     [Fact]
     public void Serialize_ShouldReadMultiplePartyRolesEnum_WhenProvidedSerialisedLowerCamelCasedPartyRoles()
     {
-        var jsonString = JsonSerializer.Serialize(_multiplePartyRolesList);
+        var jsonString = JsonSerializer.Serialize(new List<PartyRole> { PartyRole.Supplier, PartyRole.ProcuringEntity });
 
-        Assert.Equal(_serialisedMultiplePartyRolesString, jsonString);
+        jsonString.Should().BeEquivalentTo("[\"supplier\",\"procuringEntity\"]");
     }
 
     [Fact]
     public void Deserialize_ShouldSerialiseMultiplePartyRoles_WhenProvidedPartyRolesEnum()
     {
-        var partyRoles = JsonSerializer.Deserialize<List<PartyRole>>(_serialisedMultiplePartyRolesString);
+        var partyRoles = JsonSerializer.Deserialize<List<PartyRole>>("[\"supplier\",\"procuringEntity\"]");
 
-        Assert.Equal(_multiplePartyRolesList, partyRoles);
+        partyRoles.Should().BeEquivalentTo(new List<PartyRole> { PartyRole.Supplier, PartyRole.ProcuringEntity });
     }
 }
