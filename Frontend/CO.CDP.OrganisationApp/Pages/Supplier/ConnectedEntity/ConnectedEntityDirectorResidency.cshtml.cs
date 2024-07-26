fbba21080c76afde52136a13e2300e9c2fad1e0c
dpatel017 <dpatel017@outlook.com>
2024-07-26 11:03:03 +0100

    Connected Persons - Individual - Directory Residency (#358)

---
+++ Frontend/CO.CDP.OrganisationApp/Pages/Supplier/ConnectedEntity/ConnectedEntityDirectorResidency.cshtml.cs
@@ -0,0 +1,79 @@
+using CO.CDP.OrganisationApp.Constants;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace CO.CDP.OrganisationApp.Pages.Supplier;
+
+[Authorize]
+public class ConnectedEntityDirectorResidencyModel(ISession session) : PageModel
+{
+    [BindProperty(SupportsGet = true)]
+    public Guid Id { get; set; }
+
+    [BindProperty(SupportsGet = true)]
+    public Guid? ConnectedEntityId { get; set; }
+
+    [BindProperty]
+    [Required(ErrorMessage = "Please select which country does person usually live in?")]
+    public string? DirectorLocation { get; set; }
+    public string? Caption { get; set; }
+    public string? Heading { get; set; }
+
+    public IActionResult OnGet()
+    {
+        var (valid, state) = ValidatePage();
+        if (!valid)
+        {
+            return RedirectToPage(ConnectedEntityId.HasValue ?
+                "ConnectedEntityCheckAnswers" : "ConnectedEntitySupplierCompanyQuestion", new { Id, ConnectedEntityId });
+        }
+
+        InitModal(state, true);
+
+        return Page();
+    }
+
+    public IActionResult OnPost()
+    {
+        var (valid, state) = ValidatePage();
+        if (!valid)
+        {
+            return RedirectToPage(ConnectedEntityId.HasValue ?
+                "ConnectedEntityCheckAnswers" : "ConnectedEntitySupplierCompanyQuestion", new { Id, ConnectedEntityId });
+        }
+
+        InitModal(state);
+
+        if (!ModelState.IsValid) return Page();
+
+        state.DirectorLocation = DirectorLocation;
+        session.Set(Session.ConnectedPersonKey, state);
+
+        return RedirectToPage("ConnectedEntityAddress", new { Id, ConnectedEntityId, AddressType = AddressType.Registered, UkOrNonUk = "uk" });
+    }
+
+    private void InitModal(ConnectedEntityState state, bool reset = false)
+    {
+        Caption = state.GetCaption();
+        Heading = $"What country does {state.FirstName} usually live in?";
+
+        if (reset)
+        {
+            DirectorLocation = state.DirectorLocation;
+        }
+    }
+
+    private (bool valid, ConnectedEntityState state) ValidatePage()
+    {
+        var cp = session.Get<ConnectedEntityState>(Session.ConnectedPersonKey);
+        if (cp == null ||
+            cp.SupplierOrganisationId != Id ||
+            (ConnectedEntityId.HasValue && cp.ConnectedEntityId.HasValue && cp.ConnectedEntityId != ConnectedEntityId))
+        {
+            return (false, new());
+        }
+        return (true, cp);
+    }
+}
