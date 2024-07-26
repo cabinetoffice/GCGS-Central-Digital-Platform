f43808f5925daeeb63479858d2d5e2907a007543
Dharm <dharmendraverma82@gmail.com>
2024-07-26 13:32:59 +0100

    Form summary page

---
+++ Frontend/CO.CDP.OrganisationApp.Tests/Pages/Forms/FormsAddAnotherAnswerSetModelTest.cs
@@ -0,0 +1,203 @@
+using WebApiClient = CO.CDP.Forms.WebApiClient;
+using CO.CDP.OrganisationApp.Pages.Forms;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Moq;
+
+namespace CO.CDP.OrganisationApp.Tests.Pages.Forms;
+
+public class FormsAddAnotherAnswerSetModelTest
+{
+    private Mock<WebApiClient.IFormsClient> _formsClientMock;
+    private Mock<ITempDataService> _tempDataServiceMock;
+    private FormsAddAnotherAnswerSetModel _model;
+
+    public FormsAddAnotherAnswerSetModelTest()
+    {
+        _formsClientMock = new Mock<WebApiClient.IFormsClient>();
+        _tempDataServiceMock = new Mock<ITempDataService>();
+        _model = new FormsAddAnotherAnswerSetModel(_formsClientMock.Object, _tempDataServiceMock.Object)
+        {
+            OrganisationId = Guid.NewGuid(),
+            FormId = Guid.NewGuid(),
+            SectionId = Guid.NewGuid()
+        };
+    }
+
+    private static WebApiClient.SectionQuestionsResponse CreateApiSectionQuestionsResponse(Guid sectionId, Guid questionId, Guid nextQuestionId)
+    {
+        return new WebApiClient.SectionQuestionsResponse(
+            section: new WebApiClient.FormSection(
+                allowsMultipleAnswerSets: true,
+                configuration: new WebApiClient.FormSectionConfiguration(
+                    singularSummaryHeading: null,
+                    pluralSummaryHeadingFormat: null,
+                    addAnotherAnswerLabel: null,
+                    removeConfirmationCaption: null,
+                    removeConfirmationHeading: null
+                ),
+                id: sectionId,
+                title: "SectionTitle"
+            ),
+            questions: new List<WebApiClient.FormQuestion>
+            {
+            new WebApiClient.FormQuestion(
+                id: questionId,
+                title: "Question1",
+                description: "Description1",
+                type: WebApiClient.FormQuestionType.Text,
+                isRequired: true,
+                nextQuestion: nextQuestionId,
+                nextQuestionAlternative: null,
+                options: new WebApiClient.FormQuestionOptions(
+                    choiceProviderStrategy: null,
+                    choices: new List<WebApiClient.FormQuestionChoice>
+                    {
+                        new WebApiClient.FormQuestionChoice(
+                            id: Guid.NewGuid(),
+                            title: "Option1",
+                            groupName: null,
+                            hint: new WebApiClient.FormQuestionChoiceHint(
+                                title: null,
+                                description: "Hint Description"
+                            )
+                        )
+                    }
+                )
+            )
+            },
+            answerSets: new List<WebApiClient.FormAnswerSet>()
+        );
+    }
+
+    [Fact]
+    public async Task OnGet_ShouldReturnPage_WhenInitAndVerifyPageReturnsTrue()
+    {
+        // Arrange
+        var sectionQuestionsResponse = CreateApiSectionQuestionsResponse(Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid());
+
+        _formsClientMock
+            .Setup(client => client.GetFormSectionQuestionsAsync(It.IsAny<Guid>(), It.IsAny<Guid>()))
+            .ReturnsAsync(sectionQuestionsResponse);
+
+        // Act
+        var result = await _model.OnGet();
+
+        // Assert
+        result.Should().BeOfType<PageResult>();
+    }
+
+    [Fact]
+    public async Task OnGet_ShouldRedirectToPageNotFound_WhenInitAndVerifyPageReturnsFalse()
+    {
+        // Arrange
+        _formsClientMock
+            .Setup(client => client.GetFormSectionQuestionsAsync(It.IsAny<Guid>(), It.IsAny<Guid>()))
+            .ThrowsAsync(new WebApiClient.ApiException("Unexpected error", 404, "", default, null));
+
+        // Act
+        var result = await _model.OnGet();
+
+        // Assert
+        var redirectResult = result.Should().BeOfType<RedirectResult>().Subject;
+        redirectResult.Url.Should().Be("/page-not-found");
+    }
+
+    [Fact]
+    public async Task OnPost_ShouldRedirectToDynamicFormsPage_WhenAddAnotherAnswerSetIsTrue()
+    {
+        // Arrange
+        _model.AddAnotherAnswerSet = true;
+
+        var sectionQuestionsResponse = CreateApiSectionQuestionsResponse(Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid());
+
+        _formsClientMock
+            .Setup(client => client.GetFormSectionQuestionsAsync(It.IsAny<Guid>(), It.IsAny<Guid>()))
+            .ReturnsAsync(sectionQuestionsResponse);
+
+        // Act
+        var result = await _model.OnPost();
+
+        // Assert
+        var redirectToPageResult = result.Should().BeOfType<RedirectToPageResult>().Subject;
+        redirectToPageResult.PageName.Should().Be("DynamicFormsPage");
+    }
+
+    [Fact]
+    public async Task OnPost_ShouldRedirectToSupplierBasicInformation_WhenAddAnotherAnswerSetIsFalse()
+    {
+        // Arrange
+        _model.AddAnotherAnswerSet = false;
+
+        var sectionQuestionsResponse = CreateApiSectionQuestionsResponse(Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid());
+
+        _formsClientMock
+            .Setup(client => client.GetFormSectionQuestionsAsync(It.IsAny<Guid>(), It.IsAny<Guid>()))
+            .ReturnsAsync(sectionQuestionsResponse);
+
+        // Act
+        var result = await _model.OnPost();
+
+        // Assert
+        var redirectToPageResult = result.Should().BeOfType<RedirectToPageResult>().Subject;
+        redirectToPageResult.PageName.Should().Be("SupplierBasicInformation");
+    }
+
+    [Fact]
+    public async Task OnPost_ShouldReturnPage_WhenModelStateIsInvalid()
+    {
+        // Arrange
+        _model.ModelState.AddModelError("key", "error message");
+
+        var sectionQuestionsResponse = CreateApiSectionQuestionsResponse(Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid());
+
+        _formsClientMock
+            .Setup(client => client.GetFormSectionQuestionsAsync(It.IsAny<Guid>(), It.IsAny<Guid>()))
+            .ReturnsAsync(sectionQuestionsResponse);
+
+        // Act
+        var result = await _model.OnPost();
+
+        // Assert
+        result.Should().BeOfType<PageResult>();
+    }
+
+    [Fact]
+    public async Task OnGetChange_ShouldRedirectToDynamicFormsPage_WhenAnswerSetIsFound()
+    {
+        // Arrange
+        var answerSetId = Guid.NewGuid();
+        var sectionQuestionsResponse = CreateApiSectionQuestionsResponse(Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid());
+
+        _formsClientMock
+            .Setup(client => client.GetFormSectionQuestionsAsync(It.IsAny<Guid>(), It.IsAny<Guid>()))
+            .ReturnsAsync(sectionQuestionsResponse);
+
+        // Act
+        var result = await _model.OnGetChange(answerSetId);
+
+        // Assert
+        var redirectToPageResult = result.Should().BeOfType<RedirectToPageResult>().Subject;
+        redirectToPageResult.PageName.Should().Be("DynamicFormsPage");
+    }
+
+    [Fact]
+    public async Task OnGetChange_ShouldRedirectToPageNotFound_WhenAnswerSetIsNotFound()
+    {
+        // Arrange
+        var answerSetId = Guid.NewGuid();
+        var sectionQuestionsResponse = CreateApiSectionQuestionsResponse(Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid());
+
+        _formsClientMock
+            .Setup(client => client.GetFormSectionQuestionsAsync(It.IsAny<Guid>(), It.IsAny<Guid>()))
+            .ReturnsAsync(sectionQuestionsResponse);
+
+        // Act
+        var result = await _model.OnGetChange(answerSetId);
+
+        // Assert
+        var redirectResult = result.Should().BeOfType<RedirectResult>().Subject;
+        redirectResult.Url.Should().Be("/page-not-found");
+    }
+}
--- Frontend/CO.CDP.OrganisationApp/Pages/Forms/FormsAddAnotherAnswerSet.cshtml.cs
+++ Frontend/CO.CDP.OrganisationApp/Pages/Forms/FormsAddAnotherAnswerSet.cshtml.cs
@@ -1,3 +1,5 @@
+using CO.CDP.Forms.WebApiClient;
+using CO.CDP.Mvc.Validation;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -5,7 +7,9 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace CO.CDP.OrganisationApp.Pages.Forms;
 
 [Authorize]
-public class FormsAddAnotherAnswerSetModel : PageModel
+public class FormsAddAnotherAnswerSetModel(
+    IFormsClient formsClient,
+    ITempDataService tempDataService) : PageModel
 {
     [BindProperty(SupportsGet = true)]
     public Guid OrganisationId { get; set; }
@@ -15,4 +19,123 @@ public class FormsAddAnotherAnswerSetModel : PageModel
 
     [BindProperty(SupportsGet = true)]
     public Guid SectionId { get; set; }
+
+    [BindProperty]
+    [RequiredIf(nameof(AllowsMultipleAnswerSets), true, ErrorMessage = "Please select an option")]
+    public bool? AddAnotherAnswerSet { get; set; }
+
+    public ICollection<FormAnswerSet> FormAnswerSets { get; set; } = [];
+
+    public string? Heading { get; set; }
+
+    public string? AddAnotherAnswerLabel { get; set; }
+
+    public bool AllowsMultipleAnswerSets { get; set; }
+
+    public async Task<IActionResult> OnGet()
+    {
+        var verify = await InitAndVerifyPage();
+        if (!verify)
+        {
+            return Redirect("/page-not-found");
+        }
+
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPost()
+    {
+        var verify = await InitAndVerifyPage();
+        if (!verify)
+        {
+            return Redirect("/page-not-found");
+        }
+
+        ModelState.Clear();
+        if (!TryValidateModel(this))
+        {
+            return Page();
+        }
+
+        if (AddAnotherAnswerSet == true)
+        {
+            return RedirectToPage("DynamicFormsPage", new { OrganisationId, FormId, SectionId });
+        }
+        else
+        {
+            return RedirectToPage("SupplierBasicInformation", new { OrganisationId });
+        }
+    }
+
+    public async Task<IActionResult> OnGetChange([FromQuery(Name = "answer-set-id")] Guid answerSetId)
+    {
+        SectionQuestionsResponse? response;
+        try
+        {
+            response = await formsClient.GetFormSectionQuestionsAsync(FormId, SectionId);
+        }
+        catch (ApiException ex) when (ex.StatusCode == 404)
+        {
+            return Redirect("/page-not-found");
+        }
+
+        var answerSet = response?.AnswerSets.FirstOrDefault(a => a.Id == answerSetId);
+        if (answerSet == null)
+        {
+            return Redirect("/page-not-found");
+        }
+
+        var state = new Models.FormQuestionAnswerState
+        {
+            AnswerSetId = answerSetId,
+            Answers = answerSet.Answers.Select(a => new Models.QuestionAnswer
+            {
+                QuestionId = a.QuestionId,
+                Answer = new Models.FormAnswer
+                {
+                    BoolValue = a.BoolValue,
+                    DateValue = a.DateValue,
+                    EndValue = a.EndValue,
+                    NumericValue = a.NumericValue,
+                    OptionValue = a.OptionValue,
+                    StartValue = a.StartValue,
+                    TextValue = a.TextValue
+                }
+            }).ToList()
+        };
+
+        tempDataService.Put($"Form_{OrganisationId}_{FormId}_{SectionId}_Answers", state);
+
+        var checkYourAnswersQuestionId = response?.Questions?.FirstOrDefault(q => q.Type == FormQuestionType.CheckYourAnswers)?.Id;
+
+        return RedirectToPage("DynamicFormsPage", new { OrganisationId, FormId, SectionId, CurrentQuestionId = checkYourAnswersQuestionId });
+    }
+
+    private async Task<bool> InitAndVerifyPage()
+    {
+        try
+        {
+            var response = await formsClient.GetFormSectionQuestionsAsync(FormId, SectionId);
+            FormAnswerSets = response.AnswerSets;
+            var valid = response != null;
+
+            if (valid)
+            {
+                AllowsMultipleAnswerSets = response!.Section.AllowsMultipleAnswerSets;
+                AddAnotherAnswerLabel = response.Section.Configuration.AddAnotherAnswerLabel;
+                Heading = response.Section.Configuration.SingularSummaryHeading;
+
+                if (FormAnswerSets.Count > 1)
+                {
+                    Heading = string.Format(response.Section.Configuration.PluralSummaryHeadingFormat, FormAnswerSets.Count);
+                }
+            }
+
+            return valid;
+        }
+        catch (ApiException ex) when (ex.StatusCode == 404)
+        {
+            return false;
+        }
+    }
 }
