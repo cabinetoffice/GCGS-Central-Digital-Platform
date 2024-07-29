5e3caa52d8fdd8cdb9594e696ef650ae57fca58e
glgoaco <158280526+glgoaco@users.noreply.github.com>
2024-07-29 14:34:36 +0100

    EF Update Form Answer Section Set (#375)

---
+++ Services/CO.CDP.OrganisationInformation.Persistence/Migrations/20240729120131_UpdateFormAnswerSectionSet.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CO.CDP.OrganisationInformation.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateFormAnswerSectionSet : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_form_answer_sets_form_sections_section_id",
+                table: "form_answer_sets");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_form_answer_sets_form_section_section_id",
+                table: "form_answer_sets",
+                column: "section_id",
+                principalTable: "form_sections",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_form_answer_sets_form_section_section_id",
+                table: "form_answer_sets");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_form_answer_sets_form_sections_section_id",
+                table: "form_answer_sets",
+                column: "section_id",
+                principalTable: "form_sections",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
