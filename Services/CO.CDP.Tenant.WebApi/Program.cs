9c83797d3be4d64897d9cb391d9bd1b73256ea79
Marek Matulka <mareg@users.noreply.github.com>
2024-07-30 23:20:58 +0200

    DP-200: PoC feature flag for swagger ui

--- Services/CO.CDP.Tenant.WebApi/Program.cs
+++ Services/CO.CDP.Tenant.WebApi/Program.cs
@@ -48,12 +48,13 @@ var app = builder.Build();
 app.UseForwardedHeaders();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (builder.Configuration.GetValue("Features:SwagerUI", false))
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-else
+
+if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler();
     app.UseHsts();
