18513d30da45bc28bb2ce4ce5e797b91da1b8710
Shilpi Goel <goel.shilpi@hotmail.co.uk>
2024-07-30 09:31:59 +0100

    corrected migration

--- Services/CO.CDP.OrganisationInformation.Persistence/Migrations/20240729135311_FormAnswerGuid.cs
+++ Services/CO.CDP.OrganisationInformation.Persistence/Migrations/20240729135311_FormAnswerGuid.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
@@ -10,45 +10,21 @@ namespace CO.CDP.OrganisationInformation.Persistence.Migrations
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropForeignKey(
-                name: "fk_form_answer_sets_form_sections_section_id",
-                table: "form_answer_sets");
-
+        {         
             migrationBuilder.AddColumn<Guid>(
                 name: "guid",
                 table: "form_answers",
                 type: "uuid",
                 nullable: false,
-                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
-
-            migrationBuilder.AddForeignKey(
-                name: "fk_form_answer_sets_form_section_section_id",
-                table: "form_answer_sets",
-                column: "section_id",
-                principalTable: "form_sections",
-                principalColumn: "id",
-                onDelete: ReferentialAction.Cascade);
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));           
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropForeignKey(
-                name: "fk_form_answer_sets_form_section_section_id",
-                table: "form_answer_sets");
-
+        {           
             migrationBuilder.DropColumn(
                 name: "guid",
-                table: "form_answers");
-
-            migrationBuilder.AddForeignKey(
-                name: "fk_form_answer_sets_form_sections_section_id",
-                table: "form_answer_sets",
-                column: "section_id",
-                principalTable: "form_sections",
-                principalColumn: "id",
-                onDelete: ReferentialAction.Cascade);
+                table: "form_answers");         
         }
     }
 }
