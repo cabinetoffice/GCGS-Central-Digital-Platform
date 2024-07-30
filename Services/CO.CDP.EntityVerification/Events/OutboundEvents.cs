8b8d77b05e35848176d4388c7d3ca1dad4b821b5
Jakub Zalas <jakub@zalas.pl>
2024-07-30 11:53:51 +0100

    Publish the OrganisationId with PponGenerated event

--- Services/CO.CDP.EntityVerification/Events/OutboundEvents.cs
+++ Services/CO.CDP.EntityVerification/Events/OutboundEvents.cs
@@ -2,6 +2,8 @@ namespace CO.CDP.EntityVerification.Events;
 
 public record PponGenerated : IEvent
 {
+    public required Guid OrganisationId { get; init; }
+
     /// <example>"d2dab085ec23481cb970ee6b372f9f57"</example>
     public required string Id { get; init; }
 
