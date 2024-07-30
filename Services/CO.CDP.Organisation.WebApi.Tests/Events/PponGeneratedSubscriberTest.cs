5bfd94f9b2504baf6cee1b415d803b99432c2523
Jakub Zalas <jakub@zalas.pl>
2024-07-30 10:05:30 +0100

    Handle organisation not found exception in the ppon generated subscriber

--- Services/CO.CDP.Organisation.WebApi.Tests/Events/PponGeneratedSubscriberTest.cs
+++ Services/CO.CDP.Organisation.WebApi.Tests/Events/PponGeneratedSubscriberTest.cs
@@ -1,14 +1,18 @@
 using CO.CDP.Organisation.WebApi.Events;
 using CO.CDP.Organisation.WebApi.Model;
 using CO.CDP.Organisation.WebApi.UseCase;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
 using Moq;
+using static CO.CDP.Organisation.WebApi.UseCase.AssignIdentifierUseCase.AssignIdentifierException;
 
 namespace CO.CDP.Organisation.WebApi.Tests.Events;
 
 public class PponGeneratedSubscriberTest
 {
     private readonly Mock<IUseCase<AssignOrganisationIdentifier, bool>> _useCase = new();
-    private PponGeneratedSubscriber Subscriber => new(_useCase.Object);
+    private readonly Mock<ILogger<PponGeneratedSubscriber>> _logger = new();
+    private PponGeneratedSubscriber Subscriber => new(_useCase.Object, _logger.Object);
 
     [Fact]
     public async Task ItDelegatesTheIdentifierAssignmentToTheUseCase()
@@ -31,5 +35,30 @@ public class PponGeneratedSubscriberTest
                 LegalName = "Acme Ltd"
             }
         }));
+    }
+
+    [Fact]
+    public void ItIgnoresTheOrganisationNotFoundException()
+    {
+        var organisationId = Guid.Parse("36a98954-f9d3-4695-a29b-b8b97318f3ac");
+        var exception = new OrganisationNotFoundException(organisationId);
+
+        _useCase.Setup(u => u.Execute(It.IsAny<AssignOrganisationIdentifier>()))
+            .Throws(exception);
+
+        var result = Subscriber.Handle(PponGenerated(organisationId: organisationId));
+
+        result.Should().BeAssignableTo<Task>();
+    }
+
+    private static PponGenerated PponGenerated(Guid organisationId)
+    {
+        return new PponGenerated
+        {
+            OrganisationId = organisationId,
+            Id = "c0777aeb968b4113a27d94e55b10c1b4",
+            Scheme = "CDP-PPON",
+            LegalName = "Acme Ltd"
+        };
     }
 }
--- Services/CO.CDP.Organisation.WebApi/Events/PponGeneratedSubscriber.cs
+++ Services/CO.CDP.Organisation.WebApi/Events/PponGeneratedSubscriber.cs
@@ -1,13 +1,28 @@
 using CO.CDP.MQ;
 using CO.CDP.Organisation.WebApi.Model;
 using CO.CDP.Organisation.WebApi.UseCase;
+using static CO.CDP.Organisation.WebApi.UseCase.AssignIdentifierUseCase.AssignIdentifierException;
 
 namespace CO.CDP.Organisation.WebApi.Events;
 
-public class PponGeneratedSubscriber(IUseCase<AssignOrganisationIdentifier, bool> assignIdentifierUseCase)
-    : ISubscriber<PponGenerated>
+public class PponGeneratedSubscriber(
+    IUseCase<AssignOrganisationIdentifier, bool> assignIdentifierUseCase,
+    ILogger<PponGeneratedSubscriber> logger
+) : ISubscriber<PponGenerated>
 {
     public async Task Handle(PponGenerated @event)
+    {
+        try
+        {
+            await AssignIdentifier(@event);
+        }
+        catch (OrganisationNotFoundException cause)
+        {
+            logger.LogError(cause.Message, cause);
+        }
+    }
+
+    private async Task AssignIdentifier(PponGenerated @event)
     {
         await assignIdentifierUseCase.Execute(new AssignOrganisationIdentifier
         {
