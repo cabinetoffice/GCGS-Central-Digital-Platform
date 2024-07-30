1a70b3bc0d2c401bcb32d8f4612ade389d50c27f
Ali Bahman <abn@webit4.me>
2024-07-30 15:11:07 +0100

    DP-200 POC on feature flagging Swagger UI (in healthcheck service only)

--- terragrunt/tools/healthcheck-service/healthCheck/Program.cs
+++ terragrunt/tools/healthcheck-service/healthCheck/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System;
 
 namespace healthCheck
 {
@@ -8,6 +9,10 @@ namespace healthCheck
     {
         public static void Main(string[] args)
         {
+            // Log debugging stuff
+            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            Console.WriteLine($"ASPNETCORE_ENVIRONMENT: {env}");
+
             CreateHostBuilder(args).Build().Run();
         }
 
