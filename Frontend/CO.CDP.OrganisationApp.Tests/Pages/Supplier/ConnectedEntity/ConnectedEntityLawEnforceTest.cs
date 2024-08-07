2e5f208ee0517f84c3f14778e7282c79fb4b2634
dpatel017 <dpatel017@outlook.com>
2024-08-07 17:17:58 +0100

    Law enforce

---
+++ Frontend/CO.CDP.OrganisationApp.Tests/Pages/Supplier/ConnectedEntity/ConnectedEntityLawEnforceTest.cs
@@ -0,0 +1,131 @@
+using CO.CDP.OrganisationApp.Pages.Supplier;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Moq;
+
+namespace CO.CDP.OrganisationApp.Tests.Pages.Supplier.ConnectedEntity;
+
+public class ConnectedEntityLawEnforceTest
+{
+    private readonly ConnectedEntityLawEnforceModel _model;
+    private readonly Mock<ISession> _sessionMock;
+    private readonly Guid _organisationId = Guid.NewGuid();
+    private readonly Guid _entityId = Guid.NewGuid();
+
+    public ConnectedEntityLawEnforceTest()
+    {
+        _sessionMock = new Mock<ISession>();
+        _model = new ConnectedEntityLawEnforceModel(_sessionMock.Object);
+        _model.Id = _organisationId;
+    }
+
+    [Fact]
+    public void OnGet_ShouldRedirectToConnectedEntitySupplierCompanyQuestion_WhenModelStateIsInvalid()
+    {
+        ConnectedEntityState? state = null;
+        _sessionMock
+            .Setup(s => s.Get<ConnectedEntityState>(Session.ConnectedPersonKey))
+            .Returns(state);
+
+        _model.ModelState.AddModelError("Error", "Model state is invalid");
+
+        var result = _model.OnGet();
+
+        result.Should().BeOfType<RedirectToPageResult>()
+            .Which.PageName.Should().Be("ConnectedEntitySupplierCompanyQuestion");
+    }
+
+    [Fact]
+    public void OnGet_ShouldReturnPageResult()
+    {
+        var state = DummyConnectedPersonDetails();
+
+        _sessionMock
+            .Setup(s => s.Get<ConnectedEntityState>(Session.ConnectedPersonKey))
+            .Returns(state);
+
+        var result = _model.OnGet();
+
+        result.Should().BeOfType<PageResult>();
+        _model.LawRegistered.Should().Be("Law-Registered");
+    }
+
+    [Fact]
+    public void OnGet_ShouldRedirectToConnectedEntitySupplierCompanyQuestion_WhenSessionStateIsNull()
+    {
+        _sessionMock
+            .Setup(s => s.Get<ConnectedEntityState>(Session.ConnectedPersonKey))
+            .Returns((ConnectedEntityState?)null);
+
+        var result = _model.OnGet();
+
+        result.Should().BeOfType<RedirectToPageResult>()
+            .Which.PageName.Should().Be("ConnectedEntitySupplierCompanyQuestion");
+    }
+
+    [Fact]
+    public void OnPost_ShouldReturnPage_WhenModelStateIsInvalid()
+    {
+        _sessionMock
+           .Setup(s => s.Get<ConnectedEntityState>(Session.ConnectedPersonKey))
+           .Returns((ConnectedEntityState?)null);
+
+        _model.ModelState.AddModelError("Error", "Model state is invalid");
+
+        var result = _model.OnPost();
+
+        result.Should().BeOfType<RedirectToPageResult>();
+    }
+
+
+    [Theory]
+    [InlineData("ConnectedEntityCompanyQuestion", false)]
+    [InlineData("ConnectedEntityCheckAnswersOrganisation", true)]
+    public void OnPost_ShouldRedirectToExpectedPage(string expectedRedirectPage, bool redirectToCheckYourAnswer)
+    {
+        var state = DummyConnectedPersonDetails();
+
+        _sessionMock.Setup(s => s.Get<ConnectedEntityState>(Session.ConnectedPersonKey)).
+            Returns(state);
+        _model.RedirectToCheckYourAnswer = redirectToCheckYourAnswer;
+
+        var result = _model.OnPost();
+
+        var redirectToPageResult = result.Should().BeOfType<RedirectToPageResult>().Subject;
+
+        result.Should().BeOfType<RedirectToPageResult>()
+            .Which.PageName.Should().Be(expectedRedirectPage);
+    }
+
+    [Fact]
+    public void OnPost_ShouldUpdateSessionState_WhenModelStateIsValid()
+    {
+        var state = DummyConnectedPersonDetails();
+
+        _sessionMock
+            .Setup(s => s.Get<ConnectedEntityState>(Session.ConnectedPersonKey))
+            .Returns(state);
+
+        _model.OnPost();
+
+        _sessionMock.Verify(s => s.Set(Session.ConnectedPersonKey, It.Is<ConnectedEntityState>(st => st.ConnectedEntityType == Constants.ConnectedEntityType.Organisation)), Times.Once);
+    }
+
+    private ConnectedEntityState DummyConnectedPersonDetails()
+    {
+        var connectedPersonDetails = new ConnectedEntityState
+        {
+            ConnectedEntityId = _entityId,
+            SupplierHasCompanyHouseNumber = true,
+            SupplierOrganisationId = _organisationId,
+            ConnectedEntityType = Constants.ConnectedEntityType.Organisation,
+            ConnectedEntityOrganisationCategoryType = Constants.ConnectedEntityOrganisationCategoryType.RegisteredCompany,
+            OrganisationName = "Org_name",
+            LegalForm = "Legal-Form",
+            LawRegistered = "Law-Registered"
+        };
+
+        return connectedPersonDetails;
+    }
+}
