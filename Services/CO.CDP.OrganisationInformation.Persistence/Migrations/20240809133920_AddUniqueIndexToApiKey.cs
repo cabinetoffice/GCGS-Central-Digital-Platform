3b0dff951d40b8c2b4e5e2180d2b60760a507275
Marek Matulka <mareg@users.noreply.github.com>
2024-08-09 14:39:44 +0100

    DP-455: Add an unique index constraint to the API Key

---
+++ Services/CO.CDP.OrganisationInformation.Persistence/Migrations/20240809133920_AddUniqueIndexToApiKey.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CO.CDP.OrganisationInformation.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqueIndexToApiKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "ix_authentication_keys_key",
+                table: "authentication_keys",
+                column: "key",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_authentication_keys_key",
+                table: "authentication_keys");
+        }
+    }
+}
