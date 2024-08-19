0525354c35beaf7fc8506b012dfc6c91acf5f103
Richard Mohammed <richard.mohammed@goaco.com>
2024-08-19 22:05:14 +0100

    Display roles next to user's name

--- Services/CO.CDP.Organisation.WebApi/Model/Person.cs
+++ Services/CO.CDP.Organisation.WebApi/Model/Person.cs
@@ -6,4 +6,5 @@ public record Person
     public required string FirstName { get; init; }
     public required string LastName { get; init; }
     public required string Email { get; init; }
+    public List<string> Scopes { get; set; }
 }
--- Services/CO.CDP.Organisation.WebApi/UseCase/GetPersonsUseCase.cs
+++ Services/CO.CDP.Organisation.WebApi/UseCase/GetPersonsUseCase.cs
@@ -3,12 +3,25 @@ using CO.CDP.Functional;
 using CO.CDP.OrganisationInformation.Persistence;
 
 namespace CO.CDP.Organisation.WebApi.UseCase;
-public class GetPersonsUseCase(IPersonRepository personRepository, IMapper mapper)
+public class GetPersonsUseCase(IPersonRepository personRepository, IOrganisationRepository organisationRepository, IMapper mapper)
     : IUseCase<Guid, IEnumerable<Model.Person>>
 {
     public async Task<IEnumerable<Model.Person>> Execute(Guid organisationId)
     {
-        return await personRepository.FindByOrganisation(organisationId)
-            .AndThen(mapper.Map<IEnumerable<Model.Person>>);
+        var organisation = await organisationRepository.Find(organisationId);
+        var persons = await personRepository.FindByOrganisation(organisationId);
+
+        var personModels = new List<Model.Person>();
+
+        foreach (var person in persons)
+        {
+            var personModel = mapper.Map<Model.Person>(person);
+            personModel.Scopes = person.PersonOrganisations.FirstOrDefault(po => po.OrganisationId == organisation.Id)
+                .Scopes;
+
+            personModels.Add(personModel);
+        }
+
+        return personModels;
     }
 }
