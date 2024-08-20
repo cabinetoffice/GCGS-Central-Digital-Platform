e1a47db8db62939ae72eaa4e8fc89c3f92ff3d93
Dharm <dharmendraverma82@gmail.com>
2024-08-20 17:46:51 +0100

    Remove temp file

--- Frontend/CO.CDP.OrganisationApp/Pages/Forms/FormsTemporaryIds.cs
+++
@@ -1,9 +0,0 @@
-namespace CO.CDP.OrganisationApp.Pages.Forms;
-
-public static class FormsTemporaryIds
-{
-    // TODO: This is just temporary - need to delete once we have APIs returning proper IDs
-    public const string FORMID = "0618b13e-eaf2-46e3-a7d2-6f2c44be7022";
-    public const string SECTIONID = "13511cb1-9ed4-4d72-ba9e-05b4a0be880c";
-    public const string SHAREDATA_SECTIONID = "936096b3-c3bb-4475-ad7d-73b44ff61e76";
-}
