e84fab4b71964f1c290bc800add34d8fd145f81e
Richard Mohammed <richard.mohammed@goaco.com>
2024-08-20 14:49:49 +0100

    Added unit tests for GetPersonInvitesUseCase.cs and GetPersonsUseCase.cs

---
+++ Services/CO.CDP.Organisation.WebApi.Tests/UseCase/GetPersonInvitesUseCaseTest.cs
@@ -0,0 +1,108 @@
+using AutoMapper;
+using CO.CDP.Organisation.WebApi.Model;
+using CO.CDP.OrganisationInformation.Persistence;
+using CO.CDP.Organisation.WebApi.UseCase;
+
+using Moq;
+using Organisation = CO.CDP.OrganisationInformation.Persistence.Organisation;
+
+public class GetPersonInvitesUseCaseTest
+{
+    private readonly Mock<IPersonInviteRepository> _personInviteRepositoryMock;
+    private readonly Mock<IOrganisationRepository> _organisationRepositoryMock;
+    private readonly Mock<IMapper> _mapperMock;
+    private readonly GetPersonInvitesUseCase _useCase;
+    private readonly Mock<Organisation> _organisationMock;
+
+    public GetPersonInvitesUseCaseTest()
+    {
+        _personInviteRepositoryMock = new Mock<IPersonInviteRepository>();
+        _organisationRepositoryMock = new Mock<IOrganisationRepository>();
+        _mapperMock = new Mock<IMapper>();
+        _useCase = new GetPersonInvitesUseCase(_personInviteRepositoryMock.Object, _mapperMock.Object);
+        _organisationMock = new Mock<Organisation>();
+    }
+
+    [Fact]
+    public async Task Execute_OrganisationHasNoPersonInvites_ReturnsEmptyList()
+    {
+        var organisationId = Guid.NewGuid();
+        _organisationRepositoryMock.Setup(repo => repo.Find(organisationId))
+            .ReturnsAsync(_organisationMock.Object);
+        _personInviteRepositoryMock.Setup(repo => repo.FindByOrganisation(organisationId))
+            .ReturnsAsync(new List<PersonInvite>());
+
+        var result = await _useCase.Execute(organisationId);
+
+        Assert.Empty(result);
+    }
+
+    [Fact]
+    public async Task Execute_OrganisationHasInvites_ReturnsMappedPersonInviteModels()
+    {
+        var organisationId = Guid.NewGuid();
+        var personInvites = new List<PersonInvite>
+        {
+            new PersonInvite {
+                Id = 1,
+                Guid = Guid.NewGuid(),
+                FirstName = "John",
+                LastName = "Johnson",
+                Email = "john@johnson.com",
+                Organisation = null,
+                Person = null,
+                Scopes = ["ADMIN", "VIEWER"],
+                InviteSentOn = default,
+                CreatedOn = default,
+                UpdatedOn = default
+            },
+            new PersonInvite {
+                Id = 2,
+                Guid = Guid.NewGuid(),
+                FirstName = "Bill",
+                LastName = "Billson",
+                Email = "bill@billson.com",
+                Organisation = null,
+                Person = null,
+                Scopes = ["EDITOR"],
+                InviteSentOn = default,
+                CreatedOn = default,
+                UpdatedOn = default
+            }
+        };
+
+        var personInviteModels = new List<PersonInviteModel>
+        {
+            new PersonInviteModel
+            {
+                Id = personInvites[0].Guid,
+                Email = personInvites[0].Email,
+                FirstName = personInvites[0].FirstName,
+                LastName = personInvites[0].LastName,
+                Scopes = personInvites[0].Scopes
+            },
+            new PersonInviteModel
+            {
+                Id = personInvites[1].Guid,
+                Email = personInvites[1].Email,
+                FirstName = personInvites[1].FirstName,
+                LastName = personInvites[1].LastName,
+                Scopes = personInvites[1].Scopes
+            }
+        };
+
+        _personInviteRepositoryMock.Setup(repo => repo.FindByOrganisation(organisationId))
+            .ReturnsAsync(personInvites);
+
+        _mapperMock.Setup(mapper => mapper.Map<IEnumerable<PersonInviteModel>>(personInvites))
+            .Returns(personInviteModels);
+
+        var result = await _useCase.Execute(organisationId);
+
+        Assert.NotEmpty(result);
+        Assert.Equal(2, result.Count());
+        Assert.Equal(personInviteModels, result);
+        _personInviteRepositoryMock.Verify(repo => repo.FindByOrganisation(organisationId), Times.Once);
+        _mapperMock.Verify(mapper => mapper.Map<IEnumerable<PersonInviteModel>>(personInvites), Times.Once);
+    }
+}
---
+++ Services/CO.CDP.Organisation.WebApi.Tests/UseCase/GetPersonsUseCaseTest.cs
@@ -0,0 +1,133 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using CO.CDP.Functional;
+using CO.CDP.Organisation.WebApi.Model;
+using CO.CDP.OrganisationInformation.Persistence;
+using CO.CDP.Organisation.WebApi.UseCase;
+using FluentAssertions;
+using Moq;
+using Xunit;
+using Organisation = CO.CDP.OrganisationInformation.Persistence.Organisation;
+using Person = CO.CDP.OrganisationInformation.Persistence.Person;
+
+public class GetPersonsUseCaseTest
+{
+    private readonly Mock<IPersonRepository> _personRepositoryMock;
+    private readonly Mock<IOrganisationRepository> _organisationRepositoryMock;
+    private readonly Mock<IMapper> _mapperMock;
+    private readonly GetPersonsUseCase _useCase;
+    private readonly Mock<Organisation> _organisationMock;
+
+    public GetPersonsUseCaseTest()
+    {
+        _personRepositoryMock = new Mock<IPersonRepository>();
+        _organisationRepositoryMock = new Mock<IOrganisationRepository>();
+        _mapperMock = new Mock<IMapper>();
+        _useCase = new GetPersonsUseCase(_personRepositoryMock.Object, _organisationRepositoryMock.Object, _mapperMock.Object);
+        _organisationMock = new Mock<Organisation>();
+    }
+
+    [Fact]
+    public async Task Execute_OrganisationDoesNotExist_ReturnsEmptyList()
+    {
+        var organisationId = Guid.NewGuid();
+        _organisationRepositoryMock.Setup(repo => repo.Find(organisationId))
+            .ReturnsAsync((Organisation)null);
+
+        Func<Task> act = async () => await _useCase.Execute(organisationId);
+
+        await act.Should().ThrowAsync<UnknownOrganisationException>()
+            .WithMessage($"Unknown organisation {organisationId}.");
+    }
+
+    [Fact]
+    public async Task Execute_OrganisationExistsButNoPersons_ReturnsEmptyList()
+    {
+        var organisationId = Guid.NewGuid();
+        _organisationRepositoryMock.Setup(repo => repo.Find(organisationId))
+            .ReturnsAsync(_organisationMock.Object);
+        _personRepositoryMock.Setup(repo => repo.FindByOrganisation(organisationId))
+            .ReturnsAsync(new List<Person>());
+
+        var result = await _useCase.Execute(organisationId);
+
+        Assert.Empty(result);
+    }
+
+    [Fact]
+    public async Task Execute_OrganisationExistsAndHasPersons_ReturnsPersonModels()
+    {
+        var organisationGuid = Guid.NewGuid();
+        var organisation = new Organisation
+        {
+            Id = 1,
+            Guid = organisationGuid,
+            Tenant = null,
+            Name = null
+        };
+
+        var persons = new List<Person>
+        {
+            new Person
+            {
+                Id = 1,
+                PersonOrganisations = new List<OrganisationPerson>
+                {
+                    new OrganisationPerson
+                    {
+                        OrganisationId = 1,
+                        Scopes = ["Scope1"],
+                        Person = null,
+                        Organisation = organisation
+                    }
+                },
+                Guid = default,
+                FirstName = "Bill",
+                LastName = "Billson",
+                Email = "bill@billson.com"
+            },
+            new Person
+            {
+                Id = 2,
+                PersonOrganisations = new List<OrganisationPerson>
+                {
+                    new OrganisationPerson
+                    {
+                        OrganisationId = 1,
+                        Scopes = ["Scope2"],
+                        Person = null,
+                        Organisation = organisation
+                    }
+                },
+                Guid = default,
+                FirstName = "John",
+                LastName = "Johnson",
+                Email = "john@johnson.com"
+            },
+        };
+
+        _organisationRepositoryMock.Setup(repo => repo.Find(organisationGuid))
+            .ReturnsAsync(organisation);
+
+        _personRepositoryMock.Setup(repo => repo.FindByOrganisation(organisationGuid))
+            .ReturnsAsync(persons);
+
+        _mapperMock.Setup(mapper => mapper.Map<CO.CDP.Organisation.WebApi.Model.Person>(It.IsAny<Person>()))
+            .Returns((Person source) => new CO.CDP.Organisation.WebApi.Model.Person
+            {
+                Id = Guid.NewGuid(),
+                FirstName = null,
+                LastName = null,
+                Email = null
+            });
+
+        var result = await _useCase.Execute(organisationGuid);
+
+        Assert.Equal(2, result.Count());
+        Assert.Contains("Scope1", result.First().Scopes);
+        Assert.Contains("Scope2", result.Last().Scopes);
+    }
+}
