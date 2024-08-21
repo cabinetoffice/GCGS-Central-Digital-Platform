58c9256a52725f1b193bd833d4cf51caada0db7f
Marek Matulka <mareg@users.noreply.github.com>
2024-08-21 16:27:19 +0100

    DP-222: Wrap bootstrap in try/catch block

--- Services/CO.CDP.Tenant.WebApi/Program.cs
+++ Services/CO.CDP.Tenant.WebApi/Program.cs
@@ -14,64 +14,82 @@ using Tenant = CO.CDP.Tenant.WebApi.Model.Tenant;
 using TenantLookup = CO.CDP.OrganisationInformation.TenantLookup;
 using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
-builder.ConfigureForwardedHeaders();
+// Console logger to log configuration issues
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options => { options.DocumentTenantApi(builder.Configuration); });
-builder.Services.AddHealthChecks();
-builder.Services.AddSerilog();
-
-builder.Services.AddAutoMapper(typeof(WebApiToPersistenceProfile));
-
-builder.Services.AddDbContext<OrganisationInformationContext>(o =>
-    o.UseNpgsql(ConnectionStringHelper.GetConnectionString(builder.Configuration, "OrganisationInformationDatabase")));
-
-builder.Services.AddScoped<ITenantRepository, DatabaseTenantRepository>();
-
-builder.Services.AddScoped<IUseCase<RegisterTenant, Tenant>, RegisterTenantUseCase>();
-builder.Services.AddScoped<IUseCase<Guid, Tenant?>, GetTenantUseCase>();
-builder.Services.AddScoped<IUseCase<TenantLookup?>, LookupTenantUseCase>();
-builder.Services.AddScoped<IClaimService, ClaimService>();
-builder.Services.AddTenantProblemDetails();
-
-builder.Services.AddJwtBearerAndApiKeyAuthentication(builder.Configuration, builder.Environment);
-//builder.Services.AddAuthorization();
-builder.Services.AddOrganisationAuthorization();
-builder.Services.AddHttpContextAccessor();
-
-if (Assembly.GetEntryAssembly().IsRunAs("CO.CDP.Tenant.WebApi"))
+try
 {
-    builder.Services.AddHealthChecks()
-        .AddNpgSql(ConnectionStringHelper.GetConnectionString(builder.Configuration, "OrganisationInformationDatabase"));
+    var builder = WebApplication.CreateBuilder(args);
+    builder.ConfigureForwardedHeaders();
+
+    // Add services to the container.
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen(options => { options.DocumentTenantApi(builder.Configuration); });
+    builder.Services.AddHealthChecks();
+    builder.Services.AddSerilog();
+
+    builder.Services.AddAutoMapper(typeof(WebApiToPersistenceProfile));
+
+    builder.Services.AddDbContext<OrganisationInformationContext>(o =>
+        o.UseNpgsql(
+            ConnectionStringHelper.GetConnectionString(builder.Configuration, "OrganisationInformationDatabase")));
+
+    builder.Services.AddScoped<ITenantRepository, DatabaseTenantRepository>();
+
+    builder.Services.AddScoped<IUseCase<RegisterTenant, Tenant>, RegisterTenantUseCase>();
+    builder.Services.AddScoped<IUseCase<Guid, Tenant?>, GetTenantUseCase>();
+    builder.Services.AddScoped<IUseCase<TenantLookup?>, LookupTenantUseCase>();
+    builder.Services.AddScoped<IClaimService, ClaimService>();
+    builder.Services.AddTenantProblemDetails();
+
+    builder.Services.AddJwtBearerAndApiKeyAuthentication(builder.Configuration, builder.Environment);
+    //builder.Services.AddAuthorization();
+    builder.Services.AddOrganisationAuthorization();
+    builder.Services.AddHttpContextAccessor();
+
+    if (Assembly.GetEntryAssembly().IsRunAs("CO.CDP.Tenant.WebApi"))
+    {
+        builder.Services.AddHealthChecks()
+            .AddNpgSql(ConnectionStringHelper.GetConnectionString(builder.Configuration,
+                "OrganisationInformationDatabase"));
+    }
+
+    var app = builder.Build();
+    app.UseForwardedHeaders();
+
+    // Configure the HTTP request pipeline.
+    if (builder.Configuration.GetValue("Features:SwaggerUI", false))
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler();
+        app.UseHsts();
+    }
+
+    app.UseStatusCodePages();
+
+    app.MapHealthChecks("/health").AllowAnonymous();
+    app.UseHttpsRedirection();
+    app.UseAuthentication();
+    app.UseAuthorization();
+    app.UseTenantEndpoints();
+    app.UseTenantLookupEndpoints();
+    app.Run();
 }
-
-var app = builder.Build();
-app.UseForwardedHeaders();
-
-// Configure the HTTP request pipeline.
-if (builder.Configuration.GetValue("Features:SwaggerUI", false))
+catch (Exception ex)
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    Log.Fatal(ex, "Application terminated unexpectedly");
 }
-
-if (!app.Environment.IsDevelopment())
+finally
 {
-    app.UseExceptionHandler();
-    app.UseHsts();
+    Log.CloseAndFlush();
 }
-
-app.UseStatusCodePages();
-
-app.MapHealthChecks("/health").AllowAnonymous();
-app.UseHttpsRedirection();
-app.UseAuthentication();
-app.UseAuthorization();
-app.UseTenantEndpoints();
-app.UseTenantLookupEndpoints();
-app.Run();
 
 public abstract partial class Program;
