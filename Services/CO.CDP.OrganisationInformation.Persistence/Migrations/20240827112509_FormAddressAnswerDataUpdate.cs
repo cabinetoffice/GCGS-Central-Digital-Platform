0c0a5b21c840d61e9d029d994c58bea9e7a791b2
Dharm <dharmendraverma82@gmail.com>
2024-08-27 12:31:31 +0100

    Form Address Answer migration fix

---
+++ Services/CO.CDP.OrganisationInformation.Persistence/Migrations/20240827112509_FormAddressAnswerDataUpdate.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CO.CDP.OrganisationInformation.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class FormAddressAnswerDataUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"UPDATE form_answers 
+                SET address_value = address_value || '{""Country"":""GB""}'
+                WHERE address_value ->> 'CountryName' = 'United Kingdom';
+
+                UPDATE form_answers 
+                SET address_value = address_value || '{""Country"":""""}'
+                WHERE address_value ->> 'CountryName' != 'United Kingdom';");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
