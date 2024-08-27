34ad9c20e69ef58ad9bbe404708cabddb44f4047
Jawwad Baig <jawwad.baig@goaco.com>
2024-08-27 18:04:26 +0100

    #dp-271 updated the navigation flow for declaration form

--- Frontend/CO.CDP.OrganisationApp/Pages/Forms/DynamicFormsPage.cshtml.cs
+++ Frontend/CO.CDP.OrganisationApp/Pages/Forms/DynamicFormsPage.cshtml.cs
@@ -106,11 +106,15 @@ public class DynamicFormsPageModel(
 
             tempDataService.Remove(FormQuestionAnswerStateKey);
 
-            var shareCode = await formsEngine.CreateShareCodeAsync(FormId, OrganisationId);
-            return RedirectToPage(FormSectionType == Models.FormSectionType.Declaration ?
-            "/ShareInformation/ShareCodeConfirmation" : "FormsAddAnotherAnswerSet",
-            new { OrganisationId, FormId, SectionId, shareCode });
+            if (FormSectionType == Models.FormSectionType.Declaration)
+            {
+                var shareCode = await formsEngine.CreateShareCodeAsync(FormId, OrganisationId);
 
+                return RedirectToPage("/ShareInformation/ShareCodeConfirmation",
+                            new { OrganisationId, FormId, SectionId, shareCode });
+            }
+
+            return RedirectToPage("FormsAddAnotherAnswerSet", new { OrganisationId, FormId, SectionId });
         }
 
         Guid? nextQuestionId;
