347ba65cae0fd222acafe42ea20246aa26eeb20f
dpatel017 <dpatel017@outlook.com>
2024-08-30 17:13:00 +0100

    Link back to FTS with the organisation id in the query parameter

--- Frontend/CO.CDP.OrganisationApp/Constants/RegExPatterns.cs
+++ Frontend/CO.CDP.OrganisationApp/Constants/RegExPatterns.cs
@@ -6,4 +6,5 @@ public static class RegExPatterns
     public const string Day = @"^(0?[1-9]|[12][0-9]|3[01])$";
     public const string Month = @"^(0?[1-9]|1[0-2])$";
     public const string Year = @"^\d{4}$";
+    public const string OrganisationId = @"/organisation/([0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12})";
 }
