3321681aec9cb5634c7cd7acb36e0b886a2ab704
Jakub Zalas <jakub@zalas.pl>
2024-09-02 00:14:03 +0100

    Use consistent collection types

--- Services/CO.CDP.DataSharing.WebApi/Model/SupplierInformationData.cs
+++ Services/CO.CDP.DataSharing.WebApi/Model/SupplierInformationData.cs
@@ -3,6 +3,6 @@ namespace CO.CDP.DataSharing.WebApi.Model;
 public record SupplierInformationData
 {
     public required Form Form { get; init; }
-    public required IList<FormAnswerSet> AnswerSets { get; init; } = [];
+    public required ICollection<FormAnswerSet> AnswerSets { get; init; } = [];
     public required ICollection<FormQuestion> Questions { get; set; } = [];
 }
