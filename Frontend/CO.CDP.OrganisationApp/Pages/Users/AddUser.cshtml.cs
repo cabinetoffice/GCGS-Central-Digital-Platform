c1e4ba3bdda17c42e1d2683032a2982265fefff3
Andy Mantell <134642+andymantell@users.noreply.github.com>
2024-09-04 12:32:07 +0100

    Add email validation to add user form

--- Frontend/CO.CDP.OrganisationApp/Pages/Users/AddUser.cshtml.cs
+++ Frontend/CO.CDP.OrganisationApp/Pages/Users/AddUser.cshtml.cs
@@ -28,6 +28,7 @@ public class AddUserModel(
 
     [BindProperty]
     [Required(ErrorMessage = "Email required")]
+    [EmailAddress(ErrorMessage = "Enter an email address in the correct format, like name@example.com")]
     public string? Email { get; set; }
 
     [BindProperty]
