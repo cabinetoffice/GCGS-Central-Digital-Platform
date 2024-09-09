5ad23cbf344184e60c48e8252d1f118a273a742e
Andy Mantell <134642+andymantell@users.noreply.github.com>
2024-09-09 09:44:16 +0100

    Split change user role person and person invite tests into separate files

---
+++ Frontend/CO.CDP.OrganisationApp.Tests/Pages/Users/ChangePersonInviteUserRoleTests.cs
@@ -0,0 +1,137 @@
+using Moq;
+using CO.CDP.OrganisationApp.Pages.Users;
+using CO.CDP.Organisation.WebApiClient;
+using CO.CDP.OrganisationApp.Constants;
+using CO.CDP.OrganisationApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CO.CDP.OrganisationApp.Tests.Pages.Users;
+
+public class ChangePersonInviteUserRoleTests
+{
+    private readonly Mock<ISession> _mockSession;
+    private readonly Mock<IOrganisationClient> _mockOrganisationClient;
+    private readonly ChangeUserRoleModel _changeUserRoleModel;
+
+    public ChangePersonInviteUserRoleTests()
+    {
+        _mockSession = new Mock<ISession>();
+        _mockSession.Setup(s => s.Get<UserDetails>(Session.UserDetailsKey))
+            .Returns(new UserDetails() { UserUrn = "whatever" });
+
+        _mockOrganisationClient = new Mock<IOrganisationClient>();
+        _changeUserRoleModel = new ChangeUserRoleModel(_mockOrganisationClient.Object, _mockSession.Object);
+    }
+
+    [Fact]
+    public async Task OnGet_ShouldInitializeModel_WhenPersonInviteExists()
+    {
+        var mockPersonInvite = new PersonInviteModel(
+                                    "a@b.com",
+                                    "John",
+                                    new Guid(),
+                                    "Smith",
+                                    new List<string> { PersonScopes.Admin, PersonScopes.Editor }
+                                );
+
+        _mockOrganisationClient
+            .Setup(s => s.GetOrganisationPersonInvitesAsync(mockPersonInvite.Id))
+            .ReturnsAsync(new List<PersonInviteModel>() { mockPersonInvite });
+
+        var result = await _changeUserRoleModel.OnGetPersonInvite();
+
+        Assert.Equal("John Smith", _changeUserRoleModel.UserFullName);
+        Assert.True(_changeUserRoleModel.IsAdmin);
+        Assert.Equal(PersonScopes.Editor, _changeUserRoleModel.Role);
+    }
+
+    [Fact]
+    public async Task OnGet_RedirectsToNotFound_WhenPersonInviteDoesntExist()
+    {
+        _mockOrganisationClient
+            .Setup(s => s.GetOrganisationPersonInvitesAsync(It.IsAny<Guid>()))
+            .ThrowsAsync(new CO.CDP.Organisation.WebApiClient.ApiException("message", 404, "response", null, null));
+
+        var result = await _changeUserRoleModel.OnGetPersonInvite();
+
+        var redirectResult = Assert.IsType<RedirectResult>(result);
+
+        Assert.Equal("/page-not-found", redirectResult.Url);
+    }
+
+    [Fact]
+    public async Task OnPostPersonInvite_ShouldReturnPageResult_WhenModelStateIsInvalid()
+    {
+        var mockPersonInvite = new PersonInviteModel(
+                                    "a@b.com",
+                                    "John",
+                                    new Guid(),
+                                    "Smith",
+                                    new List<string> { PersonScopes.Admin, PersonScopes.Editor }
+                                );
+
+        _mockOrganisationClient
+            .Setup(s => s.GetOrganisationPersonInvitesAsync(mockPersonInvite.Id))
+            .ReturnsAsync(new List<PersonInviteModel>() { mockPersonInvite });
+
+        _changeUserRoleModel.IsAdmin = true;
+        _changeUserRoleModel.Role = null;
+
+        _changeUserRoleModel.ModelState.AddModelError("Role", "Required");       
+
+        var result = await _changeUserRoleModel.OnPostPersonInvite();
+        Assert.IsType<PageResult>(result);
+    }
+
+    [Fact]
+    public async Task OnPostPersonInvite_RedirectsToNotFound_WhenPersonInviteDoesntExist()
+    {
+        _mockOrganisationClient
+            .Setup(s => s.GetOrganisationPersonInvitesAsync(It.IsAny<Guid>()))
+            .ThrowsAsync(new CO.CDP.Organisation.WebApiClient.ApiException("message", 404, "response", null, null));
+
+        var result = await _changeUserRoleModel.OnPostPersonInvite();
+
+        var redirectResult = Assert.IsType<RedirectResult>(result);
+
+        Assert.Equal("/page-not-found", redirectResult.Url);
+    }
+
+    [Fact]
+    public async Task OnPostPersonInvite_ShouldUpdatePersonInviteTableAndRedirect_WhenModelStateIsValid()
+    {
+        var mockPersonInvite = new PersonInviteModel(
+                                    "a@b.com",
+                                    "John",
+                                    new Guid(),
+                                    "Smith",
+                                    new List<string> { PersonScopes.Admin, PersonScopes.Editor }
+                                );
+
+        _mockOrganisationClient
+            .Setup(s => s.GetOrganisationPersonInvitesAsync(mockPersonInvite.Id))
+            .ReturnsAsync(new List<PersonInviteModel>() { mockPersonInvite });
+
+        _changeUserRoleModel.IsAdmin = true;
+        _changeUserRoleModel.Role = PersonScopes.Viewer;
+
+        var result = await _changeUserRoleModel.OnPostPersonInvite();
+
+        var redirectResult = Assert.IsType<RedirectToPageResult>(result);
+        Assert.Equal("UserSummary", redirectResult.PageName);
+
+        _mockOrganisationClient.Verify(s => s.UpdatePersonInviteAsync(
+                                                It.IsAny<Guid>(),
+                                                It.IsAny<Guid>(),
+                                                It.Is<UpdateInvitedPersonToOrganisation>(u =>
+                                                    u.Scopes.Contains(PersonScopes.Viewer) &&
+                                                    u.Scopes.Contains(PersonScopes.Admin) &&
+                                                    u.Scopes.Count == 2
+                                                )
+                                            ),
+                                            Times.Once
+                                        );
+    }
+}
--- Frontend/CO.CDP.OrganisationApp.Tests/Pages/Users/ChangePersonUserRoleTests.cs
+++ Frontend/CO.CDP.OrganisationApp.Tests/Pages/Users/ChangePersonUserRoleTests.cs
@@ -9,13 +9,13 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CO.CDP.OrganisationApp.Tests.Pages.Users;
 
-public class ChangeUserRoleTests
+public class ChangePersonUserRoleTests
 {
     private readonly Mock<ISession> _mockSession;
     private readonly Mock<IOrganisationClient> _mockOrganisationClient;
     private readonly ChangeUserRoleModel _changeUserRoleModel;
 
-    public ChangeUserRoleTests()
+    public ChangePersonUserRoleTests()
     {
         _mockSession = new Mock<ISession>();
         _mockSession.Setup(s => s.Get<UserDetails>(Session.UserDetailsKey))
@@ -25,118 +25,6 @@ public class ChangeUserRoleTests
         _changeUserRoleModel = new ChangeUserRoleModel(_mockOrganisationClient.Object, _mockSession.Object);
     }
 
-    // Person invite tests first, followed by person specific tests
-    [Fact]
-    public async Task OnGet_ShouldInitializeModel_WhenPersonInviteExists()
-    {
-        var mockPersonInvite = new PersonInviteModel(
-                                    "a@b.com",
-                                    "John",
-                                    new Guid(),
-                                    "Smith",
-                                    new List<string> { PersonScopes.Admin, PersonScopes.Editor }
-                                );
-
-        _mockOrganisationClient
-            .Setup(s => s.GetOrganisationPersonInvitesAsync(mockPersonInvite.Id))
-            .ReturnsAsync(new List<PersonInviteModel>() { mockPersonInvite });
-
-        var result = await _changeUserRoleModel.OnGetPersonInvite();
-
-        Assert.Equal("John Smith", _changeUserRoleModel.UserFullName);
-        Assert.True(_changeUserRoleModel.IsAdmin);
-        Assert.Equal(PersonScopes.Editor, _changeUserRoleModel.Role);
-    }
-
-    [Fact]
-    public async Task OnGet_RedirectsToNotFound_WhenPersonInviteDoesntExist()
-    {
-        _mockOrganisationClient
-            .Setup(s => s.GetOrganisationPersonInvitesAsync(It.IsAny<Guid>()))
-            .ThrowsAsync(new CO.CDP.Organisation.WebApiClient.ApiException("message", 404, "response", null, null));
-
-        var result = await _changeUserRoleModel.OnGetPersonInvite();
-
-        var redirectResult = Assert.IsType<RedirectResult>(result);
-
-        Assert.Equal("/page-not-found", redirectResult.Url);
-    }
-
-    [Fact]
-    public async Task OnPostPersonInvite_ShouldReturnPageResult_WhenModelStateIsInvalid()
-    {
-        var mockPersonInvite = new PersonInviteModel(
-                                    "a@b.com",
-                                    "John",
-                                    new Guid(),
-                                    "Smith",
-                                    new List<string> { PersonScopes.Admin, PersonScopes.Editor }
-                                );
-
-        _mockOrganisationClient
-            .Setup(s => s.GetOrganisationPersonInvitesAsync(mockPersonInvite.Id))
-            .ReturnsAsync(new List<PersonInviteModel>() { mockPersonInvite });
-
-        _changeUserRoleModel.IsAdmin = true;
-        _changeUserRoleModel.Role = null;
-
-        _changeUserRoleModel.ModelState.AddModelError("Role", "Required");       
-
-        var result = await _changeUserRoleModel.OnPostPersonInvite();
-        Assert.IsType<PageResult>(result);
-    }
-
-    [Fact]
-    public async Task OnPostPersonInvite_RedirectsToNotFound_WhenPersonInviteDoesntExist()
-    {
-        _mockOrganisationClient
-            .Setup(s => s.GetOrganisationPersonInvitesAsync(It.IsAny<Guid>()))
-            .ThrowsAsync(new CO.CDP.Organisation.WebApiClient.ApiException("message", 404, "response", null, null));
-
-        var result = await _changeUserRoleModel.OnPostPersonInvite();
-
-        var redirectResult = Assert.IsType<RedirectResult>(result);
-
-        Assert.Equal("/page-not-found", redirectResult.Url);
-    }
-
-    [Fact]
-    public async Task OnPostPersonInvite_ShouldUpdatePersonInviteTableAndRedirect_WhenModelStateIsValid()
-    {
-        var mockPersonInvite = new PersonInviteModel(
-                                    "a@b.com",
-                                    "John",
-                                    new Guid(),
-                                    "Smith",
-                                    new List<string> { PersonScopes.Admin, PersonScopes.Editor }
-                                );
-
-        _mockOrganisationClient
-            .Setup(s => s.GetOrganisationPersonInvitesAsync(mockPersonInvite.Id))
-            .ReturnsAsync(new List<PersonInviteModel>() { mockPersonInvite });
-
-        _changeUserRoleModel.IsAdmin = true;
-        _changeUserRoleModel.Role = PersonScopes.Viewer;
-
-        var result = await _changeUserRoleModel.OnPostPersonInvite();
-
-        var redirectResult = Assert.IsType<RedirectToPageResult>(result);
-        Assert.Equal("UserSummary", redirectResult.PageName);
-
-        _mockOrganisationClient.Verify(s => s.UpdatePersonInviteAsync(
-                                                It.IsAny<Guid>(),
-                                                It.IsAny<Guid>(),
-                                                It.Is<UpdateInvitedPersonToOrganisation>(u =>
-                                                    u.Scopes.Contains(PersonScopes.Viewer) &&
-                                                    u.Scopes.Contains(PersonScopes.Admin) &&
-                                                    u.Scopes.Count == 2
-                                                )
-                                            ),
-                                            Times.Once
-                                        );
-    }
-
-    // Person specific tests below
     [Fact]
     public async Task OnGet_ShouldInitializeModel_WhenPersonExists()
     {
