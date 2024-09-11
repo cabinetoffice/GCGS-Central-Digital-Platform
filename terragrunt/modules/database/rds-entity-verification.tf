f010199965e2e9dd60947679d3bb28fd3e1d12a3
Ali Bahman <abn@webit4.me>
2024-09-11 15:25:33 +0100

    DP-524 Lift the is_production flag to main config
      - Enable diagnostics in integration

--- terragrunt/modules/database/rds-entity-verification.tf
+++ terragrunt/modules/database/rds-entity-verification.tf
@@ -1,17 +1,17 @@
 module "rds_entity_verification" {
   source = "../rds"
 
-  create_read_replica          = local.is_production
-  backup_retention_period      = local.is_production ? 14 : 0
+  create_read_replica          = var.is_production
+  backup_retention_period      = var.is_production ? 14 : 0
   db_name                      = "${local.name_prefix}-entity-verification"
   db_postgres_sg_id            = var.db_postgres_sg_id
-  deletion_protection          = local.is_production
+  deletion_protection          = var.is_production
   environment                  = var.environment
-  max_allocated_storage        = local.is_production ? 50 : 0
-  monitoring_interval          = local.is_production ? 30 : 0
-  monitoring_role_arn          = local.is_production ? var.role_rds_cloudwatch_arn : ""
-  multi_az                     = local.is_production
-  performance_insights_enabled = local.is_production
+  max_allocated_storage        = var.is_production ? 50 : 0
+  monitoring_interval          = var.is_production ? 30 : 0
+  monitoring_role_arn          = var.is_production ? var.role_rds_cloudwatch_arn : ""
+  multi_az                     = var.is_production
+  performance_insights_enabled = var.is_production
   postgres_engine_version      = var.postgres_engine_version
   postgres_instance_type       = var.postgres_instance_type
   private_subnet_ids           = var.private_subnet_ids
