1b5c89551d6c84d2d07daf851039597b44bdf450
Jakub Zalas <jakub@zalas.pl>
2024-09-19 14:55:18 +0100

    Add logging to the outbox processor

--- Libraries/CO.CDP.MQ.Tests/Outbox/OutboxProcessorTest.cs
+++ Libraries/CO.CDP.MQ.Tests/Outbox/OutboxProcessorTest.cs
@@ -1,53 +1,52 @@
 using CO.CDP.MQ.Outbox;
+using Microsoft.Extensions.Logging;
 using Moq;
 
 namespace CO.CDP.MQ.Tests.Outbox;
 
 public class OutboxProcessorTest
 {
+    private readonly Mock<IPublisher> _publisher = new();
+    private readonly Mock<IOutboxMessageRepository> _outbox = new();
+    private readonly ILogger<OutboxProcessor> _logger = LoggerFactory.Create(_ => { }).CreateLogger<OutboxProcessor>();
+
     [Fact]
     public async Task ItPublishesOutboxMessages()
     {
-        var publisher = new Mock<IPublisher>();
-        var outbox = new Mock<IOutboxMessageRepository>();
-
         var messages = new List<OutboxMessage>
         {
             GivenOutboxMessage(type: "Greeting", message: "{\"Message\":\"Hello World\"}", published: false),
             GivenOutboxMessage(type: "Greeting", message: "{\"Message\":\"Good bye\"}", published: false)
         };
 
-        outbox.Setup(m => m.FindOldest(10)).ReturnsAsync(messages);
+        _outbox.Setup(m => m.FindOldest(10)).ReturnsAsync(messages);
 
-        var processor = new OutboxProcessor(publisher.Object, outbox.Object);
+        var processor = new OutboxProcessor(_publisher.Object, _outbox.Object, _logger);
 
         await processor.ExecuteAsync(count: 10);
 
-        publisher.Verify(p => p.Publish(messages.ElementAt(0)), Times.Once);
-        publisher.Verify(p => p.Publish(messages.ElementAt(1)), Times.Once);
+        _publisher.Verify(p => p.Publish(messages.ElementAt(0)), Times.Once);
+        _publisher.Verify(p => p.Publish(messages.ElementAt(1)), Times.Once);
     }
 
     [Fact]
     public async Task ItMarksPublishedMessagesAsPublished()
     {
-        var publisher = new Mock<IPublisher>();
-        var outbox = new Mock<IOutboxMessageRepository>();
-
         var messages = new List<OutboxMessage>
         {
             GivenOutboxMessage(id: 1, message: "{\"Message\":\"Hello World\"}", published: false),
             GivenOutboxMessage(id: 2, message: "{\"Message\":\"Good bye\"}", published: false)
         };
 
-        outbox.Setup(m => m.FindOldest(10)).ReturnsAsync(messages);
+        _outbox.Setup(m => m.FindOldest(10)).ReturnsAsync(messages);
 
-        var processor = new OutboxProcessor(publisher.Object, outbox.Object);
+        var processor = new OutboxProcessor(_publisher.Object, _outbox.Object, _logger);
 
         await processor.ExecuteAsync(count: 10);
 
-        outbox.Verify(o => o.SaveAsync(It.Is<OutboxMessage>(
+        _outbox.Verify(o => o.SaveAsync(It.Is<OutboxMessage>(
             m => m.Id == 1 && m.Published == true)), Times.Once);
-        outbox.Verify(o => o.SaveAsync(It.Is<OutboxMessage>(
+        _outbox.Verify(o => o.SaveAsync(It.Is<OutboxMessage>(
             m => m.Id == 2 && m.Published == true)), Times.Once);
     }
 
--- Libraries/CO.CDP.MQ/Outbox/OutboxProcessor.cs
+++ Libraries/CO.CDP.MQ/Outbox/OutboxProcessor.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.Logging;
+
 namespace CO.CDP.MQ.Outbox;
 
 public interface IOutboxProcessor
@@ -5,17 +7,29 @@ public interface IOutboxProcessor
     Task ExecuteAsync(int count);
 }
 
-public class OutboxProcessor(IPublisher publisher, IOutboxMessageRepository outbox) : IOutboxProcessor
+public class OutboxProcessor(IPublisher publisher, IOutboxMessageRepository outbox, ILogger<OutboxProcessor> logger)
+    : IOutboxProcessor
 {
     public async Task ExecuteAsync(int count)
     {
-        var messages = await outbox.FindOldest(count);
+        logger.LogDebug("Executing the outbox processor");
+        var messages = await FetchMessages(count);
         messages.ForEach(PublishMessages);
+    }
+
+    private async Task<List<OutboxMessage>> FetchMessages(int count)
+    {
+        var messages = await outbox.FindOldest(count);
+        logger.LogDebug("Fetched `{COUNT}` messages", messages.Count);
+        return messages;
     }
 
     private async void PublishMessages(OutboxMessage m)
     {
+        logger.LogDebug("Publishing the `{TYPE}` message: `{MESSAGE}`", m.Type, m.Message);
         await publisher.Publish(m);
+
+        logger.LogDebug("Marking the message as published");
         m.Published = true;
         await outbox.SaveAsync(m);
     }
