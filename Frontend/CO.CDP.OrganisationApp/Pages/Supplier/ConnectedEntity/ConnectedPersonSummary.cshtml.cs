7a200a5cd70eb4c9ff46a8dc9297933db3c1cb04
Andy Mantell <134642+andymantell@users.noreply.github.com>
2024-09-19 09:34:12 +0100

    Viewer only permissions for connected persons section

--- Frontend/CO.CDP.OrganisationApp/Pages/Supplier/ConnectedEntity/ConnectedPersonSummary.cshtml.cs
+++ Frontend/CO.CDP.OrganisationApp/Pages/Supplier/ConnectedEntity/ConnectedPersonSummary.cshtml.cs
@@ -8,7 +8,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace CO.CDP.OrganisationApp.Pages.Supplier;
 
-[Authorize(Policy = OrgScopeRequirement.Editor)]
+[Authorize(Policy = OrgScopeRequirement.Viewer)]
 public class ConnectedPersonSummaryModel(
     IOrganisationClient organisationClient,
     ISession session) : PageModel
