ff5b8559b367ec33b761df88d02fbebe5f6d9230
maciej-goaco <maciej.gontarz@goaco.com>
2024-09-19 16:01:51 +0100

    CDP-612 - Migration updating identifiers table added

---
+++ Services/CO.CDP.OrganisationInformation.Persistence/Migrations/20240919141551_UpdatePponSchemeName.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CO.CDP.OrganisationInformation.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatePponSchemeName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+			migrationBuilder.Sql("UPDATE identifiers SET scheme = 'GB-PPON' WHERE scheme = 'CDP-PPON';");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
