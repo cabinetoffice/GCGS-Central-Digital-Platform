05bea98d99db20637367cb57f313603e8f225e5e
Richard Mohammed <richard.mohammed@goaco.com>
2024-09-23 17:21:37 +0100

    More tidy up.

--- Services/CO.CDP.Organisation.WebApi/UseCase/GetOrganisationsUseCase.cs
+++ Services/CO.CDP.Organisation.WebApi/UseCase/GetOrganisationsUseCase.cs
@@ -7,12 +7,6 @@ namespace CO.CDP.Organisation.WebApi.UseCase;
 public class GetOrganisationsUseCase(IOrganisationRepository organisationRepository, IMapper mapper)
     : IUseCase<PaginatedOrganisationQuery, IEnumerable<OrganisationExtended>>
 {
-    // public async Task<IEnumerable<Model.Organisation>> Execute(string userUrn)
-    // {
-    //     return await organisationRepository.FindByUserUrn(userUrn)
-    //         .AndThen(mapper.Map<IEnumerable<Model.Organisation>>);
-    // }
-
     public async Task<IEnumerable<OrganisationExtended>> Execute(PaginatedOrganisationQuery command)
     {
         return await organisationRepository.Get(command.Type)
