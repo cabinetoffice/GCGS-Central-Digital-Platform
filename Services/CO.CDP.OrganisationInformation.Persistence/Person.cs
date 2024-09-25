809fc01c87e63d82dba77082cccb33a1147a2e1f
Richard Mohammed <richard.mohammed@goaco.com>
2024-09-25 11:41:00 +0100

    Remove required.

--- Services/CO.CDP.OrganisationInformation.Persistence/Person.cs
+++ Services/CO.CDP.OrganisationInformation.Persistence/Person.cs
@@ -17,8 +17,7 @@ public class Person : IEntityDate
     public List<Tenant> Tenants { get; } = [];
     public List<Organisation> Organisations { get; } = [];
     public List<OrganisationPerson> PersonOrganisations { get; init; } = [];
-
-    public required List<string> Scopes { get; set; } = [];
+    public List<string> Scopes { get; set; } = [];
     public DateTimeOffset CreatedOn { get; set; }
     public DateTimeOffset UpdatedOn { get; set; }
 }
