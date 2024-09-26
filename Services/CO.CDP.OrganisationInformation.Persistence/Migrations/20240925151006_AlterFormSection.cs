97a885428194551305cb02e5691835e8533c9c5f
Dharm <dharmendraverma82@gmail.com>
2024-09-26 10:49:06 +0100

    Option to add Qualification sql script

--- Services/CO.CDP.OrganisationInformation.Persistence/Migrations/20240925151006_AlterFormSection.cs
+++ Services/CO.CDP.OrganisationInformation.Persistence/Migrations/20240925151006_AlterFormSection.cs
@@ -21,8 +21,16 @@ namespace CO.CDP.OrganisationInformation.Persistence.Migrations
                 UPDATE form_sections
                 SET
                     check_further_questions_exempted = TRUE,
-                    configuration = '{{""AddAnotherAnswerLabel"": ""Add another exclusion?"", ""SingularSummaryHeading"": ""You have added 1 exclusions"", ""RemoveConfirmationCaption"": ""Exclusions"", ""RemoveConfirmationHeading"": ""Are you sure you want to remove this exclusion?"", ""PluralSummaryHeadingFormat"": ""You have added {{0}} exclusions"", ""FurtherQuestionsExemptedHeading"": ""<h1 class=''govuk-heading-l''>Do you have any exclusions to add for your organisation or a connected person?</h1>""}}'
+                    configuration = '{{""AddAnotherAnswerLabel"": ""Add another exclusion?"", ""SingularSummaryHeading"": ""You have added 1 exclusion"", ""RemoveConfirmationCaption"": ""Exclusions"", ""RemoveConfirmationHeading"": ""Are you sure you want to remove this exclusion?"", ""PluralSummaryHeadingFormat"": ""You have added {{0}} exclusions"", ""FurtherQuestionsExemptedHeading"": ""<legend class=''govuk-fieldset__legend govuk-fieldset__legend--l''><h1 class=''govuk-fieldset__heading''>Do you have any exclusions to add for your organisation or a connected person?</h1></legend>""}}'
                 WHERE guid = '8a75cb04-fe29-45ae-90f9-168832dbea48';
+            ");
+
+            migrationBuilder.Sql($@"
+                UPDATE form_sections
+                SET
+                    check_further_questions_exempted = TRUE,
+                    configuration = '{{""AddAnotherAnswerLabel"": ""Add another qualification?"", ""SingularSummaryHeading"": ""You have added 1 qualification"", ""RemoveConfirmationCaption"": ""Qualifications"", ""RemoveConfirmationHeading"": ""Are you sure you want to remove this qualification?"", ""PluralSummaryHeadingFormat"": ""You have added {{0}} qualifications"", ""FurtherQuestionsExemptedHeading"": ""<legend class=''govuk-fieldset__legend govuk-fieldset__legend--l''><h1 class=''govuk-fieldset__heading''>Do you want to add any relevant qualifications?</h1></legend><div class=''govuk-hint''>These are general qualifications for business and trade, not procurement specific. Adding them will provide evidence of your suitability to contracting authorities and support any bids you submit. For example, ISO certification.</div>""}}'
+                WHERE guid = '798cf1c1-40be-4e49-9adb-252219d5599d';
             ");
         }
 
