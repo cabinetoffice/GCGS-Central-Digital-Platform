b8d71d8a672aba3bc24e500f9783179eb6a79d86
Richard Mohammed <richard.mohammed@goaco.com>
2024-09-26 16:49:33 +0100

    Migration for buyer organisation review fields.

---
+++ Services/CO.CDP.OrganisationInformation.Persistence/Migrations/20240926154554_ChangeApprovedToReviewed.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CO.CDP.OrganisationInformation.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeApprovedToReviewed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_organisations_persons_approved_by_id",
+                table: "organisations");
+
+            migrationBuilder.DropColumn(
+                name: "approved_comment",
+                table: "organisations");
+
+            migrationBuilder.RenameColumn(
+                name: "approved_by_id",
+                table: "organisations",
+                newName: "reviewed_by_id");
+
+            migrationBuilder.RenameIndex(
+                name: "ix_organisations_approved_by_id",
+                table: "organisations",
+                newName: "ix_organisations_reviewed_by_id");
+
+            migrationBuilder.AddColumn<string>(
+                name: "review_comment",
+                table: "organisations",
+                type: "character varying(10000)",
+                maxLength: 10000,
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_organisations_persons_reviewed_by_id",
+                table: "organisations",
+                column: "reviewed_by_id",
+                principalTable: "persons",
+                principalColumn: "id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_organisations_persons_reviewed_by_id",
+                table: "organisations");
+
+            migrationBuilder.DropColumn(
+                name: "review_comment",
+                table: "organisations");
+
+            migrationBuilder.RenameColumn(
+                name: "reviewed_by_id",
+                table: "organisations",
+                newName: "approved_by_id");
+
+            migrationBuilder.RenameIndex(
+                name: "ix_organisations_reviewed_by_id",
+                table: "organisations",
+                newName: "ix_organisations_approved_by_id");
+
+            migrationBuilder.AddColumn<string>(
+                name: "approved_comment",
+                table: "organisations",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_organisations_persons_approved_by_id",
+                table: "organisations",
+                column: "approved_by_id",
+                principalTable: "persons",
+                principalColumn: "id");
+        }
+    }
+}
