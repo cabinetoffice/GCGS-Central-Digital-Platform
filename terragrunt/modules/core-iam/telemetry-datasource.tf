432044fd0f3b24372479d4a7534f98e64e057e4a
Ali Bahman <abn@webit4.me>
2024-09-30 12:23:37 +0100

    DP-658 Allow Grafana describe all regions (#676)
    - Improve its dashboard

--- terragrunt/modules/core-iam/telemetry-datasource.tf
+++ terragrunt/modules/core-iam/telemetry-datasource.tf
@@ -11,3 +11,25 @@ data "aws_iam_policy_document" "telemetry_assume" {
 data "aws_iam_policy" "cloudwatch_read_only_access" {
   arn = "arn:aws:iam::aws:policy/CloudWatchReadOnlyAccess"
 }
+
+resource "aws_iam_policy" "grafana_generic" {
+    name   = "${local.name_prefix}-grafana-generic"
+    policy = data.aws_iam_policy_document.grafana_generic.json
+    tags   = var.tags
+}
+
+resource "aws_iam_role_policy_attachment" "grafana_generic" {
+    policy_arn = aws_iam_policy.grafana_generic.arn
+    role       = aws_iam_role.telemetry.name
+}
+
+data "aws_iam_policy_document" "grafana_generic" {
+    statement {
+        sid    = "AllowTelemetryReadAccess"
+        effect = "Allow"
+        actions = [
+            "ec2:DescribeRegions",
+        ]
+        resources = [ "*" ]
+    }
+}
