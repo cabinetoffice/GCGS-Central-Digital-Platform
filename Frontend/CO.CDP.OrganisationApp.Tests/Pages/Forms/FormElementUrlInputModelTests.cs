6372d3a5f91b35b1cfe42d4dcf93caa794251a2a
Dharm <dharmendraverma82@gmail.com>
2024-10-04 13:55:26 +0100

    Update Unit test

--- Frontend/CO.CDP.OrganisationApp.Tests/Pages/Forms/FormElementUrlInputModelTests.cs
+++ Frontend/CO.CDP.OrganisationApp.Tests/Pages/Forms/FormElementUrlInputModelTests.cs
@@ -13,7 +13,7 @@ public class FormElementUrlInputModelTests
         var model = new FormElementUrlInputModel
         {
             IsRequired = false,
-            HasValue = false
+            HasValue = null
         };
 
         var result = model.GetAnswer();
@@ -41,7 +41,7 @@ public class FormElementUrlInputModelTests
     public void SetAnswer_ShouldSetTextInputAndHasValue_WhenAnswerIsProvided()
     {
         var model = new FormElementUrlInputModel();
-        var answer = new FormAnswer { TextValue = "https://example.com" };
+        var answer = new FormAnswer { TextValue = "https://example.com", BoolValue = true };
 
         model.SetAnswer(answer);
 
