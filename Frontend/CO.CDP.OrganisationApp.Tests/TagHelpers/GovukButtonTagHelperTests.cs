d8a667361e86840a9f7e4bb203eb44f96ae49459
Jakub Zalas <jakub@zalas.pl>
2024-10-08 10:25:18 +0100

    Extract the CallTagHelper function

--- Frontend/CO.CDP.OrganisationApp.Tests/TagHelpers/GovukButtonTagHelperTests.cs
+++ Frontend/CO.CDP.OrganisationApp.Tests/TagHelpers/GovukButtonTagHelperTests.cs
@@ -2,97 +2,70 @@ using CO.CDP.OrganisationApp.TagHelpers;
 using FluentAssertions;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 using System.Text.Encodings.Web;
+using static CO.CDP.OrganisationApp.Tests.TagHelpers.TagHelperTestKit;
 
 namespace CO.CDP.OrganisationApp.Tests.TagHelpers;
+
 public class GovukButtonTagHelperTests
 {
     [Fact]
     public void GovukButtonTagHelper_ShouldRenderButtonWithDefaults()
     {
-        var attributes = new TagHelperAttributeList();
+        var result = CallTagHelper("govuk-button", "Click me", [], new GovukButtonTagHelper());
 
-        var result = callTagHelper("Click me", attributes);
-
-        result.Should().Be("<button type=\"submit\" data-module=\"govuk-button\" class=\"govuk-button\" data-prevent-double-click=\"true\">Click me</button>");
+        result.Should()
+            .Be(
+                "<button type=\"submit\" data-module=\"govuk-button\" class=\"govuk-button\" data-prevent-double-click=\"true\">Click me</button>");
     }
 
     [Fact]
     public void GovukButtonTagHelper_ShouldRenderButton_WhenCustomClassIsSet()
     {
-        var attributes = new TagHelperAttributeList();
+        var result = CallTagHelper("govuk-button", "Click me", [], new GovukButtonTagHelper
+        {
+            Class = "custom-class"
+        });
 
-        var result = callTagHelper("Click me", attributes, @class: "custom-class");
-
-        result.Should().Be("<button type=\"submit\" data-module=\"govuk-button\" class=\"govuk-button custom-class\" data-prevent-double-click=\"true\">Click me</button>");
+        result.Should()
+            .Be(
+                "<button type=\"submit\" data-module=\"govuk-button\" class=\"govuk-button custom-class\" data-prevent-double-click=\"true\">Click me</button>");
     }
 
     [Fact]
     public void GovukButtonTagHelper_ShouldRenderButton_WhenMultipleCustomClassesAreSet()
     {
-        var attributes = new TagHelperAttributeList();
+        var result = CallTagHelper("govuk-button", "Click me", [], new GovukButtonTagHelper
+        {
+            Class = "custom-class another-class many-classes"
+        });
 
-        var result = callTagHelper("Click me", attributes, @class: "custom-class another-class many-classes");
-
-        result.Should().Be("<button type=\"submit\" data-module=\"govuk-button\" class=\"govuk-button custom-class another-class many-classes\" data-prevent-double-click=\"true\">Click me</button>");
+        result.Should()
+            .Be(
+                "<button type=\"submit\" data-module=\"govuk-button\" class=\"govuk-button custom-class another-class many-classes\" data-prevent-double-click=\"true\">Click me</button>");
     }
 
     [Fact]
     public void GovukButtonTagHelper_ShouldRenderButton_WhenTypeIsSet()
     {
-        var attributes = new TagHelperAttributeList();
+        var result = CallTagHelper("govuk-button", "Click me", [], new GovukButtonTagHelper
+        {
+            Type = "button"
+        });
 
-        var result = callTagHelper("Click me", attributes, type: "button");
-
-        result.Should().Be("<button type=\"button\" data-module=\"govuk-button\" class=\"govuk-button\" data-prevent-double-click=\"true\">Click me</button>");
+        result.Should()
+            .Be(
+                "<button type=\"button\" data-module=\"govuk-button\" class=\"govuk-button\" data-prevent-double-click=\"true\">Click me</button>");
     }
 
     [Fact]
     public void GovukButtonTagHelper_ShouldRenderButton_WhenDoubleClickIsSetToFalse()
     {
-        var attributes = new TagHelperAttributeList();
-
-        var result = callTagHelper("Click me", attributes, preventDoubleClick: false);
-
-        result.Should().Be("<button type=\"submit\" data-module=\"govuk-button\" class=\"govuk-button\">Click me</button>");
-    }
-
-    public string callTagHelper(string htmlContent, TagHelperAttributeList attributes, string? @class = null, string? type = null, bool? preventDoubleClick = null)
-    {
-        var tagHelper = new GovukButtonTagHelper();
-
-        if (!string.IsNullOrEmpty(@class))
+        var result = CallTagHelper("govuk-button", "Click me", [], new GovukButtonTagHelper
         {
-            tagHelper.Class = @class;
-        }
+            PreventDoubleClick = false
+        });
 
-        if (!string.IsNullOrEmpty(type))
-        {
-            tagHelper.Type = type;
-        }
-
-        if (preventDoubleClick != null)
-        {
-            tagHelper.PreventDoubleClick = (bool)preventDoubleClick;
-        }
-
-        var tagHelperContext = new TagHelperContext(
-                        attributes,
-                        new Dictionary<object, object>(),
-                        Guid.NewGuid().ToString("N"));
-
-        var tagHelperOutput = new TagHelperOutput("govuk-button",
-            attributes,
-            (useCachedResult, encoder) =>
-            {
-                var tagHelperContent = new DefaultTagHelperContent();
-                tagHelperContent.SetHtmlContent(htmlContent);
-                return Task.FromResult<TagHelperContent>(tagHelperContent);
-            });
-        tagHelper.Process(tagHelperContext, tagHelperOutput);
-
-        var writer = new System.IO.StringWriter();
-        tagHelperOutput.WriteTo(writer, HtmlEncoder.Default);
-
-        return writer.ToString();
+        result.Should()
+            .Be("<button type=\"submit\" data-module=\"govuk-button\" class=\"govuk-button\">Click me</button>");
     }
 }
