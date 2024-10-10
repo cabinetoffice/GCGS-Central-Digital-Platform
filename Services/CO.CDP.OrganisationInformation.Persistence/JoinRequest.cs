6c4ee2fcc3a4954cd93572fa34e35523deb3ac58
Richard Mohammed <richard.mohammed@goaco.com>
2024-10-10 14:22:06 +0100

    Remove optional org and person id.

--- Services/CO.CDP.OrganisationInformation.Persistence/JoinRequest.cs
+++ Services/CO.CDP.OrganisationInformation.Persistence/JoinRequest.cs
@@ -9,9 +9,9 @@ public class JoinRequest : IEntityDate
     public int Id { get; set; }
     public required Guid Guid { get; set; }
     public Organisation? Organisation { get; set; }
-    public int? OrganisationId { get; set; }
+    public int OrganisationId { get; set; }
     public Person? Person { get; set; }
-    public int? PersonId { get; set; }
+    public int PersonId { get; set; }
     public required JoinRequestStatus Status { get; set; }
     public DateTimeOffset? ReviewedOn { get; set; }
     public Person? ReviewedBy { get; set; }
