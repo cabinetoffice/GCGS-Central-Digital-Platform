a7c5cbc179581d36d70d235dc767b20b2877bf21
Richard Mohammed <richard.mohammed@goaco.com>
2024-10-16 09:47:29 +0100

    Change string org request type to an enum.

--- Services/CO.CDP.Organisation.WebApi/Model/OrganisationJoinRequest.cs
+++ Services/CO.CDP.Organisation.WebApi/Model/OrganisationJoinRequest.cs
@@ -1,3 +1,5 @@
+using CO.CDP.OrganisationInformation;
+
 namespace CO.CDP.Organisation.WebApi.Model;
 
 public record OrganisationJoinRequest
@@ -7,5 +9,5 @@ public record OrganisationJoinRequest
     public required Organisation Organisation { get; init; }
     public required Person ReviewedBy { get; init; }
     public required DateTimeOffset ReviewedOn { get; set; }
-    public required string Status { get; set; }
+    public required OrganisationJoinRequestStatus Status { get; set; }
 }
