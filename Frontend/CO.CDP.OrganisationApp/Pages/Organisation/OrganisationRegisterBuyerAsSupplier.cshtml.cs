7d4da9775c1052fa886f2792d0d595944612b5e1
Andy Mantell <134642+andymantell@users.noreply.github.com>
2024-10-22 10:42:26 +0100

    Show a flash message when converting a buyer into a supplier

--- Frontend/CO.CDP.OrganisationApp/Pages/Organisation/OrganisationRegisterBuyerAsSupplier.cshtml.cs
+++ Frontend/CO.CDP.OrganisationApp/Pages/Organisation/OrganisationRegisterBuyerAsSupplier.cshtml.cs
@@ -1,14 +1,19 @@
 using CO.CDP.Organisation.WebApiClient;
 using CO.CDP.OrganisationApp.Constants;
+using CO.CDP.OrganisationApp.Models;
 using CO.CDP.OrganisationApp.WebApiClients;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CO.CDP.OrganisationApp.Pages.Organisation;
 
 [Authorize(Policy = OrgScopeRequirement.Editor)]
-public class OrganisationRegisterBuyerAsSupplierModel(IOrganisationClient organisationClient) : PageModel
+public class OrganisationRegisterBuyerAsSupplierModel(
+    IOrganisationClient organisationClient,
+    ITempDataService tempDataService
+    ) : PageModel
 {
     public async Task<IActionResult> OnGet(Guid id)
     {
@@ -17,8 +22,12 @@ public class OrganisationRegisterBuyerAsSupplierModel(IOrganisationClient organi
         {
             orgInfo.Roles.Add(PartyRole.Tenderer);
             await organisationClient.UpdateOrganisationRoles(id, orgInfo.Roles);
+            tempDataService.Put(FlashMessageTypes.Success, new FlashMessage(
+                "You have been registered as a supplier",
+                $"You'll need to <a href=\"{Url.Page("/Supplier/SupplierInformationSummary", new { id })}\">add supplier information</a> to be able to create sharecode."
+            ));
         }
 
-        return RedirectToPage("./OrganisationOverview", new { id });
+        return RedirectToPage("OrganisationOverview", new { id });
     }
 }
