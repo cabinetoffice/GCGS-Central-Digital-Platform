09dd227f9ffcbc338a6c702e08aa0a63ac1e822c
maciej-goaco <maciej.gontarz@goaco.com>
2024-10-22 13:03:20 +0100

    DP-71 - Migration updating Form Questions sort order added

---
+++ Services/CO.CDP.OrganisationInformation.Persistence/Migrations/20241022113409_UpdateFormQuestionsSortOrder.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CO.CDP.OrganisationInformation.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateFormQuestionsSortOrder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($@"
+                WITH RECURSIVE SectionQuestions AS (
+                    SELECT q.id, q.next_question_id, q.section_id, 1 AS sort_order
+                    FROM form_questions q
+                    WHERE NOT EXISTS (
+                            SELECT 1
+                            FROM form_questions
+                            WHERE next_question_id = q.id
+                            AND section_id = q.section_id
+                        )
+                    UNION ALL
+                      SELECT q.id, q.next_question_id, q.section_id, sq.sort_order + 1
+                      FROM form_questions q
+                      JOIN SectionQuestions sq
+                      ON q.id = sq.next_question_id
+                      AND q.section_id = sq.section_id
+                )
+
+                UPDATE form_questions q
+                SET sort_order = sq.sort_order
+                FROM SectionQuestions sq
+                WHERE q.id = sq.id;
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
