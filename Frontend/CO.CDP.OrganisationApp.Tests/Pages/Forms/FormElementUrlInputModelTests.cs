de5bc26d6da7ab73bab7c26d27057a2a13cd6353
Dharmendra Verma <64859911+dharmverma@users.noreply.github.com>
2024-10-23 16:43:39 +0100

    Fixed - form website input validation (#814)
    * Fixed - form website input validation
    
    * Code review update

--- Frontend/CO.CDP.OrganisationApp.Tests/Pages/Forms/FormElementUrlInputModelTests.cs
+++ Frontend/CO.CDP.OrganisationApp.Tests/Pages/Forms/FormElementUrlInputModelTests.cs
@@ -49,6 +49,7 @@ public class FormElementUrlInputModelTests
     [InlineData(null, null)]
     [InlineData(" ", null)]
     [InlineData("https://example.com", "https://example.com")]
+    [InlineData("https://example.com/withspaceinend ", "https://example.com/withspaceinend")]
     public void GetAnswer_WhenHasValueIsNull_GetsExpectedFormAnswer(string? input, string? expected)
     {
         var model = new FormElementUrlInputModel
@@ -118,14 +119,16 @@ public class FormElementUrlInputModelTests
         validationResults[0].ErrorMessage.Should().Be("Enter a website address");
     }
 
-    [Fact]
-    public void Validate_ShouldReturnError_WhenTextInputIsInvalidUrl()
+    [Theory]
+    [InlineData("invalid-url")]
+    [InlineData("http://example.url/has space")]
+    public void Validate_ShouldReturnError_WhenTextInputIsInvalidUrl(string url)
     {
         var model = new FormElementUrlInputModel
         {
             IsRequired = true,
             HasValue = true,
-            TextInput = "invalid-url"
+            TextInput = url
         };
 
         var validationResults = new List<ValidationResult>();
@@ -133,17 +136,21 @@ public class FormElementUrlInputModelTests
         Validator.TryValidateObject(model, context, validationResults, true);
 
         validationResults.Should().ContainSingle();
-        validationResults[0].ErrorMessage.Should().Be("Enter a valid website address in the correct format");
+        validationResults[0].ErrorMessage.Should().Be("Website address must be in the correct format, like www.companyname.com");
     }
 
-    [Fact]
-    public void Validate_ShouldPass_WhenValidUrlIsProvidedAndIsRequired()
+
+
+    [Theory]
+    [InlineData("https://example.com")]
+    [InlineData("http://example.url/hasspaceinend ")]
+    public void Validate_ShouldPass_WhenValidUrlIsProvidedAndIsRequired(string url)
     {
         var model = new FormElementUrlInputModel
         {
             IsRequired = true,
             HasValue = true,
-            TextInput = "https://example.com"
+            TextInput = url
         };
 
         var validationResults = new List<ValidationResult>();
