a63aef43cbc98ff93e82c1f000ae16a634345117
Ali Bahman <abn@webit4.me>
2024-10-24 22:13:05 +0100

    DP-691 Prepare PGAdmin for Production Deployment
      - Enable PGAdmin in the production environment.
      - Tag and document PGAdmin and Grafana credentials.
      - Provision an RDS instance for storing PGAdmin configurations.
      - Update the PGAdmin task to receive RDS connection details and use versioned images.
      - Grant ECS task execution role permissions to decrypt RDS credentials.
      - Modify Docker image to support PostgreSQL connectivity for PGAdmin.
      - Update documentation to reflect versioning of Docker images in ECR.

--- terragrunt/modules/tools/service-pgadmin.tf
+++ terragrunt/modules/tools/service-pgadmin.tf
@@ -4,24 +4,28 @@ module "ecs_service_pgadmin" {
   container_definitions = templatefile(
     "${path.module}/templates/task-definitions/pgadmin.json.tftpl",
     {
-      account_id                      = data.aws_caller_identity.current.account_id
-      container_port                  = var.pgadmin_config.port
-      cpu                             = var.pgadmin_config.cpu
-      db_entity_verification_address  = var.db_entity_verification_address
-      db_entity_verification_name     = var.db_entity_verification_name
-      db_entity_verification_username = "${var.db_entity_verification_credentials_arn}:username::"
-      db_sirsi_address                = var.db_sirsi_address
-      db_sirsi_name                   = var.db_sirsi_name
-      db_sirsi_username               = "${var.db_sirsi_credentials_arn}:username::"
-      pgadmin_admin_password          = "${aws_secretsmanager_secret.pgadmin_credentials.arn}:ADMIN_PASSWORD::"
-      pgadmin_admin_user              = "${aws_secretsmanager_secret.pgadmin_credentials.arn}:ADMIN_USERNAME::"
-      host_port                       = var.pgadmin_config.port
-      image                           = "${local.orchestrator_account_id}.dkr.ecr.${data.aws_region.current.name}.amazonaws.com/cdp-pgadmin:latest"
-      lg_name                         = aws_cloudwatch_log_group.pgadmin.name
-      lg_prefix                       = "tools"
-      lg_region                       = data.aws_region.current.name
-      memory                          = var.pgadmin_config.memory
-      name                            = var.pgadmin_config.name
+      account_id                       = data.aws_caller_identity.current.account_id
+      container_port                   = var.pgadmin_config.port
+      cpu                              = var.pgadmin_config.cpu
+      db_entity_verification_address   = var.db_entity_verification_address
+      db_entity_verification_name      = var.db_entity_verification_name
+      db_entity_verification_username  = "${var.db_entity_verification_credentials_arn}:username::"
+      db_sirsi_address                 = var.db_sirsi_address
+      db_sirsi_name                    = var.db_sirsi_name
+      db_sirsi_username                = "${var.db_sirsi_credentials_arn}:username::"
+      pgadmin_admin_password           = "${aws_secretsmanager_secret.pgadmin_credentials.arn}:ADMIN_PASSWORD::"
+      pgadmin_admin_user               = "${aws_secretsmanager_secret.pgadmin_credentials.arn}:ADMIN_USERNAME::"
+      pgadmin_config_database_host     = module.rds_pgadmin.db_address
+      pgadmin_config_database_name     = module.rds_pgadmin.db_name
+      pgadmin_config_database_password = "${module.rds_pgadmin.db_credentials_arn}:password::"
+      pgadmin_config_database_username = "${module.rds_pgadmin.db_credentials_arn}:username::"
+      host_port                        = var.pgadmin_config.port
+      image                            = "${local.orchestrator_account_id}.dkr.ecr.${data.aws_region.current.name}.amazonaws.com/cdp-pgadmin:8.12.0"
+      lg_name                          = aws_cloudwatch_log_group.pgadmin.name
+      lg_prefix                        = "tools"
+      lg_region                        = data.aws_region.current.name
+      memory                           = var.pgadmin_config.memory
+      name                             = var.pgadmin_config.name
     }
   )
 
@@ -32,10 +36,10 @@ module "ecs_service_pgadmin" {
   ecs_listener_arn              = var.ecs_listener_arn
   ecs_service_base_sg_id        = var.ecs_sg_id
   family                        = "tools"
-  healthcheck_healthy_threshold = 10
-  healthcheck_interval          = 120
+  healthcheck_healthy_threshold = 5
+  healthcheck_interval          = 60
   healthcheck_path              = "/login"
-  healthcheck_timeout           = 60
+  healthcheck_timeout           = 30
   host_port                     = var.pgadmin_config.port
   memory                        = var.pgadmin_config.memory
   name                          = var.pgadmin_config.name
--- terragrunt/tools/pgadmin/configs/custom-entrypoint.sh
+++ terragrunt/tools/pgadmin/configs/custom-entrypoint.sh
@@ -22,9 +22,31 @@ cat <<EOF > /pgadmin4/servers.json
             "MaintenanceDB": "${DB_ENTITY_VERIFICATION_NAME}",
             "Username": "${DB_ENTITY_VERIFICATION_USERNAME}",
             "SSLMode": "prefer"
+        },
+        "3": {
+            "Name": "PGAdmin",
+            "Group": "Servers",
+            "Host": "${PGADMIN_CONFIG_DATABASE_HOST}",
+            "Port": 5432,
+            "MaintenanceDB": "${PGADMIN_CONFIG_DATABASE_NAME}",
+            "Username": "${PGADMIN_CONFIG_DATABASE_USERNAME}",
+            "SSLMode": "prefer"
         }
     }
 }
 EOF
+
+echo "Bake CONFIG_DATABASE_URI in config_local.py:"
+export CONFIG_DATABASE_URI="postgresql://${PGADMIN_CONFIG_DATABASE_USERNAME}:${PGADMIN_CONFIG_DATABASE_PASSWORD}@${PGADMIN_CONFIG_DATABASE_HOST}:5432/${PGADMIN_CONFIG_DATABASE_NAME}"
+if [ ! -f /pgadmin4/config_local.py ]; then
+    touch /pgadmin4/config_local.py
+fi
+echo "CONFIG_DATABASE_URI = '${CONFIG_DATABASE_URI}'" > /pgadmin4/config_local.py
+
+
+export PGADMIN_CONFIG_DATABASE_PASSWORD="\"$PGADMIN_CONFIG_DATABASE_PASSWORD\""
+export PGADMIN_CONFIG_DATABASE_USERNAME="\"$PGADMIN_CONFIG_DATABASE_USERNAME\""
+export PGADMIN_CONFIG_DATABASE_HOST="\"$PGADMIN_CONFIG_DATABASE_HOST\""
+export PGADMIN_CONFIG_DATABASE_NAME="\"$PGADMIN_CONFIG_DATABASE_NAME\""
 
 /entrypoint.sh "$@"
