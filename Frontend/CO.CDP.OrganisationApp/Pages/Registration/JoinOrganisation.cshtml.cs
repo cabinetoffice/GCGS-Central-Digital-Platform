31b9dcf41d898ff1ebd5913a515dc180ba5e29ae
Richard Mohammed <richard.mohammed@goaco.com>
2024-10-24 15:43:26 +0100

    Add check to see if person is already a member of the organisation.

--- Frontend/CO.CDP.OrganisationApp/Pages/Registration/JoinOrganisation.cshtml.cs
+++ Frontend/CO.CDP.OrganisationApp/Pages/Registration/JoinOrganisation.cshtml.cs
@@ -1,5 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 using CO.CDP.Organisation.WebApiClient;
+using CO.CDP.OrganisationApp.Constants;
+using CO.CDP.OrganisationApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using OrganisationWebApiClient = CO.CDP.Organisation.WebApiClient;
 
@@ -7,13 +9,16 @@ namespace CO.CDP.OrganisationApp.Pages.Registration;
 
 public class JoinOrganisationModel(
     IOrganisationClient organisationClient,
-    ISession session) : LoggedInUserAwareModel(session)
+    ISession session,
+    ITempDataService tempDataService) : LoggedInUserAwareModel(session)
 {
     public OrganisationWebApiClient.Organisation? OrganisationDetails { get; set; }
 
     [BindProperty]
     [Required(ErrorMessage = "Select an option")]
     public bool Join { get; set; }
+
+    public FlashMessage NotificationBannerAlreadyMemberOfOrganisation { get { return new FlashMessage("You are already a member of this organisation. <a class='govuk-notification-banner__link' href='/organisation/" + OrganisationDetails?.Id + "'>View Organisation</a>"); } }
 
     public async Task<IActionResult> OnGet(string identifier)
     {
@@ -41,9 +46,18 @@ public class JoinOrganisationModel(
         {
             if (Join == true)
             {
-                await organisationClient.CreateJoinRequestAsync(OrganisationDetails.Id, new CreateOrganisationJoinRequest(
-                    personId: UserDetails.PersonId.Value
-                ));
+                try
+                {
+                    await organisationClient.CreateJoinRequestAsync(OrganisationDetails.Id,
+                        new CreateOrganisationJoinRequest(
+                            personId: UserDetails.PersonId.Value
+                        ));
+                }
+                catch (ApiException<OrganisationWebApiClient.ProblemDetails> aex)
+                {
+                    tempDataService.Put(FlashMessageTypes.Important, NotificationBannerAlreadyMemberOfOrganisation);
+                    return Page();
+                }
 
                 return Redirect("/registration/" + identifier + "/join-organisation/success");
             }
