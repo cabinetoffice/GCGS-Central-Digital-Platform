5227ba7e65aedeb136e66b53faa471b62382aa81
Ali Bahman <abn@webit4.me>
2024-11-05 16:01:49 +0000

    DP-819 Aurora cluster it is

--- terragrunt/modules/externals/database/rds-fts.tf
+++ terragrunt/modules/externals/database/rds-fts.tf
@@ -1,20 +1,17 @@
 module "rds_fts" {
-  source = "../../rds"
+  source = "../../rds-cluster"
 
-  backup_retention_period      = var.is_production ? 14 : 0
+  backup_retention_period      = var.is_production ? 14 : 1
   copy_tags_to_snapshot        = true
-  create_read_replica          = var.is_production
   db_name                      = local.name_prefix
   db_sg_id                     = var.db_mysql_sg_id
   deletion_protection          = var.is_production
-  engine                       = "aurora-mysql"
-  engine_version               = "8.0.39"
+  engine_version               = "8.0.mysql_aurora.3.07.1"
   family                       = "aurora-mysql8.0"
-  instance_type                = "db.m5.large"
+  instance_type                = "db.r5.large"
   max_allocated_storage        = var.is_production ? 50 : 0
   monitoring_interval          = var.is_production ? 30 : 0
   monitoring_role_arn          = var.is_production ? var.role_rds_cloudwatch_arn : ""
-  multi_az                     = var.is_production
   parameter_group_name         = "${local.name_prefix}-8"
   performance_insights_enabled = var.is_production
   private_subnet_ids           = var.private_subnet_ids
--- terragrunt/modules/externals/database/security-groups.tf
+++ terragrunt/modules/externals/database/security-groups.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group_rule" "ecs_service_from_ecs_alb" {
-  description              = "To ${local.name_prefix} MySQL"
+  description       = "To ${local.name_prefix} MySQL"
   from_port         = 3306
   protocol          = "TCP"
   cidr_blocks       = var.externals_product.mysql_access_allowed_ip_ranges
