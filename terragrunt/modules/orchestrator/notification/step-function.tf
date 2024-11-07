777c721e78ef652a3eae2cc8b5d8f22a093aff93
Ali Bahman <abn@webit4.me>
2024-11-07 09:02:01 +0000

    DP-816 Improve deployment messages (#904)
    * DP-819 Add middleman SF to improve notifications
      - Include Commit Details
      - Switch back to main
      - Remove old notification step-function and co

--- terragrunt/modules/orchestrator/notification/step-function.tf
+++ terragrunt/modules/orchestrator/notification/step-function.tf
@@ -1,12 +1,25 @@
+resource "aws_sfn_state_machine" "slack_notification_middleman" {
+  name     = "${local.name_prefix}-slack-notification-middleman"
+  role_arn = var.role_notification_step_function_arn
+  tags     = var.tags
+
+  definition = templatefile("${path.module}/templates/state-machine/trigger-slack-notification.json.tftpl", {
+    slack_notification_arn = aws_sfn_state_machine.slack_notification.arn
+  })
+}
+
 resource "aws_sfn_state_machine" "slack_notification" {
   name     = "${local.name_prefix}-slack-notification"
   role_arn = var.role_notification_step_function_arn
   tags     = var.tags
 
   definition = templatefile("${path.module}/templates/state-machine/send-slack-notification.json.tftpl", {
-    api_endpoint             = data.aws_secretsmanager_secret_version.slack_api_endpoint.secret_string
-    auth_connection_arn      = aws_cloudwatch_event_connection.slack.arn
-    ssm_service_version_name = var.ssm_service_version_name
+    auth_connection_arn      = aws_cloudwatch_event_connection.slack_unified_notification.arn
     deployment_pipeline_name = var.deployment_pipeline_name
+    dynamodb_table_name      = aws_dynamodb_table.pipeline_execution_timestamps.name
+    slack_channel_id         = var.slack_channel_id //"C07V54GN52L"
+    slack_post_endpoint      = "${local.slack_api_endpoint}/chat.postMessage"
+    slack_update_endpoint    = "${local.slack_api_endpoint}/chat.update"
+    ssm_service_version_name = var.ssm_service_version_name
   })
 }
