42c3daaf700771000726475b78db9bb96fccaaaf
David Baker <david.baker@goaco.com>
2024-11-19 15:28:54 +0000

    New PPON ID is now feature toggled. Defaults to UUID.

--- Services/CO.CDP.EntityVerification.Tests/Ppon/PponServiceTests.cs
+++ Services/CO.CDP.EntityVerification.Tests/Ppon/PponServiceTests.cs
@@ -5,6 +5,17 @@ namespace CO.CDP.EntityVerification.Tests.Ppon;
 public class PponServiceTests
 {
     [Fact]
+    public void GenerateUuidPponId_CreatesNewPponId_ReturnsPponIdWithValidLength()
+    {
+        UuidPponService ppon = new UuidPponService();
+        var idLength = 32;
+
+        string pponId = ppon.GeneratePponId();
+
+        Assert.Equal(idLength, pponId.Length);
+    }
+
+    [Fact]
     public void GeneratePponId_CreatesNewPponId_ReturnsPponIdWithValidLength()
     {
         PponService ppon = new PponService();
