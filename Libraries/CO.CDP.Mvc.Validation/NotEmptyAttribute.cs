3087763c94c35c2367c521a947a065e0d0119753
Andy Mantell <134642+andymantell@users.noreply.github.com>
2024-11-19 09:16:06 +0000

    Extend NotEmptyAttribute to handle localization

--- Libraries/CO.CDP.Mvc.Validation/NotEmptyAttribute.cs
+++ Libraries/CO.CDP.Mvc.Validation/NotEmptyAttribute.cs
@@ -1,12 +1,15 @@
-using CO.CDP.Localization;
 using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using System.Collections;
+using System.Reflection;
+using System.Resources;
 
 namespace CO.CDP.Mvc.Validation;
 
 public class NotEmptyAttribute : Attribute, IModelValidator
 {
-    public required string ErrorMessage { get; set; }
+    public string? ErrorMessage { get; set; }
+    public string? ErrorMessageResourceName { get; set; }
+    public Type? ErrorMessageResourceType { get; set; }
 
     public IEnumerable<ModelValidationResult> Validate(ModelValidationContext context)
     {
@@ -18,7 +21,35 @@ public class NotEmptyAttribute : Attribute, IModelValidator
 
         return new List<ModelValidationResult>
         {
-            new ModelValidationResult(context.ModelMetadata.PropertyName, StaticTextResource.ResourceManager.GetString(ErrorMessage))
+            new ModelValidationResult(context.ModelMetadata.PropertyName, GetErrorMessage())
         };
+    }
+
+    private string? GetErrorMessage()
+    {
+        if(ErrorMessage != null)
+        {
+            return ErrorMessage;
+        }
+
+        if(ErrorMessageResourceName == null)
+        {
+            throw new InvalidOperationException("No Resource specified.");
+        }
+
+        var resourceManagerProperty = ErrorMessageResourceType?.GetProperty("ResourceManager", BindingFlags.Static | BindingFlags.Public);
+        if (resourceManagerProperty == null)
+        {
+            throw new InvalidOperationException($"No ResourceManager found in '{ErrorMessageResourceType?.FullName}'.");
+        }
+
+        var resourceManager = resourceManagerProperty.GetValue(null) as ResourceManager;
+
+        if (resourceManager == null)
+        {
+            throw new InvalidOperationException($"No ResourceManager found in '{ErrorMessageResourceType?.FullName}'.");
+        }
+
+        return resourceManager.GetString(ErrorMessageResourceName);
     }
 }
