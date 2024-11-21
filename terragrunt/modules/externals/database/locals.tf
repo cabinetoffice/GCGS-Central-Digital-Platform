4d9b807870437e2a5f712d165ca48c58024605fd
Ali Bahman <abn@webit4.me>
2024-11-21 10:03:45 +0000

    DP-885 Configure FTS DB cluster's parameter groups (#964)
    No change in the DB version was required

--- terragrunt/modules/externals/database/locals.tf
+++ terragrunt/modules/externals/database/locals.tf
@@ -1,3 +1,12 @@
 locals {
   name_prefix = var.externals_product.resource_name
+
+  db_parameters_cluster = {
+    character_set_database = "latin1"
+    character_set_server = "latin1"
+    collation_server = "latin1_swedish_ci"
+    explicit_defaults_for_timestamp = false
+    sql_mode = "NO_ENGINE_SUBSTITUTION"
+  }
+
 }
--- terragrunt/modules/rds-cluster/rds-cluster.tf
+++ terragrunt/modules/rds-cluster/rds-cluster.tf
@@ -1,17 +1,19 @@
-# Aurora MySQL Module
-
 resource "aws_rds_cluster" "this" {
-  cluster_identifier      = var.db_name
-  engine                  = var.engine
-  engine_version          = var.engine_version
-  database_name           = replace(var.db_name, "-", "_")
-  master_username         = "${replace(var.db_name, "-", "_")}_user"
-  master_password         = jsondecode(aws_secretsmanager_secret_version.master_user_credentials.secret_string)["password"]
-  backup_retention_period = var.backup_retention_period
-  db_subnet_group_name    = aws_db_subnet_group.this.name
-  vpc_security_group_ids  = [var.db_sg_id]
-  deletion_protection     = var.deletion_protection
-  copy_tags_to_snapshot   = var.copy_tags_to_snapshot
+  backup_retention_period          = var.backup_retention_period
+  cluster_identifier               = var.db_name
+  copy_tags_to_snapshot            = var.copy_tags_to_snapshot
+  database_name = replace(var.db_name, "-", "_")
+  db_cluster_parameter_group_name  = aws_rds_cluster_parameter_group.this.name
+  db_instance_parameter_group_name = aws_db_parameter_group.this.name
+  db_subnet_group_name             = aws_db_subnet_group.this.name
+  deletion_protection              = var.deletion_protection
+  engine                           = var.engine
+  engine_version                   = var.engine_version
+  master_password                  = jsondecode(aws_secretsmanager_secret_version.master_user_credentials.secret_string)["password"]
+  master_username                  = "${replace(var.db_name, "-", "_")}_user"
+
+  vpc_security_group_ids = [var.db_sg_id]
+
   tags = merge(
     var.tags,
     {
@@ -33,15 +35,17 @@ resource "aws_db_subnet_group" "this" {
 }
 
 resource "aws_rds_cluster_instance" "this" {
-  count                        = var.instance_count
-  identifier                   = "${var.db_name}-${count.index}"
-  engine                       = var.engine
+  count = var.instance_count
+
   cluster_identifier           = aws_rds_cluster.this.id
+  engine                       = var.engine
+  identifier                   = "${var.db_name}-${count.index}"
   instance_class               = var.instance_type
-  publicly_accessible          = var.publicly_accessible
   monitoring_interval          = var.monitoring_interval
   monitoring_role_arn          = var.monitoring_role_arn
   performance_insights_enabled = var.performance_insights_enabled
+  publicly_accessible          = var.publicly_accessible
+
   tags = merge(
     var.tags,
     {
@@ -51,18 +55,42 @@ resource "aws_rds_cluster_instance" "this" {
 }
 
 resource "aws_db_parameter_group" "this" {
-  name   = var.parameter_group_name
   family = var.family
+  name   = "${var.db_name}-instance"
 
-  parameter {
-    apply_method = "pending-reboot"
-    name         = "max_connections"
-    value        = "100"
+  dynamic "parameter" {
+    for_each = var.db_parameters_instance
+    content {
+      apply_method = "pending-reboot"
+      name         = parameter.key
+      value        = parameter.value
+    }
   }
 
   lifecycle {
     create_before_destroy = true
   }
+
+  tags = var.tags
 }
 
 
+resource "aws_rds_cluster_parameter_group" "this" {
+  family      = var.family
+  name        = "${var.db_name}-cluster"
+
+  dynamic "parameter" {
+    for_each = var.db_parameters_cluster
+    content {
+      apply_method = "pending-reboot"
+      name         = parameter.key
+      value        = parameter.value
+    }
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = var.tags
+}
