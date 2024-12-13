0e3ee721ffd7b0a072a73674717366c04fe9f907
Ali Bahman <abn@webit4.me>
2024-12-13 16:10:54 +0000

    DP-1002 Configure WAF to logs not allowd requests

--- terragrunt/modules/core-iam/terraform-product-datasource.tf
+++ terragrunt/modules/core-iam/terraform-product-datasource.tf
@@ -125,9 +125,10 @@ data "aws_iam_policy_document" "terraform_product" {
     ]
     effect = "Allow"
     resources = [
-      "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:/ecs*",
       "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:/${local.name_prefix}*",
       "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:/aws/vendedlogs/${local.name_prefix}*",
+      "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:/ecs*",
+      "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:aws-waf-logs-*",
     ]
     sid = "ManageProductLogs"
   }
@@ -231,14 +232,18 @@ data "aws_iam_policy_document" "terraform_product" {
     actions = [
       "wafv2:AssociateWebACL",
       "wafv2:CreateWebACL",
+      "wafv2:DeleteLoggingConfiguration",
+      "wafv2:GetLoggingConfiguration",
       "wafv2:GetWebACL",
       "wafv2:ListTagsForResource",
+      "wafv2:PutLoggingConfiguration",
       "wafv2:TagResource",
       "wafv2:UpdateWebACL",
     ]
     effect = "Allow"
     resources = [
-      "arn:aws:wafv2:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:regional/webacl/${local.name_prefix}-*"
+      "arn:aws:wafv2:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:regional/webacl/${local.name_prefix}-*",
+      "arn:aws:wafv2:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:regional/webacl/${local.name_prefix}-*/*",
     ]
     sid = "ManageProductWAF"
   }
---
+++ terragrunt/modules/core-networking/datasource.tf
@@ -0,0 +1,31 @@
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
+
+
+data "aws_iam_policy_document" "waf_manage_logs" {
+  version = "2012-10-17"
+  statement {
+    sid = "AllowWAFManageLogs"
+    effect = "Allow"
+    principals {
+      identifiers = ["delivery.logs.amazonaws.com"]
+      type        = "Service"
+    }
+    actions   = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    resources = ["${aws_cloudwatch_log_group.waf.arn}:*"]
+    condition {
+      test     = "ArnLike"
+      values   = ["arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:*"]
+      variable = "aws:SourceArn"
+    }
+    condition {
+      test     = "StringEquals"
+      values   = [tostring(data.aws_caller_identity.current.account_id)]
+      variable = "aws:SourceAccount"
+    }
+  }
+}
