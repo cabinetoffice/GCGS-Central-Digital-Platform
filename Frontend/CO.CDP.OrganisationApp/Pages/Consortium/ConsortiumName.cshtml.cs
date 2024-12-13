34930fcfcb43866f9236d3e498166539a8d50b1e
dpatel017 <dpatel017@outlook.com>
2024-12-13 09:14:31 +0000

    DP-836 - Create consortium  - Name, email, address & overview (#1009)
    * Create consortium  - Name, email, address & overview - WIP
    
    * Added feature flag
    
    * Saving Consortium to DB & refactoring
    
    * Added GB-PPON as identifier
    
    * Consortium overview - wip
    
    * Test for consortium email - wip
    
    * Added create a new consortium button with feature flag
    
    * localization for consortium overview
    
    * changed to new enum
    
    * chnaged scheme type in register method
    
    * removed back link on Consortium overview page
    
    * Added test for consortium email post method
    
    * test for consortium address
    
    * Renamed test file
    
    * address page change for localization & bug fix
    
    * Update Frontend/CO.CDP.OrganisationApp/Models/ConsortiumDetails.cs
    
    Co-authored-by: Andy Mantell <134642+andymantell@users.noreply.github.com>
    
    * renamed property
    
    * Update Frontend/CO.CDP.OrganisationApp/Pages/Consortium/ConsortiumAddress.cshtml.cs
    
    Co-authored-by: Andy Mantell <134642+andymantell@users.noreply.github.com>
    
    * Update Services/CO.CDP.Localization/StaticTextResource.resx
    
    Co-authored-by: Andy Mantell <134642+andymantell@users.noreply.github.com>
    
    * Update Frontend/CO.CDP.OrganisationApp/Pages/Consortium/ConsortiumEmail.cshtml
    
    Co-authored-by: Andy Mantell <134642+andymantell@users.noreply.github.com>
    
    * Update Frontend/CO.CDP.OrganisationApp/Pages/Consortium/ConsortiumEmail.cshtml.cs
    
    Co-authored-by: Andy Mantell <134642+andymantell@users.noreply.github.com>
    
    * Removed commented code
    
    * removed blank localization from consortium address
    
    * Informal Consortium - Assign PPON
    
    * restored localization file
    
    * bug fix on post
    
    * Removed blank consortium address hint
    
    * corrected spelling mistake
    
    * Localization change
    
    ---------
    
    Co-authored-by: Andy Mantell <134642+andymantell@users.noreply.github.com>
    Co-authored-by: Dharmendra Verma <64859911+dharmverma@users.noreply.github.com>

---
+++ Frontend/CO.CDP.OrganisationApp/Pages/Consortium/ConsortiumName.cshtml.cs
@@ -0,0 +1,36 @@
+using CO.CDP.Localization;
+using CO.CDP.OrganisationApp.Constants;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.FeatureManagement.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace CO.CDP.OrganisationApp.Pages.Consortium;
+
+[FeatureGate(FeatureFlags.Consortium)]
+[ValidateConsortiumStep]
+public class ConsortiumNameModel(ISession session) : ConsortiumStepModel(session)
+{
+    public override string CurrentPage => ConsortiumNamePage;
+
+    [BindProperty]
+    [Required(ErrorMessageResourceName = nameof(StaticTextResource.Consortium_ConsortiumName_EnterNameError), ErrorMessageResourceType = typeof(StaticTextResource))]
+    public string? ConsortiumName { get; set; }
+
+    public IActionResult OnGet()
+    {   
+        ConsortiumName = ConsortiumDetails.ConsortiumName;
+
+        return Page();
+    }
+
+    public IActionResult OnPost()
+    {
+        if (!ModelState.IsValid) return Page();
+
+        ConsortiumDetails.ConsortiumName = ConsortiumName;
+
+        SessionContext.Set(Session.ConsortiumKey, ConsortiumDetails);
+
+        return RedirectToPage("ConsortiumAddress", new { UkOrNonUk = "uk" });
+    }
+}
