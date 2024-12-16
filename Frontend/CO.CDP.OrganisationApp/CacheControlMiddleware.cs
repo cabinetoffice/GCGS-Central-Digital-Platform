9b1b0f3b96e9f73abb2b2c2a2b6a8d6f56e0b1e0
Shilpi Goel <Shilpi.Goel@goaco.com>
2024-12-16 11:12:19 +0000

    Exculded static files to add in cache-control header

--- Frontend/CO.CDP.OrganisationApp/CacheControlMiddleware.cs
+++ Frontend/CO.CDP.OrganisationApp/CacheControlMiddleware.cs
@@ -1,16 +1,26 @@
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.StaticFiles;
 
 public class CacheControlMiddleware
 {
     private readonly RequestDelegate _next;
+    private readonly IContentTypeProvider _contentTypeProvider;
+
 
     public CacheControlMiddleware(RequestDelegate next)
     {
         _next = next;
+        _contentTypeProvider = new FileExtensionContentTypeProvider();
+
     }
 
     public async Task Invoke(HttpContext context)
     {
+        if (IsStaticFileRequest(context.Request.Path))
+        {
+            await _next(context);
+            return;
+        }
         context.Response.OnStarting(() =>
         {
             if (!context.Response.Headers.ContainsKey("Cache-Control"))
@@ -21,5 +31,11 @@ public class CacheControlMiddleware
         });
 
         await _next(context);
+    }
+
+    private bool IsStaticFileRequest(PathString path)
+    {
+        var fileExtension = System.IO.Path.GetExtension(path);
+        return fileExtension != null && _contentTypeProvider.TryGetContentType(fileExtension, out _);
     }
 }
