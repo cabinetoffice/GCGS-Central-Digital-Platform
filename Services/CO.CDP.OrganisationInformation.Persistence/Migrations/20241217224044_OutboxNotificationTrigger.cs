2818df1ce918fe8fb7b0c3c682bc0db639f11f72
Jakub Zalas <jakub@zalas.pl>
2024-12-17 22:58:25 +0000

    Add migrations for outbox notification triggers

---
+++ Services/CO.CDP.OrganisationInformation.Persistence/Migrations/20241217224044_OutboxNotificationTrigger.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using static CO.CDP.MQ.Outbox.Migrations;
+
+#nullable disable
+
+namespace CO.CDP.OrganisationInformation.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class OutboxNotificationTrigger : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(NotificationFunction(name: "notify_of_outbox_message"));
+            migrationBuilder.Sql(NotificationTrigger(
+                 name: "trigger_notify_of_outbox_message",
+                 function: "notify_of_outbox_message",
+                 table: "outbox_messages",
+                 channel: "outbox"
+            ));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP TRIGGER IF EXISTS trigger_notify_of_outbox_message ON outbox_messages");
+            migrationBuilder.Sql("DROP FUNCTION IF EXISTS notify_of_outbox_message");
+        }
+    }
+}
