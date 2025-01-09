fd67fa02074481e7fb8bdb477c81039fbe6aacb5
Ali Bahman <abn@webit4.me>
2025-01-09 13:59:32 +0000

    DP-979 Add Aurora PostgreSQL Clusters for SIRSI and Entity Verification; Update PGAdmin Configuration (#1116)
    - Provisioned Aurora PostgreSQL clusters for SIRSI and Entity Verification services in staging.
      - Updated PGAdmin configuration to link with new cluster endpoints and credentials.
      - Added new Terragrunt modules for managing Aurora clusters, including configuration for encryption, parameter groups, and instances.
      - Enhanced PGAdmin setup to dynamically configure clusters and associated credentials.
      - Updated ECS configurations and task definitions to accommodate the new database clusters.
      - Modified IAM policies to include new resource ARNs for Aurora clusters and Secrets Manager.
      - Introduced centralized logging improvements and refactored environment variable usage.
      - Documentation updates to reflect the new database cluster setup and PGAdmin configuration process.
    
    Next Steps:
      - Reconfigure ECS services to use the new Aurora clusters.
      - Test connectivity and functionality for SIRSI and Entity Verification with the updated setup.

--- terragrunt/modules/database/locals.tf
+++ terragrunt/modules/database/locals.tf
@@ -1,3 +1,5 @@
 locals {
   name_prefix = var.product.resource_name
+  sirsi_cluster_name = "${local.name_prefix}-cluster"
+  ev_cluster_name = "${local.name_prefix}-ev-cluster"
 }
--- terragrunt/modules/database/outputs.tf
+++ terragrunt/modules/database/outputs.tf
@@ -2,6 +2,22 @@ output "entity_verification_address" {
   value = module.rds_entity_verification.db_address
 }
 
+output "entity_verification_cluster_address" {
+  value = module.cluster_entity_verification.db_address
+}
+
+output "entity_verification_cluster_credentials_arn" {
+  value = module.cluster_entity_verification.db_master_user_secret_arn
+}
+
+output "entity_verification_cluster_credentials_kms_key_id" {
+  value = module.cluster_entity_verification.db_master_user_secret_kms_key_id
+}
+
+output "entity_verification_cluster_name" {
+  value = module.cluster_entity_verification.db_name
+}
+
 output "entity_verification_credentials_arn" {
   value = module.rds_entity_verification.db_credentials_arn
 }
@@ -16,6 +32,22 @@ output "entity_verification_name" {
 
 output "sirsi_address" {
   value = module.rds_sirsi.db_address
+}
+
+output "sirsi_cluster_address" {
+  value = module.cluster_sirsi.db_address
+}
+
+output "sirsi_cluster_credentials_arn" {
+  value = module.cluster_sirsi.db_master_user_secret_arn
+}
+
+output "sirsi_cluster_credentials_kms_key_id" {
+  value = module.cluster_sirsi.db_master_user_secret_arn
+}
+
+output "sirsi_cluster_name" {
+  value = module.cluster_sirsi.db_name
 }
 
 output "sirsi_credentials_arn" {
