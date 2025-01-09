93d4cf60b22449f092c002e754f99e906b428565
Andy Mantell <134642+andymantell@users.noreply.github.com>
2025-01-09 09:50:59 +0000

    Initial work on organisation search endpoint

---
+++ Services/CO.CDP.Organisation.WebApi/UseCase/SearchOrganisationUseCase.cs
@@ -0,0 +1,15 @@
+using AutoMapper;
+using CO.CDP.Functional;
+using CO.CDP.Organisation.WebApi.Model;
+
+using CO.CDP.OrganisationInformation.Persistence;
+
+namespace CO.CDP.Organisation.WebApi.UseCase;
+public class SearchOrganisationUseCase(IOrganisationRepository organisationRepository, IMapper mapper) : IUseCase<OrganisationSearchQuery, IEnumerable<Model.Organisation>>
+{
+    public async Task<IEnumerable<Model.Organisation>> Execute(OrganisationSearchQuery query)
+    {
+        return await organisationRepository.SearchByName(query.Name, query.Role)
+            .AndThen(organisations => organisations.Select(mapper.Map<Model.Organisation>));        
+    }
+}
