215009b8c55814640cdd644715487bec58e56542
Shilpi Goel <Shilpi.Goel@goaco.com>
2025-02-14 16:37:23 +0000

    unit tests

--- Services/CO.CDP.Organisation.WebApi.Tests/UseCase/UpdateSupplierInformationUseCaseTest.cs
+++ Services/CO.CDP.Organisation.WebApi.Tests/UseCase/UpdateSupplierInformationUseCaseTest.cs
@@ -110,6 +110,85 @@ public class UpdateSupplierInformationUseCaseTests : IClassFixture<AutoMapperFix
     }
 
     [Fact]
+    public async Task Execute_ShouldUpdateSupplierType_WhenSupplierTypeIsProvided()
+    {
+        var organisation = Organisation;
+        var updateInfo = new UpdateSupplierInformation
+        {
+            Type = SupplierInformationUpdateType.SupplierType,
+            SupplierInformation = new SupplierInfo { SupplierType = SupplierType.Individual }
+        };
+
+        _organisationRepositoryMock.Setup(repo => repo.Find(_organisationId)).ReturnsAsync(organisation);
+
+        var command = (_organisationId, updateInfo);
+
+        var result = await _useCase.Execute(command);
+
+        organisation.Should().NotBeNull();
+        organisation.SupplierInfo.Should().NotBeNull();
+        organisation.SupplierInfo!.SupplierType.Should().NotBeNull();
+        organisation.SupplierInfo.SupplierType.Should().Be(SupplierType.Individual);
+        organisation.SupplierInfo.OperationTypes.Should().BeEmpty();
+        organisation.SupplierInfo.LegalForm.Should().BeNull();
+        organisation.SupplierInfo.CompletedLegalForm.Should().BeFalse();
+        organisation.SupplierInfo.CompletedOperationType.Should().BeFalse();
+
+        result.Should().BeTrue();
+        _organisationRepositoryMock.Verify(repo => repo.Save(organisation), Times.Once);
+    }
+
+    [Fact]
+    public async Task Execute_ShouldResetFields_WhenSupplierTypeIsNotIndividual()
+    {
+        var organisation = new Persistence.Organisation
+        {
+            Guid = _organisationId,
+            Name = "Test",
+            Type = OrganisationType.Organisation,
+            Tenant = It.IsAny<Tenant>(),
+            ContactPoints = [new Persistence.Organisation.ContactPoint { Email = "test@test.com" }],
+            SupplierInfo = new Persistence.Organisation.SupplierInformation
+            {
+                SupplierType = SupplierType.Individual,  // Initially set to Individual
+                OperationTypes = new List<OperationType> { OperationType.SmallOrMediumSized },
+                LegalForm = new Persistence.Organisation.LegalForm
+                {
+                    RegisteredUnderAct2006 = true,
+                    RegisteredLegalForm = "Private Limited",
+                    LawRegistered = "UK",
+                    RegistrationDate = DateTimeOffset.UtcNow.AddYears(-10)
+                },
+                CompletedLegalForm = true,
+                CompletedOperationType = true
+            }
+        };     
+
+        var updateInfo = new UpdateSupplierInformation
+        {
+            Type = SupplierInformationUpdateType.SupplierType,
+            SupplierInformation = new SupplierInfo { SupplierType = SupplierType.Organisation }
+        };
+
+        _organisationRepositoryMock.Setup(repo => repo.Find(_organisationId)).ReturnsAsync(organisation);
+
+        var command = (_organisationId, updateInfo);
+        var result = await _useCase.Execute(command);
+
+        
+        organisation.SupplierInfo.Should().NotBeNull();
+        organisation.SupplierInfo.SupplierType.Should().Be(SupplierType.Organisation);
+        
+        organisation.SupplierInfo.OperationTypes.Should().NotBeNull();
+        organisation.SupplierInfo.LegalForm.Should().NotBeNull();
+        organisation.SupplierInfo.CompletedLegalForm.Should().BeTrue();
+        organisation.SupplierInfo.CompletedOperationType.Should().BeTrue();
+
+        result.Should().BeTrue();
+        _organisationRepositoryMock.Verify(repo => repo.Save(organisation), Times.Once);
+    }
+
+    [Fact]
     public async Task Execute_ShouldThrowInvalidUpdateSupplierInformationCommand_WhenLegalFormIsNull()
     {
         var updateSupplierInformation = new UpdateSupplierInformation
