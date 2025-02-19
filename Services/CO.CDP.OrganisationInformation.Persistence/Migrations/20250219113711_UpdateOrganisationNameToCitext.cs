b7b8d58f7b31b4d78d961654191c914cabe68050
Richard Mohammed <richard.mohammed@goaco.com>
2025-02-19 11:39:39 +0000

    Changes the organisation name type to be a citext type

---
+++ Services/CO.CDP.OrganisationInformation.Persistence/Migrations/20250219113711_UpdateOrganisationNameToCitext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CO.CDP.OrganisationInformation.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateOrganisationNameToCitext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("CREATE EXTENSION IF NOT EXISTS citext;");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Organisations",
+                type: "citext",
+                nullable: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Organisations",
+                type: "text",
+                nullable: false);
+
+            migrationBuilder.Sql("DROP EXTENSION IF EXISTS citext;");
+        }
+    }
+}
