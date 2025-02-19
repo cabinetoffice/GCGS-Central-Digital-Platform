7c0ed43649114b0a1a1acdded6ae2fb3933a9f7b
Andy Mantell <134642+andymantell@users.noreply.github.com>
2025-02-19 12:40:47 +0000

    Output trace ID to error pages to assist with support queries

--- Frontend/CO.CDP.OrganisationApp/Pages/Error.cshtml.cs
+++ Frontend/CO.CDP.OrganisationApp/Pages/Error.cshtml.cs
@@ -6,9 +6,12 @@ namespace CO.CDP.OrganisationApp.Pages;
 [AuthenticatedSessionNotRequired]
 public class ErrorModel : PageModel
 {
+    public required string TraceId { get; set; }
+
     public IActionResult OnGet()
     {
         Response.StatusCode = 500;
+        TraceId = HttpContext.TraceIdentifier;
 
         return Page();
     }
