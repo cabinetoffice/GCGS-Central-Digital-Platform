fbee2e54f760f5e38dad46a23c30f8ec27b53a04
Ali Bahman <abn@webit4.me>
2025-02-25 16:24:04 +0000

    BAU Make sure SQS metrics are collected corresponding account

--- terragrunt/tools/grafana/configs/entrypoint.sh
+++ terragrunt/tools/grafana/configs/entrypoint.sh
@@ -4,6 +4,9 @@ set -e
 sed -i "s/GRAFANA_CLOUDWATCH_ACCOUNT_ID/${GRAFANA_CLOUDWATCH_ACCOUNT_ID}/g" /etc/grafana/provisioning/dashboards/application/*
 sed -i "s/GRAFANA_CLOUDWATCH_ACCOUNT_ID/${GRAFANA_CLOUDWATCH_ACCOUNT_ID}/g" /etc/grafana/provisioning/dashboards/infrastructure/*
 sed -i "s/GRAFANA_CLOUDWATCH_ACCOUNT_ID/${GRAFANA_CLOUDWATCH_ACCOUNT_ID}/g" /etc/grafana/provisioning/dashboards/overview/*
+sed -i "s/CDP_SIRSI_ENVIRONMENT/${CDP_SIRSI_ENVIRONMENT}/g" /etc/grafana/provisioning/dashboards/application/*
+sed -i "s/CDP_SIRSI_ENVIRONMENT/${CDP_SIRSI_ENVIRONMENT}/g" /etc/grafana/provisioning/dashboards/infrastructure/*
+sed -i "s/CDP_SIRSI_ENVIRONMENT/${CDP_SIRSI_ENVIRONMENT}/g" /etc/grafana/provisioning/dashboards/overview/*
 
 sed -i "s#__GRAFANA_CLOUDWATCH_ROLE__#${GRAFANA_CLOUDWATCH_ROLE}#g" /etc/grafana/provisioning/datasources/datasource.yaml.tpl
 
