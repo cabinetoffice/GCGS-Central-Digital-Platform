10ad085fdaf7ba87c5ae81dd8e00f718db009f99
Ali Bahman <abn@webit4.me>
2025-02-28 11:43:31 +0000

    DP-1270 Add RDS & SQS alarms (#1378)

--- terragrunt/modules/alerting/locals.tf
+++ terragrunt/modules/alerting/locals.tf
@@ -10,7 +10,14 @@ locals {
   ecs_threshold_cup_percent    = 80
   ecs_threshold_memory_percent = 80
 
+  rds_threshold_cup_percent         = 80
+  rds_threshold_freeable_storage_gb = 2.4
+  rds_threshold_connections         = 9000
+
   redis_threshold_cpu_percent             = 80
   redis_threshold_database_memory_percent = 80
   redis_threshold_evictions               = 1000
+
+  sqs_threshold_message_age_min = 5
+  sqs_threshold_queue_depth     = 1000
 }
