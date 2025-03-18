3f587879feb1140e5ee5e38dcaec89ac491cda53
Andy Mantell <134642+andymantell@users.noreply.github.com>
2025-03-18 11:37:51 +0000

    Revert "Connected entity end date validation - do not allow today and future …"
    This reverts commit 9ade613247786bf8c769d788e8e99fe8c37a88c1.

--- Frontend/CO.CDP.OrganisationApp/Pages/Supplier/ConnectedEntity/ConnectedEntityRemoveConfirmation.cshtml.cs
+++ Frontend/CO.CDP.OrganisationApp/Pages/Supplier/ConnectedEntity/ConnectedEntityRemoveConfirmation.cshtml.cs
@@ -1,11 +1,11 @@
-using CO.CDP.Localization;
-using CO.CDP.Mvc.Validation;
 using CO.CDP.Organisation.WebApiClient;
-using CO.CDP.OrganisationApp.Constants;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
+using CO.CDP.Mvc.Validation;
+using CO.CDP.OrganisationApp.Constants;
+using Microsoft.AspNetCore.Authorization;
+using CO.CDP.Localization;
 
 namespace CO.CDP.OrganisationApp.Pages.Supplier.ConnectedEntity;
 
@@ -78,13 +78,6 @@ public class ConnectedEntityRemoveConfirmationModel(
                 ModelState.AddModelError(nameof(EndDate), StaticTextResource.Supplier_ConnectedEntity_ConnectedEntityRemoveConfirmation_DateInvalidError);
                 return Page();
             }
-
-            if (endDate >= DateTime.Today)
-            {
-                ModelState.AddModelError(nameof(EndDate), StaticTextResource.Supplier_ConnectedEntity_ConnectedEntityRemoveConfirmation_DateNotInPastError);
-                return Page();
-            }
-
             endDate = endDate.AddHours(23).AddMinutes(59).AddSeconds(59).ToUniversalTime();
             await organisationClient.DeleteConnectedEntityAsync(Id, ConnectedPersonId, new DeleteConnectedEntity(endDate));
         }
@@ -96,7 +89,7 @@ public class ConnectedEntityRemoveConfirmationModel(
         return RedirectToPage("ConnectedPersonSummary", new { Id });
     }
 
-    private async Task<ConnectedEntityLookup?> GetConnectedEntity(IOrganisationClient organisationClient)
+    private async Task<CO.CDP.Organisation.WebApiClient.ConnectedEntityLookup?> GetConnectedEntity(IOrganisationClient organisationClient)
     {
         try
         {
