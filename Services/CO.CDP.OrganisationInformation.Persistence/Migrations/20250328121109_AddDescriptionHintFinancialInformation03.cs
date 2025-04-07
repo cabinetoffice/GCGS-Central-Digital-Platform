c52738eb0bf0f01d7739eef3505baba4986bea0d
dpatel017 <dpatel017@outlook.com>
2025-04-07 11:26:18 +0100

    Updated migration after review

--- Services/CO.CDP.OrganisationInformation.Persistence/Migrations/20250328121109_AddDescriptionHintFinancialInformation03.cs
+++ Services/CO.CDP.OrganisationInformation.Persistence/Migrations/20250328121109_AddDescriptionHintFinancialInformation03.cs
@@ -14,7 +14,7 @@ namespace CO.CDP.OrganisationInformation.Persistence.Migrations
                 UPDATE
                     form_questions
                 SET
-                    description = '<p class=""govuk-body"">Auditing requirements are defined in <a href=""https://www.legislation.gov.uk/ukpga/2006/46/part/16"" target=""https://www.legislation.gov.uk/ukpga/2006/46/part/16"">part 16 of the Companies Act 2006 (opens in new tab)</a> or an overseas equivalent</p>'                
+                    description = 'FinancialInformation_03_Description'                
                 WHERE
                     name = '_FinancialInformation03'
             ");
