d8b1e6256a89fe01418b6306b2197cd20c364530
Andy Mantell <134642+andymantell@users.noreply.github.com>
2025-04-11 15:26:44 +0100

    Add npgsql healthcheck to scheduled worker

--- Services/CO.CDP.ScheduledWorker/Program.cs
+++ Services/CO.CDP.ScheduledWorker/Program.cs
@@ -2,13 +2,15 @@ using CO.CDP.Configuration.Helpers;
 using CO.CDP.GovUKNotify;
 using CO.CDP.OrganisationInformation.Persistence;
 using CO.CDP.ScheduledWorker;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Npgsql;
 
-var builder = Host.CreateApplicationBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = ConnectionStringHelper.GetConnectionString(builder.Configuration, "OrganisationInformationDatabase");
 builder.Services.AddSingleton(new NpgsqlDataSourceBuilder(connectionString).MapEnums().Build());
+builder.Services.AddHealthChecks().AddNpgSql(sp => sp.GetRequiredService<NpgsqlDataSource>());
 builder.Services.AddDbContext<OrganisationInformationContext>((sp, o) => o.UseNpgsql(sp.GetRequiredService<NpgsqlDataSource>()));
 
 builder.Services.AddScoped<IMouRepository, DatabaseMouRepository>();
@@ -16,5 +18,7 @@ builder.Services.AddHostedService<CompleteMoUTimedHostedService>();
 builder.Services.AddScoped<IScopedProcessingService, CompleteMoUReminderService>();
 builder.Services.AddGovUKNotifyApiClient(builder.Configuration);
 
-using IHost host = builder.Build();
-await host.RunAsync();
+var app = builder.Build();
+app.MapHealthChecks("/health").AllowAnonymous();
+
+app.Run();
