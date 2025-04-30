2cff82d19f2f9a0b21dfc0547a6210baf5d20ae2
Andy Mantell <134642+andymantell@users.noreply.github.com>
2025-04-30 09:02:26 +0100

    Manage stored procedures with sql scripts on disk

---
+++ Services/CO.CDP.OrganisationInformation.Persistence/Migrations/20250429102036_MovingToGitManagedStoredProcedures.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CO.CDP.OrganisationInformation.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class MovingToGitManagedStoredProcedures : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(File.ReadAllText("Migrations/StoredProcedures/get_shared_consent_details.psql"));
+            migrationBuilder.Sql(File.ReadAllText("Migrations/StoredProcedures/create_shared_consent_snapshot.psql"));
+        }
+    }
+}
