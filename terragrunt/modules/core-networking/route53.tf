a3cbd9ba78e69c0003b08be018bec533926643e9
Ali Bahman <abn@webit4.me>
2025-04-30 13:53:11 +0100

    DP-1404 (WIP) Delegate development domain from production, update compose template

--- terragrunt/modules/core-networking/route53.tf
+++ terragrunt/modules/core-networking/route53.tf
@@ -1,3 +1,7 @@
+import {
+  id = "Z04057483Q7XT55RB4P27"
+  to = aws_route53_zone.public
+}
 resource "aws_route53_zone" "public" {
 
   name = var.product.public_hosted_zone
@@ -18,3 +22,20 @@ resource "aws_route53_zone" "production_private_beta" {
     prevent_destroy = true
   }
 }
+
+resource "aws_route53_record" "development_delegation"{
+  count = var.is_production ? 1 : 0
+
+  name    = "dev.${aws_route53_zone.public.name}"
+  type    = "NS"
+  ttl     = 300
+  zone_id = aws_route53_zone.public.id
+
+  records = [
+     "ns-814.awsdns-37.net.",
+     "ns-1119.awsdns-11.org.",
+     "ns-318.awsdns-39.com.",
+     "ns-1696.awsdns-20.co.uk.",
+  ]
+}
+
