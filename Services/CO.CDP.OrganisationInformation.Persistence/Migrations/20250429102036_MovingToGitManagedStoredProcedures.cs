03f3208ed4bc1eb29648b0b73cfa98d7eb8e115f
Andy Mantell <134642+andymantell@users.noreply.github.com>
2025-04-30 10:24:40 +0100

    add facility for down migrations of stored procedures

--- Services/CO.CDP.OrganisationInformation.Persistence/Migrations/20250429102036_MovingToGitManagedStoredProcedures.cs
+++ Services/CO.CDP.OrganisationInformation.Persistence/Migrations/20250429102036_MovingToGitManagedStoredProcedures.cs
@@ -13,5 +13,12 @@ namespace CO.CDP.OrganisationInformation.Persistence.Migrations
             migrationBuilder.Sql(File.ReadAllText("Migrations/StoredProcedures/get_shared_consent_details.psql"));
             migrationBuilder.Sql(File.ReadAllText("Migrations/StoredProcedures/create_shared_consent_snapshot.psql"));
         }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(File.ReadAllText("Migrations/StoredProcedures/get_shared_consent_details-PREVIOUS.psql"));
+            migrationBuilder.Sql(File.ReadAllText("Migrations/StoredProcedures/create_shared_consent_snapshot-PREVIOUS.psql"));
+        }
     }
 }
