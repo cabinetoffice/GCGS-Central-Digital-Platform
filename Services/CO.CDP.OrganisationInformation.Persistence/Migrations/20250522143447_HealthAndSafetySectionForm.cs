bb67e689e8e7f720434e34616c660d87fa66aca0
Edward Dennis <98972365+EdwardDennis@users.noreply.github.com>
2025-05-22 17:16:00 +0100

    Implement page 1

--- Services/CO.CDP.OrganisationInformation.Persistence/Migrations/20250522143447_HealthAndSafetySectionForm.cs
+++ Services/CO.CDP.OrganisationInformation.Persistence/Migrations/20250522143447_HealthAndSafetySectionForm.cs
@@ -22,6 +22,12 @@ namespace CO.CDP.OrganisationInformation.Persistence.Migrations
 	                INSERT INTO form_sections (guid, title, form_id, allows_multiple_answer_sets, check_further_questions_exempted, type,display_order, configuration)
                     VALUES ('{Guid.NewGuid()}', 'HealthAndSafety_SectionTitle', form_id, FALSE, FALSE, 3,1, '{{""AddAnotherAnswerLabel"": ""HealthAndSafety_Configuration_AddAnotherAnswerLabel"", ""SingularSummaryHeading"": ""HealthAndSafety_Configuration_SingularSummaryHeading"", ""SummaryRenderFormatter"": {{""KeyParams"": [""_HealthAndSafetyQuestion02"", ""_HealthAndSafetyQuestion04""], ""ValueParams"": [""_HealthAndSafetyQuestion02"", ""_HealthAndSafetyQuestion04""], ""KeyExpression"": ""{{0}}"", ""ValueExpression"": ""{{1}}"", ""KeyExpressionOperation"": ""StringFormat"", ""ValueExpressionOperation"": ""StringFormat""}}, ""RemoveConfirmationCaption"": ""HealthAndSafety_SectionTitle"", ""RemoveConfirmationHeading"": ""HealthAndSafety_Configuration_RemoveConfirmationHeading"", ""PluralSummaryHeadingFormat"": ""HealthAndSafety_Configuration_PluralSummaryHeadingFormat"", ""SingularSummaryHeadingHint"": ""HealthAndSafety_Configuration_SingularSummaryHeadingHint"", ""FurtherQuestionsExemptedHint"": ""HealthAndSafety_Configuration_FurtherQuestionsExemptedHint"", ""PluralSummaryHeadingHintFormat"": ""HealthAndSafety_Configuration_PluralSummaryHeadingHintFormat"", ""FurtherQuestionsExemptedHeading"": ""HealthAndSafety_Configuration_FurtherQuestionsExemptedHeading""}}');
 
+                    SELECT id INTO sectionId FROM form_sections where title = 'HealthAndSafety_SectionTitle';
+
+                    INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"",  caption, summary_title, ""name"", sort_order)
+                    VALUES('{Guid.NewGuid()}', previousQuestionId, NULL, sectionId, 0, true, 'HealthAndSafetyQuestion_01_Title', 'HealthAndSafetyQuestion_01_Description', '{{}}', NULL, '', '_HealthAndSafetyQuestion01', 1)
+                    RETURNING id INTO previousQuestionId;
+
                 END $$;
             ");
         }
