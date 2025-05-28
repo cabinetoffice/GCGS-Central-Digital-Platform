e844e41d13198f3e69df853d605212c460c161ea
Ali Bahman <abn@webit4.me>
2025-05-28 13:52:47 +0100

    DP-1474 Tune FTS Task config
      - Split FTS and its healthcheck
      - Put FTS services behind the Cognito
      - Commit commented out DMS componenets

--- terragrunt/modules/auth/outputs.tf
+++ terragrunt/modules/auth/outputs.tf
@@ -6,6 +6,23 @@ output "cloud_beaver_user_pool_client_id" {
   value = aws_cognito_user_pool_client.cloud_beaver.id
 }
 
+output "fts_user_pool_arn" {
+  value = aws_cognito_user_pool.auth.arn
+}
+
+output "fts_user_pool_client_id" {
+  value = aws_cognito_user_pool_client.fts.id
+}
+
+output "fts_healthcheck_user_pool_arn" {
+  value = aws_cognito_user_pool.auth.arn
+}
+
+output "fts_healthcheck_user_pool_client_id" {
+  value = aws_cognito_user_pool_client.fts_healthcheck.id
+}
+
+
 output "grafana_user_pool_arn" {
   value = aws_cognito_user_pool.auth.arn
 }
