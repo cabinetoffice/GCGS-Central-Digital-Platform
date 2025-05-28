d80868068e16497d35dd845b2fd134fdcc346c36
Ali Bahman <abn@webit4.me>
2025-05-28 15:19:14 +0100

    DP-1474 Tune FTS Task config (#1586)
    - Split FTS and its healthcheck
    - Put FTS services behind the Cognito
    - Commit commented out DMS components

---
+++ terragrunt/modules/ecs/service-fts-healthcheck.tf
@@ -0,0 +1,48 @@
+module "ecs_service_fts_healthcheck" {
+  source = "../ecs-service"
+
+  container_definitions = templatefile(
+    "${path.module}/templates/task-definitions/${var.service_configs.fts_healthcheck.name}.json.tftpl",
+    {
+      container_port  = var.service_configs.fts_healthcheck.port
+      cpu             = var.service_configs.fts_healthcheck.cpu
+      db_host         = var.db_fts_cluster_address
+      db_name         = var.db_fts_cluster_name
+      db_pass         = local.db_fts_password
+      db_user         = local.db_fts_username
+      host_port       = var.service_configs.fts_healthcheck.port
+      image           = local.ecr_urls[var.service_configs.fts_healthcheck.name]
+      lg_name         = aws_cloudwatch_log_group.tasks[var.service_configs.fts_healthcheck.name].name
+      lg_prefix       = "app"
+      lg_region       = data.aws_region.current.name
+      memory          = var.service_configs.fts_healthcheck.memory
+      name            = var.service_configs.fts_healthcheck.name
+      public_domain   = var.public_domain
+      service_version = "latest" //local.service_version
+      vpc_cidr        = var.vpc_cider
+    }
+  )
+
+  cluster_id             = aws_ecs_cluster.this.id
+  container_port         = var.service_configs.fts_healthcheck.port
+  cpu                    = var.service_configs.fts_healthcheck.cpu
+  desired_count          = var.service_configs.fts_healthcheck.desired_count
+  ecs_alb_sg_id          = var.alb_sg_id
+  ecs_listener_arn       = aws_lb_listener.ecs.arn
+  ecs_service_base_sg_id = var.ecs_sg_id
+  family                 = "app"
+  healthcheck_path       = "/healthz.php"
+  host_port              = var.service_configs.fts_healthcheck.port
+  memory                 = var.service_configs.fts_healthcheck.memory
+  name                   = var.service_configs.fts_healthcheck.name
+  private_subnet_ids     = var.private_subnet_ids
+  product                = var.product
+  public_domain          = var.public_domain
+  role_ecs_task_arn      = var.role_ecs_task_arn
+  role_ecs_task_exec_arn = var.role_ecs_task_exec_arn
+  tags                   = var.tags
+  user_pool_arn          = var.user_pool_fts_healthcheck_arn
+  user_pool_client_id    = var.user_pool_fts_healthcheck_client_id
+  user_pool_domain       = var.user_pool_fts_healthcheck_domain
+  vpc_id                 = var.vpc_id
+}
--- terragrunt/tools/healthcheck-fts-service/README.md
+++ terragrunt/tools/healthcheck-fts-service/README.md
@@ -28,8 +28,8 @@ There is ECR repositories in orchestrator account. Using the following commands,
 aws-switch-to-cdp-sirsi-orchestrator-goaco-terraform
 ACCOUNT_ID=$(ave aws sts get-caller-identity | jq -r '.Account')
 ave aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin ${ACCOUNT_ID}.dkr.ecr.eu-west-2.amazonaws.com
-docker tag cabinetoffice/cdp-fts:latest ${ACCOUNT_ID}.dkr.ecr.eu-west-2.amazonaws.com/cdp-fts:latest
-docker push ${ACCOUNT_ID}.dkr.ecr.eu-west-2.amazonaws.com/cdp-fts:latest
+docker tag cabinetoffice/cdp-fts-healthcheck:latest ${ACCOUNT_ID}.dkr.ecr.eu-west-2.amazonaws.com/cdp-fts-healthcheck:latest
+docker push ${ACCOUNT_ID}.dkr.ecr.eu-west-2.amazonaws.com/cdp-fts-healthcheck:latest
 ```
 
 ### Re-deploy Healthcheck service
