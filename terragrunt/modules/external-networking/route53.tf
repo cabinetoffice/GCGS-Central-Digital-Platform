b8ff638a2a6acb3f3a00ef69e869ee2f0d8eee15
Ali Bahman <abn@webit4.me>
2025-06-09 14:10:10 +0100

    DP-1512 Provision Data Platform's Hosted zone in production

--- terragrunt/modules/external-networking/route53.tf
+++ terragrunt/modules/external-networking/route53.tf
@@ -1,7 +1,19 @@
+resource "aws_route53_zone" "data_platform" {
+
+  count = var.is_production ? 1 : 0
+
+  name = replace(var.product.public_hosted_zone, "supplier-information" , "data-platform")
+  tags = var.tags
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
 resource "aws_route53_record" "www_to_fts" {
   count = var.fts_azure_frontdoor == null ? 0 : 1
 
-  zone_id = var.hosted_zone_id
+  zone_id = var.core_hosted_zone_id
   name    = "www"
   type    = "CNAME"
   ttl     = 60
