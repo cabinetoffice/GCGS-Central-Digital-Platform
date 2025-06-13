af73a523203d87e0fba1669444a4d0a76d1e4d4b
Mark Gorman <markgorman1998@gmail.com>
2025-06-13 12:01:49 +0100

    Add summary titles and edit migration file

--- Services/CO.CDP.OrganisationInformation.Persistence/Migrations/20250604113701_ModernSlaverySectionForm.cs
+++ Services/CO.CDP.OrganisationInformation.Persistence/Migrations/20250604113701_ModernSlaverySectionForm.cs
@@ -1,98 +1,112 @@
 ﻿using Microsoft.EntityFrameworkCore.Migrations;
-
 #nullable disable
-
-namespace CO.CDP.OrganisationInformation.Persistence.Migrations
+namespace CO.CDP.OrganisationInformation.Persistence.Migrations {
+/// <inheritdoc />
+public partial class ModernSlaverySectionForm : Migration
 {
     /// <inheritdoc />
-    public partial class ModernSlaverySectionForm : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-                migrationBuilder.Sql($@"
-                    DO $$
-                    DECLARE
-                        form_id int;
-                        sectionId INT;
-                        previousQuestionId INT;
-                        question4Id INT;
-                    BEGIN
-                        SELECT id INTO form_id FROM forms WHERE name = 'Standard Questions';
-
-                        INSERT INTO form_sections (guid, title, form_id, allows_multiple_answer_sets, check_further_questions_exempted, type,display_order, configuration)
-                        VALUES ('{Guid.NewGuid()}', 'ModernSlavery_SectionTitle', form_id, FALSE, FALSE, 3,1, '{{""AddAnotherAnswerLabel"": ""ModernSlavery_Configuration_AddAnotherAnswerLabel"", ""SingularSummaryHeading"": ""ModernSlavery_Configuration_SingularSummaryHeading"", ""SummaryRenderFormatter"": {{""KeyParams"": [""_ModernSlaveryQuestion02"", ""_ModernSlavery04""], ""ValueParams"": [""_ModernSlavery02"", ""_ModernSlaveryQuestion04""], ""KeyExpression"": ""{{0}}"", ""ValueExpression"": ""{{1}}"", ""KeyExpressionOperation"": ""StringFormat"", ""ValueExpressionOperation"": ""StringFormat""}}, ""RemoveConfirmationCaption"": ""ModernSlavery_SectionTitle"", ""RemoveConfirmationHeading"": ""ModernSlavery_Configuration_RemoveConfirmationHeading"", ""PluralSummaryHeadingFormat"": ""ModernSlavery_Configuration_PluralSummaryHeadingFormat"", ""SingularSummaryHeadingHint"": ""ModernSlavery_Configuration_SingularSummaryHeadingHint"", ""FurtherQuestionsExemptedHint"": ""ModernSlavery_Configuration_FurtherQuestionsExemptedHint"", ""PluralSummaryHeadingHintFormat"": ""ModernSlavery_Configuration_PluralSummaryHeadingHintFormat"", ""FurtherQuestionsExemptedHeading"": ""ModernSlavery_Configuration_FurtherQuestionsExemptedHeading""}}');
-
-                        SELECT id INTO sectionId FROM form_sections where title = 'ModernSlavery_SectionTitle';
-
-                        INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"",  caption, summary_title, ""name"", sort_order)
-                        VALUES('{Guid.NewGuid()}', previousQuestionId, NULL, sectionId, 6, true, 'Global_CheckYourAnswers', NULL, '{{}}', NULL, '', '_ModernSlaveryQuestion12', 12)
-                        RETURNING id INTO previousQuestionId;
-
-                        INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"",  caption, summary_title, ""name"", sort_order)
-                        VALUES('{Guid.NewGuid()}', previousQuestionId, NULL, sectionId, 10, true, 'ModernSlavery_11_Title', 'ModernSlavery_11_Description', '{{}}', NULL, 'ModernSlavery_11_SummaryTitle', '_ModernSlaveryQuestion11', 11)
-                        RETURNING id INTO previousQuestionId;
-
-                        INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"",  caption, summary_title, ""name"", sort_order)
-                        VALUES('{Guid.NewGuid()}', previousQuestionId, NULL, sectionId, 10, true, 'ModernSlavery_10_Title', 'ModernSlavery_10_Description', '{{}}', NULL, '', '_ModernSlaveryQuestion10', 10)
-                        RETURNING id INTO previousQuestionId;
-
-                        INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"",  caption, summary_title, ""name"", sort_order)
-                        VALUES('{Guid.NewGuid()}', previousQuestionId, NULL, sectionId, 10, true, 'ModernSlavery_09_Title', 'ModernSlavery_09_Description', '{{}}', NULL, '', '_ModernSlaveryQuestion09', 9)
-                        RETURNING id INTO previousQuestionId;
-
-                        INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"",  caption, summary_title, ""name"", sort_order)
-                        VALUES('{Guid.NewGuid()}', previousQuestionId, NULL, sectionId, 2, true, 'ModernSlavery_08_Title', NULL, '{{}}', NULL, '', '_ModernSlaveryQuestion08', 8)
-                        RETURNING id INTO previousQuestionId;
-
-                        INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"",  caption, summary_title, ""name"", sort_order)
-                        VALUES('{Guid.NewGuid()}', previousQuestionId, NULL, sectionId, 12, true, 'ModernSlavery_07_Title', NULL, '{{}}', NULL, '', '_ModernSlaveryQuestion07', 7)
-                        RETURNING id INTO previousQuestionId;
-
-                        INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"",  caption, summary_title, ""name"", sort_order)
-                        VALUES('{Guid.NewGuid()}', previousQuestionId, NULL, sectionId, 2, true, 'ModernSlavery_06_Title', '', '{{}}', NULL, '', '_ModernSlaveryQuestion06', 6)
-                        RETURNING id INTO previousQuestionId;
-
-                        INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"",  caption, summary_title, ""name"", sort_order)
-                        VALUES('{Guid.NewGuid()}', previousQuestionId, NULL, sectionId, 12, true, 'ModernSlavery_05_Title', 'ModernSlavery_05_Description', '{{}}', NULL, '', '_ModernSlaveryQuestion05', 5)
-                        RETURNING id INTO previousQuestionId;
-
-                        INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"",  caption, summary_title, ""name"", sort_order)
-                        VALUES('{Guid.NewGuid()}', previousQuestionId, NULL, sectionId, 3, true, 'ModernSlavery_04_Title', 'ModernSlavery_04_Description', '{{}}', NULL, '', '_ModernSlaveryQuestion04', 4)
-                        RETURNING id INTO previousQuestionId;
-                        question4Id := previousQuestionId;
-
-                        INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"",  caption, summary_title, ""name"", sort_order)
-                        VALUES('{Guid.NewGuid()}', previousQuestionId, NULL, sectionId, 3, true, 'ModernSlavery_03_Title', '', '{{}}', NULL, '', '_ModernSlaveryQuestion03', 3)
-                        RETURNING id INTO previousQuestionId;
-
-                        INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"",  caption, summary_title, ""name"", sort_order)
-                        VALUES('{Guid.NewGuid()}', previousQuestionId, question4Id, sectionId, 3, true, 'ModernSlavery_02_Title', 'ModernSlavery_02_Description', '{{}}', NULL, '', '_ModernSlaveryQuestion02', 2)
-                        RETURNING id INTO previousQuestionId;
-
-                        INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"",  caption, summary_title, ""name"", sort_order)
-                        VALUES('{Guid.NewGuid()}', previousQuestionId, NULL, sectionId, 0, true, 'ModernSlavery_01_Title', 'ModernSlavery_01_Description', '{{}}', NULL, '', '_ModernSlaveryQuestion01', 1)
-                        RETURNING id INTO previousQuestionId;
-
-                    END $$;
-                ");
-        }
-
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.Sql($@"
+        migrationBuilder.Sql($@"
                 DO $$
                 DECLARE
-                    sectionId INT;
+                    v_form_id INTEGER;
+                    v_section_id INTEGER;
+                    v_question1_id INTEGER;
+                    v_question2_id INTEGER;
+                    v_question3_id INTEGER;
+                    v_question4_id INTEGER;
+                    v_question5_id INTEGER;
+                    v_question6_id INTEGER;
+                    v_question7_id INTEGER;
+                    v_question8_id INTEGER;
+                    v_question9_id INTEGER;
+                    v_question10_id INTEGER;
+                    v_question11_id INTEGER;
+                    v_question12_id INTEGER;
                 BEGIN
-                    -- Get the ID of the section for Modern Slavery
-                    SELECT id INTO sectionId FROM form_sections WHERE title = 'ModernSlavery_SectionTitle';
+                    SELECT id INTO v_form_id FROM forms WHERE name = 'Standard Questions';
 
-                    -- Delete the Modern Slavery section
-                    DELETE FROM form_sections WHERE id = sectionId;
+                    IF v_form_id IS NULL THEN
+                        RAISE EXCEPTION 'Form ""Standard Questions"" not found. Migration cannot proceed.';
+                    END IF;
+
+                    INSERT INTO form_sections (guid, title, form_id, allows_multiple_answer_sets, check_further_questions_exempted, type, display_order, configuration)
+                    VALUES ('{Guid.NewGuid()}', 'ModernSlavery_SectionTitle', v_form_id, FALSE, FALSE, 3, 1, '{{""AddAnotherAnswerLabel"": ""ModernSlavery_Configuration_AddAnotherAnswerLabel"", ""SingularSummaryHeading"": ""ModernSlavery_Configuration_SingularSummaryHeading"", ""SummaryRenderFormatter"": {{""KeyParams"": [""_ModernSlaveryQuestion02"", ""_ModernSlaveryQuestion04""], ""ValueParams"": [""_ModernSlaveryQuestion02"", ""_ModernSlaveryQuestion04""], ""KeyExpression"": ""{{0}}"", ""ValueExpression"": ""{{1}}"", ""KeyExpressionOperation"": ""StringFormat"", ""ValueExpressionOperation"": ""StringFormat""}}, ""RemoveConfirmationCaption"": ""ModernSlavery_SectionTitle"", ""RemoveConfirmationHeading"": ""ModernSlavery_Configuration_RemoveConfirmationHeading"", ""PluralSummaryHeadingFormat"": ""ModernSlavery_Configuration_PluralSummaryHeadingFormat"", ""SingularSummaryHeadingHint"": ""ModernSlavery_Configuration_SingularSummaryHeadingHint"", ""FurtherQuestionsExemptedHint"": ""ModernSlavery_Configuration_FurtherQuestionsExemptedHint"", ""PluralSummaryHeadingHintFormat"": ""ModernSlavery_Configuration_PluralSummaryHeadingHintFormat"", ""FurtherQuestionsExemptedHeading"": ""ModernSlavery_Configuration_FurtherQuestionsExemptedHeading""}}')
+                    RETURNING id INTO v_section_id;
+
+                    INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"", caption, summary_title, ""name"", sort_order)
+                    VALUES('{Guid.NewGuid()}', NULL, NULL, v_section_id, 0, true, 'ModernSlavery_01_Title', 'ModernSlavery_01_Description', '{{}}', NULL, '', '_ModernSlaveryQuestion01', 1)
+                    RETURNING id INTO v_question1_id;
+                    INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"", caption, summary_title, ""name"", sort_order)
+                    VALUES('{Guid.NewGuid()}', NULL, NULL, v_section_id, 3, true, 'ModernSlavery_02_Title', 'ModernSlavery_02_Description', '{{}}', NULL, 'ModernSlavery_02_SummaryTitle', '_ModernSlaveryQuestion02', 2)
+                    RETURNING id INTO v_question2_id;
+                    INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"", caption, summary_title, ""name"", sort_order)
+                    VALUES('{Guid.NewGuid()}', NULL, NULL, v_section_id, 3, true, 'ModernSlavery_03_Title', '', '{{}}', NULL, 'ModernSlavery_03_SummaryTitle', '_ModernSlaveryQuestion03', 3)
+                    RETURNING id INTO v_question3_id;
+                    INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"", caption, summary_title, ""name"", sort_order)
+                    VALUES('{Guid.NewGuid()}', NULL, NULL, v_section_id, 3, true, 'ModernSlavery_04_Title', 'ModernSlavery_04_Description', '{{}}', NULL, 'ModernSlavery_04_SummaryTitle', '_ModernSlaveryQuestion04', 4)
+                    RETURNING id INTO v_question4_id;
+                    INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"", caption, summary_title, ""name"", sort_order)
+                    VALUES('{Guid.NewGuid()}', NULL, NULL, v_section_id, 12, true, 'ModernSlavery_05_Title', 'ModernSlavery_05_Description', '{{}}', NULL, 'ModernSlavery_05_SummaryTitle', '_ModernSlaveryQuestion05', 5)
+                    RETURNING id INTO v_question5_id;
+                    INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"", caption, summary_title, ""name"", sort_order)
+                    VALUES('{Guid.NewGuid()}', NULL, NULL, v_section_id, 2, true, 'ModernSlavery_06_Title', '', '{{}}', NULL, 'ModernSlavery_06_SummaryTitle', '_ModernSlaveryQuestion06', 6)
+                    RETURNING id INTO v_question6_id;
+                    INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"", caption, summary_title, ""name"", sort_order)
+                    VALUES('{Guid.NewGuid()}', NULL, NULL, v_section_id, 12, true, 'ModernSlavery_07_Title', NULL, '{{}}', NULL, 'ModernSlavery_07_SummaryTitle', '_ModernSlaveryQuestion07', 7)
+                    RETURNING id INTO v_question7_id;
+                    INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"", caption, summary_title, ""name"", sort_order)
+                    VALUES('{Guid.NewGuid()}', NULL, NULL, v_section_id, 2, true, 'ModernSlavery_08_Title', NULL, '{{}}', NULL, 'ModernSlavery_08_SummaryTitle', '_ModernSlaveryQuestion08', 8)
+                    RETURNING id INTO v_question8_id;
+                    INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"", caption, summary_title, ""name"", sort_order)
+                    VALUES('{Guid.NewGuid()}', NULL, NULL, v_section_id, 10, true, 'ModernSlavery_09_Title', 'ModernSlavery_09_Description', '{{}}', NULL, 'ModernSlavery_09_SummaryTitle', '_ModernSlaveryQuestion09', 9)
+                    RETURNING id INTO v_question9_id;
+                    INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"", caption, summary_title, ""name"", sort_order)
+                    VALUES('{Guid.NewGuid()}', NULL, NULL, v_section_id, 10, true, 'ModernSlavery_10_Title', 'ModernSlavery_10_Description', '{{}}', NULL, 'ModernSlavery_10_SummaryTitle', '_ModernSlaveryQuestion10', 10)
+                    RETURNING id INTO v_question10_id;
+                    INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"", caption, summary_title, ""name"", sort_order)
+                    VALUES('{Guid.NewGuid()}', NULL, NULL, v_section_id, 10, true, 'ModernSlavery_11_Title', 'ModernSlavery_11_Description', '{{}}', NULL, 'ModernSlavery_11_SummaryTitle', '_ModernSlaveryQuestion11', 11)
+                    RETURNING id INTO v_question11_id;
+                    INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"", caption, summary_title, ""name"", sort_order)
+                    VALUES('{Guid.NewGuid()}', NULL, NULL, v_section_id, 6, true, 'Global_CheckYourAnswers', NULL, '{{}}', NULL, '', '_ModernSlaveryQuestion12', 12)
+                    RETURNING id INTO v_question12_id;
+
+                    -- Update the next question links
+                    UPDATE form_questions SET next_question_id = v_question2_id WHERE title = 'ModernSlavery_01_Title' AND section_id = v_section_id;
+                    UPDATE form_questions SET next_question_id = v_question3_id, next_question_alternative_id = v_question4_id WHERE title = 'ModernSlavery_02_Title' AND section_id = v_section_id;
+                    UPDATE form_questions SET next_question_id = v_question5_id, next_question_alternative_id = v_question6_id WHERE title = 'ModernSlavery_03_Title' AND section_id = v_section_id;
+                    UPDATE form_questions SET next_question_id = v_question7_id, next_question_alternative_id = v_question8_id WHERE title = 'ModernSlavery_04_Title' AND section_id = v_section_id;
+                    UPDATE form_questions SET next_question_id = v_question9_id WHERE title = 'ModernSlavery_05_Title' AND section_id = v_section_id;
+                    UPDATE form_questions SET next_question_id = v_question9_id WHERE title = 'ModernSlavery_06_Title' AND section_id = v_section_id;
+                    UPDATE form_questions SET next_question_id = v_question10_id WHERE title = 'ModernSlavery_07_Title' AND section_id = v_section_id;
+                    UPDATE form_questions SET next_question_id = v_question10_id WHERE title = 'ModernSlavery_08_Title' AND section_id = v_section_id;
+                    UPDATE form_questions SET next_question_id = v_question11_id WHERE title = 'ModernSlavery_09_Title' AND section_id = v_section_id;
+                    UPDATE form_questions SET next_question_id = v_question11_id WHERE title = 'ModernSlavery_10_Title' AND section_id = v_section_id;
+                    UPDATE form_questions SET next_question_id = v_question12_id WHERE title = 'ModernSlavery_11_Title' AND section_id = v_section_id;
                 END $$;
             ");
-        }
     }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.Sql($@"
+                DO $$
+                DECLARE
+                    v_section_id INTEGER;
+                BEGIN
+                    -- Get the ID of the section for Modern Slavery
+                    SELECT id INTO v_section_id FROM form_sections WHERE title = 'ModernSlavery_SectionTitle';
+
+                    IF v_section_id IS NOT NULL THEN
+                        -- Delete the questions associated with the section first
+                        DELETE FROM form_questions WHERE section_id = v_section_id;
+                        -- Delete the Modern Slavery section
+                        DELETE FROM form_sections WHERE id = v_section_id;
+                    END IF;
+                END $$;
+            ");
+    }
+}
 }
