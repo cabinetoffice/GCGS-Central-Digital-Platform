e138b55e43675f2c8c8a10abadf74b79398842e5
Edward Dennis <98972365+EdwardDennis@users.noreply.github.com>
2025-06-17 19:04:33 +0100

    Add feature flag/content for Search Registry PPON in OrganisationOverview

--- Frontend/CO.CDP.OrganisationApp.Tests/Pages/OrganisationOverviewTest.cs
+++ Frontend/CO.CDP.OrganisationApp.Tests/Pages/OrganisationOverviewTest.cs
@@ -1,8 +1,14 @@
+using CO.CDP.EntityVerificationClient;
 using CO.CDP.Organisation.WebApiClient;
+using CO.CDP.OrganisationApp.Constants;
 using CO.CDP.OrganisationApp.Pages.Organisation;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
+using Microsoft.FeatureManagement;
+using ApiException = CO.CDP.Organisation.WebApiClient.ApiException;
+using DevolvedRegulation = CO.CDP.Organisation.WebApiClient.DevolvedRegulation;
+using OrganisationType = CO.CDP.Organisation.WebApiClient.OrganisationType;
 
 namespace CO.CDP.OrganisationApp.Tests.Pages;
 
@@ -10,12 +16,14 @@ public class OrganisationOverviewTest
 {
     private readonly Mock<IOrganisationClient> _organisationClientMock;
     private readonly Mock<EntityVerificationClient.IPponClient> _pponClient = new();
+    private readonly Mock<IFeatureManager> _featureManagerMock;
     private readonly OrganisationOverviewModel _model;
 
     public OrganisationOverviewTest()
     {
         _organisationClientMock = new Mock<IOrganisationClient>();
-        _model = new OrganisationOverviewModel(_organisationClientMock.Object, _pponClient.Object);
+        _featureManagerMock = new Mock<IFeatureManager>();
+        _model = new OrganisationOverviewModel(_organisationClientMock.Object, _pponClient.Object, _featureManagerMock.Object);
     }
 
     [Fact]
@@ -139,7 +147,38 @@ public class OrganisationOverviewTest
         _model.HasBuyerSignedMou.Should().BeFalse();
     }
 
-    private static CO.CDP.Organisation.WebApiClient.Organisation GivenOrganisationClientModel(Guid? id, ICollection<PartyRole>? pendingRoles = null)
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public async Task OnGet_SetsIsSearchRegistryPponEnabled_BasedOnFeatureFlag(
+        bool featureFlagEnabled)
+    {
+        var id = Guid.NewGuid();
+        _model.Id = id;
+
+        _organisationClientMock.Setup(o => o.GetOrganisationAsync(id))
+            .ReturnsAsync(GivenOrganisationClientModel(id: id, roles: new List<PartyRole> { PartyRole.Buyer }));
+
+        _featureManagerMock.Setup(fm => fm.IsEnabledAsync(FeatureFlags.SearchRegistryPpon))
+            .ReturnsAsync(featureFlagEnabled);
+
+        _pponClient.Setup(p => p.GetIdentifierRegistriesDetailAsync(It.IsAny<string[]>()))
+            .ReturnsAsync(new List<IdentifierRegistries>());
+        _organisationClientMock.Setup(o => o.GetOrganisationBuyerInformationAsync(id))
+            .ReturnsAsync(new BuyerInformation("TestBuyerType", new List<CO.CDP.Organisation.WebApiClient.DevolvedRegulation>()));
+        _organisationClientMock.Setup(o => o.GetOrganisationLatestMouSignatureAsync(id))
+            .ThrowsAsync(new ApiException("Not Found", 404, "", default, null));
+
+        await _model.OnGet();
+
+        _model.SearchRegistryPponEnabled.Should().Be(featureFlagEnabled);
+        _featureManagerMock.Verify(fm => fm.IsEnabledAsync(FeatureFlags.SearchRegistryPpon), Times.Once);
+    }
+
+    private static CO.CDP.Organisation.WebApiClient.Organisation GivenOrganisationClientModel(
+        Guid? id,
+        ICollection<PartyRole>? roles = null,
+        ICollection<PartyRole>? pendingRoles = null)
     {
         return new CO.CDP.Organisation.WebApiClient.Organisation(
             additionalIdentifiers: null,
@@ -149,8 +188,15 @@ public class OrganisationOverviewTest
             identifier: null,
             name: "Test Org",
             type: OrganisationType.Organisation,
-            roles: [],
-            details: new Details(approval: null, buyerInformation: null, pendingRoles: pendingRoles != null ? pendingRoles : [], publicServiceMissionOrganization: null, scale: null, shelteredWorkshop: null, vcse: null)
-              );
+            roles: roles ?? new List<PartyRole>(),
+            details: new Details(
+                approval: null,
+                buyerInformation: null,
+                pendingRoles: pendingRoles ?? new List<PartyRole>(),
+                publicServiceMissionOrganization: null,
+                scale: null,
+                shelteredWorkshop: null,
+                vcse: null)
+        );
     }
 }
