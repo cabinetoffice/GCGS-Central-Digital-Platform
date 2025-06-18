fc48af98a904357e9f8b92c73246d0aec9fcd3ea
Sohail-Saiedi-EY <98962319+Sohail-Saiedi-EY@users.noreply.github.com>
2025-06-18 13:06:40 +0100

    completed branching logic for cyber essentials

--- Services/CO.CDP.OrganisationInformation.Persistence/Migrations/20250611082829_CyberEssentialsSectionForm.cs
+++ Services/CO.CDP.OrganisationInformation.Persistence/Migrations/20250611082829_CyberEssentialsSectionForm.cs
@@ -13,56 +13,80 @@ namespace CO.CDP.OrganisationInformation.Persistence.Migrations
             migrationBuilder.Sql($@"
                 DO $$
                 DECLARE
-                    form_id int;
-                    sectionId INT;
-                    previousQuestionId INT;
+                    v_form_id INTEGER;
+                    v_section_id INTEGER;
+                    v_question1_id INTEGER;
+                    v_question2_id INTEGER;
+                    v_question3_id INTEGER;
+                    v_question4_id INTEGER;
+                    v_question5_id INTEGER;
+                    v_question6_id INTEGER;
+                    v_question7_id INTEGER;
+                    v_question8_id INTEGER;
+                    v_question9_id INTEGER;
+                    v_question10_id INTEGER;
                 BEGIN
-                    SELECT id INTO form_id FROM forms WHERE name = 'Standard Questions';
+                    SELECT id INTO v_form_id FROM forms WHERE name = 'Standard Questions';
+
+                    IF v_form_id IS NULL THEN
+                        RAISE EXCEPTION 'Form ""Standard Questions"" not found. Migration cannot proceed.';
+                    END IF;
 
                     INSERT INTO form_sections (guid, title, form_id, allows_multiple_answer_sets, check_further_questions_exempted, type,display_order, configuration)
-                    VALUES ('{Guid.NewGuid()}', 'CyberEssentials_SectionTitle', form_id, FALSE, FALSE, 3,1, '{{""AddAnotherAnswerLabel"": ""CyberEssentials_Configuration_AddAnotherAnswerLabel"", ""SingularSummaryHeading"": ""CyberEssentials_Configuration_SingularSummaryHeading"", ""SummaryRenderFormatter"": {{""KeyParams"": [""_CyberEssentialsZeroQuestion01"", ""_CyberEssentials02""], ""ValueParams"": [""_CyberEssentials01"", ""_CyberEssentials02""], ""KeyExpression"": ""{{0}}"", ""ValueExpression"": ""{{1}}"", ""KeyExpressionOperation"": ""StringFormat"", ""ValueExpressionOperation"": ""StringFormat""}}, ""RemoveConfirmationCaption"": ""CyberEssentials_SectionTitle"", ""RemoveConfirmationHeading"": ""CyberEssentials_Configuration_RemoveConfirmationHeading"", ""PluralSummaryHeadingFormat"": ""CyberEssentials_Configuration_PluralSummaryHeadingFormat"", ""SingularSummaryHeadingHint"": ""CyberEssentials_Configuration_SingularSummaryHeadingHint"", ""FurtherQuestionsExemptedHint"": ""CyberEssentials_Configuration_FurtherQuestionsExemptedHint"", ""PluralSummaryHeadingHintFormat"": ""CyberEssentials_Configuration_PluralSummaryHeadingHintFormat"", ""FurtherQuestionsExemptedHeading"": ""CyberEssentials_Configuration_FurtherQuestionsExemptedHeading""}}');
-
-                    SELECT id INTO sectionId FROM form_sections where title = 'CyberEssentials_SectionTitle';
-
-                    INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"",  caption, summary_title, ""name"", sort_order)
-                    VALUES('{Guid.NewGuid()}', NULL, NULL, sectionId, 6, true, 'Global_CheckYourAnswers', '', '{{}}', NULL, NULL, '_CyberEssentials10', 10)
-                    RETURNING id INTO previousQuestionId;
+                    VALUES ('{Guid.NewGuid()}', 'CyberEssentials_SectionTitle', v_form_id, FALSE, FALSE, 3,1, '{{}}')
+                    RETURNING id INTO v_section_id;
 
                     INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"",  caption, summary_title, ""name"", sort_order)
-                    VALUES('{Guid.NewGuid()}', previousQuestionId, NULL, sectionId, 7, true, 'CyberEssentials_09_Title', NULL, '{{}}', NULL, 'CyberEssentials_09_SummaryTitle', '_CyberEssentials09', 9)
-                    RETURNING id INTO previousQuestionId;
+                    VALUES('{Guid.NewGuid()}', NULL, NULL, v_section_id, 0, true, 'CyberEssentials_01_Title', 'CyberEssentials_01_Description', '{{}}', NULL, NULL, '_CyberEssentials01', 1)
+                    RETURNING id INTO v_question1_id;
 
                     INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"",  caption, summary_title, ""name"", sort_order)
-                    VALUES('{Guid.NewGuid()}', previousQuestionId, NULL, sectionId, 2, true, 'CyberEssentials_08_Title', NULL, '{{}}', NULL, 'CyberEssentials_08_SummaryTitle', '_CyberEssentials08', 8)
-                    RETURNING id INTO previousQuestionId;
+                    VALUES('{Guid.NewGuid()}', NULL, NULL, v_section_id, 3, true, 'CyberEssentials_02_Title', NULL, '{{}}', NULL, 'CyberEssentials_02_SummaryTitle', '_CyberEssentials02', 2)
+                    RETURNING id INTO v_question2_id;
 
                     INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"",  caption, summary_title, ""name"", sort_order)
-                    VALUES('{Guid.NewGuid()}', previousQuestionId, NULL, sectionId, 3, true, 'CyberEssentials_07_Title', '', '{{}}', NULL, 'CyberEssentials_07_SummaryTitle', '_CyberEssentials07', 7)
-                    RETURNING id INTO previousQuestionId;
+                    VALUES('{Guid.NewGuid()}', NULL, NULL, v_section_id, 3, true, 'CyberEssentials_03_Title', NULL, '{{}}', NULL, 'CyberEssentials_03_SummaryTitle', '_CyberEssentials03', 3)
+                    RETURNING id INTO v_question3_id;
 
                     INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"",  caption, summary_title, ""name"", sort_order)
-                    VALUES('{Guid.NewGuid()}', previousQuestionId, NULL, sectionId, 3, true, 'CyberEssentials_06_Title', '', '{{}}', NULL, 'CyberEssentials_06_SummaryTitle', '_CyberEssentials06', 6)
-                    RETURNING id INTO previousQuestionId;
+                    VALUES('{Guid.NewGuid()}', NULL, NULL, v_section_id, 2, true, 'CyberEssentials_04_Title', NULL, '{{}}', NULL, 'CyberEssentials_04_SummaryTitle', '_CyberEssentials04', 4)
+                    RETURNING id INTO v_question4_id;
 
                     INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"",  caption, summary_title, ""name"", sort_order)
-                    VALUES('{Guid.NewGuid()}', previousQuestionId, NULL, sectionId, 7, true, 'CyberEssentials_05_Title', NULL, '{{}}', NULL, 'CyberEssentials_05_SummaryTitle', '_CyberEssentials05', 5)
-                    RETURNING id INTO previousQuestionId;
+                    VALUES('{Guid.NewGuid()}', NULL, NULL, v_section_id, 7, true, 'CyberEssentials_05_Title', NULL, '{{}}', NULL, 'CyberEssentials_05_SummaryTitle', '_CyberEssentials05', 5)
+                    RETURNING id INTO v_question5_id;
 
                     INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"",  caption, summary_title, ""name"", sort_order)
-                    VALUES('{Guid.NewGuid()}', previousQuestionId, NULL, sectionId, 2, true, 'CyberEssentials_04_Title', NULL, '{{}}', NULL, 'CyberEssentials_04_SummaryTitle', '_CyberEssentials04', 4)
-                    RETURNING id INTO previousQuestionId;
+                    VALUES('{Guid.NewGuid()}', NULL, NULL, v_section_id, 3, true, 'CyberEssentials_06_Title', NULL, '{{}}', NULL, 'CyberEssentials_06_SummaryTitle', '_CyberEssentials06', 6)
+                    RETURNING id INTO v_question6_id;
 
                     INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"",  caption, summary_title, ""name"", sort_order)
-                    VALUES('{Guid.NewGuid()}', previousQuestionId, NULL, sectionId, 3, true, 'CyberEssentials_03_Title', NULL, '{{}}', NULL, 'CyberEssentials_03_SummaryTitle', '_CyberEssentials03', 3)
-                    RETURNING id INTO previousQuestionId;
+                    VALUES('{Guid.NewGuid()}', NULL, NULL, v_section_id, 3, true, 'CyberEssentials_07_Title', NULL, '{{}}', NULL, 'CyberEssentials_07_SummaryTitle', '_CyberEssentials07', 7)
+                    RETURNING id INTO v_question7_id;
 
                     INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"",  caption, summary_title, ""name"", sort_order)
-                    VALUES('{Guid.NewGuid()}', previousQuestionId, NULL, sectionId, 3, true, 'CyberEssentials_02_Title', NULL, '{{}}', NULL, 'CyberEssentials_02_SummaryTitle', '_CyberEssentials02', 2)
-                    RETURNING id INTO previousQuestionId;
+                    VALUES('{Guid.NewGuid()}', NULL, NULL, v_section_id, 2, true, 'CyberEssentials_08_Title', NULL, '{{}}', NULL, 'CyberEssentials_08_SummaryTitle', '_CyberEssentials08', 8)
+                    RETURNING id INTO v_question8_id;
 
                     INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"",  caption, summary_title, ""name"", sort_order)
-                    VALUES('{Guid.NewGuid()}', previousQuestionId, NULL, sectionId, 0, true, 'CyberEssentials_01_Title', 'CyberEssentials_01_Description', '{{}}', NULL, NULL, '_CyberEssentials01', 1)
-                    RETURNING id INTO previousQuestionId;
+                    VALUES('{Guid.NewGuid()}', NULL, NULL, v_section_id, 7, true, 'CyberEssentials_09_Title', NULL, '{{}}', NULL, 'CyberEssentials_09_SummaryTitle', '_CyberEssentials09', 9)
+                    RETURNING id INTO v_question9_id;
+
+                    INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"",  caption, summary_title, ""name"", sort_order)
+                    VALUES('{Guid.NewGuid()}', NULL, NULL, v_section_id, 6, true, 'Global_CheckYourAnswers', NULL, '{{}}', NULL, NULL, '_CyberEssentials10', 10)
+                    RETURNING id INTO v_question10_id;
+
+                    -- Update the next question links
+                    UPDATE form_questions SET next_question_id = v_question2_id WHERE title = 'CyberEssentials_01_Title' AND section_id = v_section_id;
+                    UPDATE form_questions SET next_question_id = v_question3_id, next_question_alternative_id = v_question10_id WHERE title = 'CyberEssentials_02_Title' AND section_id = v_section_id;
+                    UPDATE form_questions SET next_question_id = v_question4_id, next_question_alternative_id = v_question6_id WHERE title = 'CyberEssentials_03_Title' AND section_id = v_section_id;
+                    UPDATE form_questions SET next_question_id = v_question5_id WHERE title = 'CyberEssentials_04_Title' AND section_id = v_section_id;
+                    UPDATE form_questions SET next_question_id = v_question6_id WHERE title = 'CyberEssentials_05_Title' AND section_id = v_section_id;
+                    UPDATE form_questions SET next_question_id = v_question7_id, next_question_alternative_id = v_question10_id WHERE title = 'CyberEssentials_06_Title' AND section_id = v_section_id;
+                    UPDATE form_questions SET next_question_id = v_question8_id, next_question_alternative_id = v_question10_id WHERE title = 'CyberEssentials_07_Title' AND section_id = v_section_id;
+                    UPDATE form_questions SET next_question_id = v_question9_id WHERE title = 'CyberEssentials_08_Title' AND section_id = v_section_id;
+                    UPDATE form_questions SET next_question_id = v_question10_id WHERE title = 'CyberEssentials_09_Title' AND section_id = v_section_id;
+
 
                 END $$;
             ");
@@ -75,13 +99,20 @@ namespace CO.CDP.OrganisationInformation.Persistence.Migrations
             migrationBuilder.Sql($@"
                 DO $$
                 DECLARE
-                    sectionId INT;
+                    v_section_id INTEGER;
                 BEGIN
-                    SELECT id INTO sectionId FROM form_sections WHERE title = 'CyberEssentials_SectionTitle';
-                    DELETE FROM form_questions WHERE section_id = sectionId;
-                    DELETE FROM form_sections WHERE id = sectionId;
+                    -- Get the ID of the section for Cyber Essentials
+                    SELECT id INTO v_section_id FROM form_sections WHERE title = 'CyberEssentials_SectionTitle';
+
+                    IF v_section_id IS NOT NULL THEN
+                        -- Delete the questions associated with the section first
+                        DELETE FROM form_questions WHERE section_id = v_section_id;
+                        -- Delete the Cyber Essentials section
+                        DELETE FROM form_sections WHERE id = v_section_id;
+                    END IF;
                 END $$;
             ");
         }
     }
 }
+
