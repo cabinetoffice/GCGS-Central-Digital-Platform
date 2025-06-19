6ce669928091f7661aa6fc365a528c2042858db7
Ali Bahman <abn@webit4.me>
2025-06-19 22:45:58 +0100

    DP-1540 Stitch Sirsi and FTS versions into single SSM param for notifications (#1645)

--- terragrunt/modules/orchestrator/common/locals.tf
+++ terragrunt/modules/orchestrator/common/locals.tf
@@ -1,11 +1,31 @@
 locals {
   envs_service_version_sirsi = jsonencode({
-    for env, version in var.pinned_service_versions_sirsi : env => coalesce(version, data.aws_ssm_parameter.service_version_sirsi.value)
+    for env, version in var.pinned_service_versions_sirsi : env =>
+    coalesce(version, data.aws_ssm_parameter.service_version_sirsi.value)
   })
 
   envs_service_version_fts = jsonencode({
-    for env, version in var.pinned_service_versions_fts : env => coalesce(version, data.aws_ssm_parameter.service_version_fts.value)
+    for env, version in var.pinned_service_versions_fts : env =>
+    coalesce(version, data.aws_ssm_parameter.service_version_fts.value)
   })
+
+  envs_combined_versions = jsonencode({
+    for env in ["development", "staging", "integration", "production"] : env =>
+    "S:${coalesce(var.pinned_service_versions_sirsi[env], data.aws_ssm_parameter.service_version_sirsi.value)},F:${coalesce(var.pinned_service_versions_fts[env], data.aws_ssm_parameter.service_version_fts.value)}"
+  })
+
+  envs_service_versions = jsonencode({
+    sirsi = {
+      for env, version in var.pinned_service_versions_sirsi : env =>
+      coalesce(version, data.aws_ssm_parameter.service_version_sirsi.value)
+    }
+    fts = {
+      for env, version in var.pinned_service_versions_fts : env =>
+      coalesce(version, data.aws_ssm_parameter.service_version_fts.value)
+    }
+  })
+
+
 
   name_prefix = var.product.resource_name
 }
