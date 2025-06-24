0f5066357ba7323663be50d273a70a5a62f87b5c
Edward Dennis <98972365+EdwardDennis@users.noreply.github.com>
2025-06-24 12:51:20 +0100

    Add Child Organisation Results Page for Buyer Parent-Child Relationship

---
+++ Frontend/CO.CDP.OrganisationApp/Pages/BuyerParentChildRelationship/ChildOrganisationResultsPage.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CO.CDP.OrganisationApp.Pages.BuyerParentChildRelationship;
+
+public class ChildOrganisationResultsPage : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
