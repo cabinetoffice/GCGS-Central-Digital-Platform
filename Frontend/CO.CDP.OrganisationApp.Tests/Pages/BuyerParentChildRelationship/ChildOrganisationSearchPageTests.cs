89f7b9b917f77afda0ef00bae625760ca7536135
Edward Dennis <98972365+EdwardDennis@users.noreply.github.com>
2025-06-24 11:58:55 +0100

    Add validation and error message to Child Organisation Search Page

--- Frontend/CO.CDP.OrganisationApp.Tests/Pages/BuyerParentChildRelationship/ChildOrganisationSearchPageTests.cs
+++ Frontend/CO.CDP.OrganisationApp.Tests/Pages/BuyerParentChildRelationship/ChildOrganisationSearchPageTests.cs
@@ -2,27 +2,53 @@ using CO.CDP.Organisation.WebApiClient;
 using CO.CDP.OrganisationApp.Pages.BuyerParentChildRelationship;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 using Moq;
 
 namespace CO.CDP.OrganisationApp.Tests.Pages.BuyerParentChildRelationship;
 
 public class ChildOrganisationSearchPageTests
 {
-    private readonly ChildOrganisationSearchPage _model;
+    private readonly ChildOrganisationSearchPage _page;
 
     public ChildOrganisationSearchPageTests()
     {
-        var organisationClientMock = new Mock<IOrganisationClient>();
-        _model = new ChildOrganisationSearchPage(organisationClientMock.Object);
+        var mockOrganisationClient = new Mock<IOrganisationClient>();
+        _page = new ChildOrganisationSearchPage(mockOrganisationClient.Object);
     }
 
     [Fact]
     public void OnGet_ReturnsPageResult()
     {
-        _model.Id = Guid.NewGuid();
+        _page.Id = Guid.NewGuid();
 
-        var result = _model.OnGet();
+        var result = _page.OnGet();
 
         result.Should().BeOfType<PageResult>();
+    }
+
+    [Fact]
+    public void OnPost_EmptyQuery_ReturnsBadRequest()
+    {
+        _page.Query = string.Empty;
+
+        var validationContext = new ValidationContext(_page);
+        var validationResults = new List<ValidationResult>();
+        Validator.TryValidateObject(_page, validationContext, validationResults, true);
+
+        foreach (var validationResult in validationResults)
+        {
+            foreach (var memberName in validationResult.MemberNames)
+            {
+                if (validationResult.ErrorMessage != null)
+                    _page.ModelState.AddModelError(memberName, validationResult.ErrorMessage);
+            }
+        }
+
+        var result = _page.OnPost();
+
+        result.Should().BeOfType<PageResult>();
+        _page.ModelState.IsValid.Should().BeFalse();
+        _page.ModelState.Should().ContainKey("Query");
     }
 }
--- Frontend/CO.CDP.OrganisationApp/Pages/BuyerParentChildRelationship/ChildOrganisationSearchPage.cshtml.cs
+++ Frontend/CO.CDP.OrganisationApp/Pages/BuyerParentChildRelationship/ChildOrganisationSearchPage.cshtml.cs
@@ -1,6 +1,8 @@
+using CO.CDP.Localization;
 using CO.CDP.Organisation.WebApiClient;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace CO.CDP.OrganisationApp.Pages.BuyerParentChildRelationship;
 
@@ -10,10 +12,21 @@ public class ChildOrganisationSearchPage(IOrganisationClient organisationClient)
     public Guid Id { get; set; }
 
     [BindProperty]
+    [Required(ErrorMessage = nameof(StaticTextResource.BuyerParentChildRelationship_SearchPage_Error))]
     public string? Query { get; set; }
 
     public IActionResult OnGet()
     {
+        return Page();
+    }
+
+    public IActionResult OnPost()
+    {
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+
         return Page();
     }
 }
