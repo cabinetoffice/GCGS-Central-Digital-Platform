47f0692f80db285aed14101b4328a7137516789f
Ali Bahman <abn@webit4.me>
2025-06-28 09:36:30 +0100

    DP-1518 Remove Fluentbit's Log Groups (#1670)

--- terragrunt/modules/ecs/locals.tf
+++ terragrunt/modules/ecs/locals.tf
@@ -59,8 +59,6 @@ locals {
     config.name
   ]
 
-  fts_log_groups = ["app", "clamav", "supervisor", "unhandled", "web"]
-
   waf_enabled = true
 
   # @TODO (ABN) DP-1747 Remove env-based logic and these locals as well as old source of FTS configs once migration is completed
