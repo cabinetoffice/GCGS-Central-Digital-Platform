15ed70ce225223e103eeed692db14faa352d2f7e
Edward Dennis <98972365+EdwardDennis@users.noreply.github.com>
2025-07-04 15:51:07 +0100

    Add HttpClient configuration for CommercialToolsApiClient

--- Frontend/CO.CDP.RegisterOfCommercialTools.App/Program.cs
+++ Frontend/CO.CDP.RegisterOfCommercialTools.App/Program.cs
@@ -1,3 +1,4 @@
+using CO.CDP.RegisterOfCommercialTools.WebApiClient;
 using GovUk.Frontend.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +6,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddGovUkFrontend();
+
+builder.Services.AddHttpClient<ICommercialToolsApiClient, CommercialToolsApiClient>(
+    (client) =>
+    {
+        var url = builder.Configuration.GetValue<string>("CommercialToolsApi:ServiceUrl")
+                    ?? throw new Exception("Missing CommercialToolsApi:ServiceUrl configuration.");
+        client.BaseAddress = new Uri(url);
+    });
 
 var app = builder.Build();
 
