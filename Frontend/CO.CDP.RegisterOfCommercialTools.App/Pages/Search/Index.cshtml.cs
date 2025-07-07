1c72fe720479de04fd344acfc825f2ceafba77da
Edward Dennis <98972365+EdwardDennis@users.noreply.github.com>
2025-07-07 14:41:07 +0100

    Add search page with Razor Pages implementation

---
+++ Frontend/CO.CDP.RegisterOfCommercialTools.App/Pages/Search/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CO.CDP.RegisterOfCommercialTools.App.Pages.Search
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
+
