83da162789bd481abdb3b5cde7ed21951f3ebc24
Edward Dennis <98972365+EdwardDennis@users.noreply.github.com>
2025-07-10 14:46:47 +0100

    Revert "Implement CPV children retrieval endpoint and corresponding use case with tests"
    This reverts commit bf5e547a7a6a5d46385f454652850e262796fa5d.

--- Services/CO.CDP.RegisterOfCommercialTools.WebApi.Tests/Api/CpvEndpointTest.cs
+++ Services/CO.CDP.RegisterOfCommercialTools.WebApi.Tests/Api/CpvEndpointTest.cs
@@ -1,25 +1,15 @@
 using System.Net;
-using System.Net.Http.Json;
-using CO.CDP.RegisterOfCommercialTools.WebApi.UseCases;
 using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.AspNetCore.TestHost;
-using Microsoft.Extensions.DependencyInjection;
-using Moq;
 
 namespace CO.CDP.RegisterOfCommercialTools.WebApi.Tests.Api;
 
 public class CpvEndpointTest
 {
     private readonly HttpClient _httpClient;
-    private readonly Mock<ICpvCodeRepository> _repositoryMock = new();
 
     public CpvEndpointTest()
     {
-        var webApplicationFactory = new WebApplicationFactory<Program>()
-            .WithWebHostBuilder(builder =>
-            {
-                builder.ConfigureTestServices(services => { services.AddSingleton(_repositoryMock.Object); });
-            });
+        var webApplicationFactory = new WebApplicationFactory<Program>();
         _httpClient = webApplicationFactory.CreateClient();
     }
 
@@ -31,24 +21,9 @@ public class CpvEndpointTest
     }
 
     [Fact]
-    public async Task GetCpvChildren_WhenCodeIsValid_ReturnsChildren()
+    public async Task GetCpvChildren_ReturnsNotImplemented()
     {
-        var cpvCodes = new List<CpvCode>
-        {
-            new("03000000-1", "Agricultural, farming, fishing, forestry and related products"),
-            new("03100000-2", "Agricultural and horticultural products"),
-            new("03200000-3", "Cereals, potatoes, vegetables, fruits and nuts"),
-            new("03300000-4", "Farming, hunting and fishing products"),
-            new("03400000-5", "Forestry and logging products"),
-        };
-        _repositoryMock.Setup(r => r.GetAllAsync()).ReturnsAsync(cpvCodes);
-
-        var response = await _httpClient.GetAsync("/filters/cpv/03000000/children");
-        response.EnsureSuccessStatusCode();
-
-        var children = await response.Content.ReadFromJsonAsync<List<CpvCode>>();
-
-        var expectedCodes = new[] { "03100000-2", "03200000-3", "03300000-4", "03400000-5" };
-        Assert.Equal(expectedCodes, children.Select(c => c.Code).ToList());
+        var response = await _httpClient.GetAsync("/filters/cpv/12345678/children");
+        Assert.Equal(HttpStatusCode.NotImplemented, response.StatusCode);
     }
 }
--- Services/CO.CDP.RegisterOfCommercialTools.WebApi/Api/CpvEndpoint.cs
+++ Services/CO.CDP.RegisterOfCommercialTools.WebApi/Api/CpvEndpoint.cs
@@ -1,6 +1,3 @@
-using CO.CDP.RegisterOfCommercialTools.WebApi.UseCases;
-using Microsoft.AspNetCore.Mvc;
-
 namespace CO.CDP.RegisterOfCommercialTools.WebApi.Api;
 
 public static class CpvEndpoint
@@ -10,12 +7,8 @@ public static class CpvEndpoint
         app.MapGet("/filters/cpv/search", () => Results.Problem(statusCode: StatusCodes.Status501NotImplemented))
             .Produces(StatusCodes.Status501NotImplemented);
 
-        app.MapGet("/filters/cpv/{code}/children", async (string code, [FromServices] GetCpvChildrenUseCase useCase) =>
-            {
-                var children = await useCase.ExecuteAsync(code);
-                return Results.Ok(children);
-            })
-            .Produces<IEnumerable<CpvCode>>(StatusCodes.Status200OK);
+        app.MapGet("/filters/cpv/{code}/children", (string code) => Results.Problem(statusCode: StatusCodes.Status501NotImplemented))
+            .Produces(StatusCodes.Status501NotImplemented);
 
         return app;
     }
