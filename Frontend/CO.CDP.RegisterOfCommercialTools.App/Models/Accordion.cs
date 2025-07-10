26ab389d6c14e6eabc7475edade7a071bbd49284
Edward Dennis <98972365+EdwardDennis@users.noreply.github.com>
2025-07-10 14:51:02 +0100

    Make properties of Accordion class required

--- Frontend/CO.CDP.RegisterOfCommercialTools.App/Models/Accordion.cs
+++ Frontend/CO.CDP.RegisterOfCommercialTools.App/Models/Accordion.cs
@@ -2,8 +2,8 @@ namespace CO.CDP.RegisterOfCommercialTools.App.Models
 {
     public class Accordion
     {
-        public string Id { get; set; }
-        public string Title { get; set; }
-        public Func<object, object> Content { get; set; }
+        public required string Id { get; set; }
+        public required string Title { get; set; }
+        public required Func<object, object> Content { get; set; }
     }
 }
