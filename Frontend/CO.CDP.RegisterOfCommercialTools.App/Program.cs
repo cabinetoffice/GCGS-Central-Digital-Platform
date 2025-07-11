667d35ed6398a72f778c39f65a9f73c6c4e55db5
Edward Dennis <98972365+EdwardDennis@users.noreply.github.com>
2025-07-11 21:18:51 +0100

    Refactor Program.cs to integrate UI Foundation services and enhance session management

--- Frontend/CO.CDP.RegisterOfCommercialTools.App/Program.cs
+++ Frontend/CO.CDP.RegisterOfCommercialTools.App/Program.cs
@@ -1,19 +1,25 @@
 using CO.CDP.RegisterOfCommercialTools.WebApiClient;
+using CO.CDP.UI.Foundation;
 using GovUk.Frontend.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddGovUkFrontend();
 
-builder.Services.AddHttpClient<ICommercialToolsApiClient, CommercialToolsApiClient>(
-    (client) =>
-    {
-        var url = builder.Configuration.GetValue<string>("CommercialToolsApi:ServiceUrl")
-                    ?? throw new Exception("Missing CommercialToolsApi:ServiceUrl configuration.");
-        client.BaseAddress = new Uri(url);
-    });
+builder.Services.AddUiFoundation(builder.Configuration, uiFoundationBuilder =>
+{
+    uiFoundationBuilder.AddSession(builder.Environment.IsDevelopment());
+    uiFoundationBuilder.AddFtsUrlService();
+    uiFoundationBuilder.AddCookiePreferences("ROCT");
+});
+
+builder.Services.AddHttpClient<ICommercialToolsApiClient, CommercialToolsApiClient>((client) =>
+{
+    var url = builder.Configuration.GetValue<string>("CommercialToolsApi:ServiceUrl")
+              ?? throw new Exception("Missing CommercialToolsApi:ServiceUrl configuration.");
+    client.BaseAddress = new Uri(url);
+});
 
 var app = builder.Build();
 
@@ -30,6 +36,8 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseGovUkFrontend();
+
+app.UseSession();
 
 app.UseAuthorization();
 
