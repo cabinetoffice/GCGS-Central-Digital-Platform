8767ec332fb057a58820642deba1cbf38cc42a34
Edward Dennis <98972365+EdwardDennis@users.noreply.github.com>
2025-07-12 14:36:41 +0100

    Refactor SearchModel by removing DisplayName attributes

--- Frontend/CO.CDP.RegisterOfCommercialTools.App/Pages/Search/Index.cshtml.cs
+++ Frontend/CO.CDP.RegisterOfCommercialTools.App/Pages/Search/Index.cshtml.cs
@@ -81,58 +81,43 @@ namespace CO.CDP.RegisterOfCommercialTools.App.Pages.Search
     [FeesValidator("FeeFrom", "FeeTo", "NoFees", ErrorMessage = "Fee values cannot be provided when 'No fees' is selected")]
     public class SearchModel
     {
-        [DisplayName("Keywords")]
         public string? Keywords { get; set; }
 
         public string? SortOrder { get; set; }
 
-        [DisplayName("Framework options")]
         public string? FrameworkOptions { get; set; }
 
-        [DisplayName("Dynamic market options")]
         public string? DynamicMarketOptions { get; set; }
 
-        [DisplayName("Commercial tool status")]
         public string? CommercialToolStatus { get; set; }
 
-        [DisplayName("Award method")]
         public string? AwardMethod { get; set; }
 
-        [DisplayName("Fee from")]
         [Range(0, 100, ErrorMessage = "Enter a value between 0 and 100")]
         public decimal? FeeFrom { get; set; }
 
-        [DisplayName("Fee to")]
         [Range(0, 100, ErrorMessage = "Enter a value between 0 and 100")]
         [DecimalRange("FeeFrom", ErrorMessage = "To must be more than from")]
         public decimal? FeeTo { get; set; }
 
-        [DisplayName("No fees")]
         public string? NoFees { get; set; }
 
         public List<string> Status { get; set; } = [];
 
-        [DisplayName("Contracting authority usage")]
         public string? ContractingAuthorityUsage { get; set; }
 
-        [DisplayName("Submission deadline from")]
         public DateOnly? SubmissionDeadlineFrom { get; set; }
 
-        [DisplayName("Submission deadline to")]
         [DateRange("SubmissionDeadlineFrom", ErrorMessage = "To date must be after from date")]
         public DateOnly? SubmissionDeadlineTo { get; set; }
 
-        [DisplayName("Contract start date from")]
         public DateOnly? ContractStartDateFrom { get; set; }
 
-        [DisplayName("Contract start date to")]
         [DateRange("ContractStartDateFrom", ErrorMessage = "To date must be after from date")]
         public DateOnly? ContractStartDateTo { get; set; }
 
-        [DisplayName("Contract end date from")]
         public DateOnly? ContractEndDateFrom { get; set; }
 
-        [DisplayName("Contract end date to")]
         [DateRange("ContractEndDateFrom", ErrorMessage = "To date must be after from date")]
         public DateOnly? ContractEndDateTo { get; set; }
     }
