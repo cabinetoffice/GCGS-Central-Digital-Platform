cc83a6f5331af0922eb579753f0fa0aef3a44a32
Edward Dennis <98972365+EdwardDennis@users.noreply.github.com>
2025-07-12 16:39:06 +0100

    Refactor Accordion class to use constructor for initialisation

--- Frontend/CO.CDP.RegisterOfCommercialTools.App/Models/Accordion.cs
+++ Frontend/CO.CDP.RegisterOfCommercialTools.App/Models/Accordion.cs
@@ -1,9 +1,8 @@
-namespace CO.CDP.RegisterOfCommercialTools.App.Models
+namespace CO.CDP.RegisterOfCommercialTools.App.Models;
+
+public class Accordion(string id, string title, Func<object, object> content)
 {
-    public class Accordion
-    {
-        public required string Id { get; set; }
-        public required string Title { get; set; }
-        public required Func<object, object> Content { get; set; }
-    }
+    public string Id { get; init; } = id ?? throw new ArgumentNullException(nameof(id));
+    public string Title { get; init; } = title ?? throw new ArgumentNullException(nameof(title));
+    public Func<object, object> Content { get; init; } = content ?? throw new ArgumentNullException(nameof(content));
 }
