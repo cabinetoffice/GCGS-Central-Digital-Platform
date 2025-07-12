b69a8f3b9f6a99f5ea8493396437bf347df72077
Ali Bahman <abn@webit4.me>
2025-07-12 11:57:41 +0100

    DP-1633 Add CFS Orchestrator componenets

--- terragrunt/modules/orchestrator/common/outputs.tf
+++ terragrunt/modules/orchestrator/common/outputs.tf
@@ -6,6 +6,14 @@ output "ssm_envs_combined_service_version_name" {
   value = aws_ssm_parameter.service_versions.name
 }
 
+output "ssm_envs_cfs_service_version_arn" {
+  value = aws_ssm_parameter.service_versions_cfs.arn
+}
+
+output "ssm_envs_cfs_service_version_name" {
+  value = aws_ssm_parameter.service_versions_cfs.name
+}
+
 output "ssm_envs_fts_service_version_arn" {
   value = aws_ssm_parameter.service_versions_fts.arn
 }
