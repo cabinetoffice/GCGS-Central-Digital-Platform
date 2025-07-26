6d393aaae1c8a952e08dd6713936d100a0d63934
Edward Dennis <98972365+EdwardDennis@users.noreply.github.com>
2025-07-26 19:15:12 +0100

    Add cognito auth to RoCT

--- Frontend/CO.CDP.RegisterOfCommercialTools.App/Program.cs
+++ Frontend/CO.CDP.RegisterOfCommercialTools.App/Program.cs
@@ -1,3 +1,4 @@
+using CO.CDP.RegisterOfCommercialTools.App;
 using CO.CDP.RegisterOfCommercialTools.App.Services;
 using CO.CDP.RegisterOfCommercialTools.WebApiClient;
 using CO.CDP.UI.Foundation;
@@ -6,7 +7,12 @@ using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddRazorPages();
+builder.Services.AddRazorPages(options =>
+{
+    options.Conventions.AuthorizeFolder("/");
+    options.Conventions.AllowAnonymousToPage("/Auth/Login");
+    options.Conventions.AllowAnonymousToPage("/Auth/Logout");
+});
 builder.Services.AddGovUkFrontend();
 builder.Services.AddHttpContextAccessor();
 
@@ -18,12 +24,37 @@ builder.Services.AddUiFoundation(builder.Configuration, uiFoundationBuilder =>
     uiFoundationBuilder.AddCookiePreferences("ROCT");
 });
 
+builder.Services.AddScoped<CO.CDP.RegisterOfCommercialTools.App.Handlers.BearerTokenHandler>();
+
 builder.Services.AddHttpClient<ICommercialToolsApiClient, CommercialToolsApiClient>((client) =>
 {
     var url = builder.Configuration.GetValue<string>("CommercialToolsApi:ServiceUrl")
               ?? throw new Exception("Missing CommercialToolsApi:ServiceUrl configuration.");
     client.BaseAddress = new Uri(url);
+})
+.AddHttpMessageHandler<CO.CDP.RegisterOfCommercialTools.App.Handlers.BearerTokenHandler>();
+
+var sessionTimeoutInMinutes = builder.Configuration.GetValue<double>("SessionTimeoutInMinutes", 30);
+var cookieSecurePolicy = builder.Environment.IsDevelopment() ? CookieSecurePolicy.SameAsRequest : CookieSecurePolicy.Always;
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(sessionTimeoutInMinutes);
+    options.Cookie.IsEssential = true;
+    options.Cookie.SameSite = SameSiteMode.Lax;
+    options.Cookie.SecurePolicy = cookieSecurePolicy;
 });
+
+builder.Services.AddCookiePolicy(options =>
+{
+    options.MinimumSameSitePolicy = SameSiteMode.Lax;
+    options.HttpOnly = Microsoft.AspNetCore.CookiePolicy.HttpOnlyPolicy.Always;
+    options.Secure = cookieSecurePolicy;
+});
+
+builder.Services.AddAwsCognitoAuthentication(builder.Configuration, builder.Environment);
+
+builder.Configuration.GetValue<string>("CommercialToolsApi:ServiceUrl");
 
 builder.Services.AddScoped<ISearchService, InMemorySearchService>();
 builder.Services.AddScoped<CO.CDP.UI.Foundation.Pages.NotFoundPage>();
@@ -38,22 +69,24 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+app.UseCookiePolicy();
 app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseGovUkFrontend();
+
+app.UseAuthentication();
+app.UseSession();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
 
 app.MapFallback(ctx =>
 {
     ctx.Response.Redirect("/page-not-found");
     return Task.CompletedTask;
 });
-
-app.UseRouting();
-
-app.UseGovUkFrontend();
-
-app.UseSession();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
 
 app.Run();
