5a97a57e868202f6d2bac64c80adebf4152e3ad5
Edward Dennis <98972365+EdwardDennis@users.noreply.github.com>
2025-07-30 00:19:42 +0100

    Remove multiple answer sets from additional supplier modules configuration in form sections

---
+++ Services/CO.CDP.OrganisationInformation.Persistence/Migrations/20250729224843_RemoveMultipleAnswerSetsFromASM.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CO.CDP.OrganisationInformation.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveMultipleAnswerSetsFromASM : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                UPDATE form_sections
+                SET ""configuration"" = '{}',
+                    ""allows_multiple_answer_sets"" = false,
+                    ""check_further_questions_exempted"" = false
+                WHERE ""title"" IN (
+                    'Steel_SectionTitle',
+                    'HealthAndSafety_SectionTitle',
+                    'ModernSlavery_SectionTitle',
+                    'CyberEssentials_SectionTitle',
+                    'DataProtection_SectionTitle',
+                    'CarbonNetZero_SectionTitle',
+                    'Payments_SectionTitle'
+                );
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // fixing broken data, not needed
+        }
+    }
+}
