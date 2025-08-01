1cbd6411a9d77e30c0469aa6ecbf8d4a343b175d
Edward Dennis <98972365+EdwardDennis@users.noreply.github.com>
2025-08-01 20:17:03 +0100

    Refactor answer rendering to group consecutive single questions and improve modularity

--- Frontend/CO.CDP.OrganisationApp/Pages/Forms/FormElementCheckYourAnswersModel.cs
+++ Frontend/CO.CDP.OrganisationApp/Pages/Forms/FormElementCheckYourAnswersModel.cs
@@ -6,6 +6,27 @@ public class FormElementCheckYourAnswersModel
 {
     public List<IAnswerDisplayItem> DisplayItems { get; set; } = [];
     public FormSectionType? FormSectionType { get; set; }
+
+    public IEnumerable<IAnswerDisplayItem> GroupedDisplayItems => 
+        DisplayItems.GroupConsecutiveSingleQuestions();
+}
+
+public static class DisplayItemExtensions
+{
+    public static IEnumerable<IAnswerDisplayItem> GroupConsecutiveSingleQuestions(this IEnumerable<IAnswerDisplayItem> items)
+    {
+        return items
+            .Aggregate(
+                new List<List<IAnswerDisplayItem>>(),
+                (groups, item) => item.IsGroup 
+                    ? groups.Append([item]).ToList()
+                    : groups.Any() && !groups.Last().First().IsGroup
+                        ? groups.Take(groups.Count - 1).Append(groups.Last().Append(item).ToList()).ToList()
+                        : groups.Append([item]).ToList())
+            .Select(group => group.First().IsGroup 
+                ? group.Single()
+                : new SingleQuestionsGroup { Answers = group.Cast<AnswerSummary>().ToList() });
+    }
 }
 
 public interface IAnswerDisplayItem
@@ -27,4 +48,10 @@ public class AnswerSummary : IAnswerDisplayItem
     public required string Answer { get; set; }
     public string? ChangeLink { get; set; }
     public bool IsGroup => false;
+}
+
+public class SingleQuestionsGroup : IAnswerDisplayItem
+{
+    public List<AnswerSummary> Answers { get; set; } = [];
+    public bool IsGroup => true;
 }
