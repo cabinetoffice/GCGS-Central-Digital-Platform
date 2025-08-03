0fca32fd5ded32db4f9fc421104c6cffbc815c71
Edward Dennis <98972365+EdwardDennis@users.noreply.github.com>
2025-08-04 00:12:48 +0100

    Add down method to migration

--- Services/CO.CDP.OrganisationInformation.Persistence/Migrations/20250729224843_RemoveMultipleAnswerSetsFromASM.cs
+++ Services/CO.CDP.OrganisationInformation.Persistence/Migrations/20250729224843_RemoveMultipleAnswerSetsFromASM.cs
@@ -11,26 +11,36 @@ namespace CO.CDP.OrganisationInformation.Persistence.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.Sql(@"
-                UPDATE form_sections
-                SET ""configuration"" = '{}',
-                    ""allows_multiple_answer_sets"" = false,
-                    ""check_further_questions_exempted"" = false
-                WHERE ""title"" IN (
-                    'Steel_SectionTitle',
-                    'HealthAndSafety_SectionTitle',
-                    'ModernSlavery_SectionTitle',
-                    'CyberEssentials_SectionTitle',
-                    'DataProtection_SectionTitle',
-                    'CarbonNetZero_SectionTitle',
-                    'Payments_SectionTitle'
-                );
+                DO $$
+                BEGIN
+                    UPDATE form_sections
+                    SET ""configuration"" = '{}',
+                        ""allows_multiple_answer_sets"" = false,
+                        ""check_further_questions_exempted"" = false
+                    WHERE ""title"" IN (
+                        'Steel_SectionTitle',
+                        'HealthAndSafety_SectionTitle',
+                        'ModernSlavery_SectionTitle',
+                        'CyberEssentials_SectionTitle',
+                        'DataProtection_SectionTitle',
+                        'CarbonNetZero_SectionTitle',
+                        'Payments_SectionTitle'
+                    );
+                END $$;
             ");
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            // fixing broken data, not needed
+            migrationBuilder.Sql(@"
+                DO $$
+                BEGIN
+                    UPDATE form_sections
+                    SET ""configuration"" = '{""AddAnotherAnswerLabel"": ""CarbonNetZero_Configuration_AddAnotherAnswerLabel"", ""SingularSummaryHeading"": ""CarbonNetZero_Configuration_SingularSummaryHeading"", ""SummaryRenderFormatter"": {""KeyParams"": [""_CarbonNetZeroQuestion01"", ""_CarbonNetZeroQuestion02""], ""ValueParams"": [""_CarbonNetZeroQuestion01"", ""_CarbonNetZeroQuestion02""], ""KeyExpression"": ""{0}"", ""ValueExpression"": ""{1}"", ""KeyExpressionOperation"": ""StringFormat"", ""ValueExpressionOperation"": ""StringFormat""}, ""RemoveConfirmationCaption"": ""CarbonNetZero_SectionTitle"", ""RemoveConfirmationHeading"": ""CarbonNetZero_Configuration_RemoveConfirmationHeading"", ""PluralSummaryHeadingFormat"": ""CarbonNetZero_Configuration_PluralSummaryHeadingFormat"", ""SingularSummaryHeadingHint"": ""CarbonNetZero_Configuration_SingularSummaryHeadingHint"", ""FurtherQuestionsExemptedHint"": ""CarbonNetZero_Configuration_FurtherQuestionsExemptedHint"", ""PluralSummaryHeadingHintFormat"": ""CarbonNetZero_Configuration_PluralSummaryHeadingHintFormat"", ""FurtherQuestionsExemptedHeading"": ""CarbonNetZero_Configuration_FurtherQuestionsExemptedHeading""}'
+                    WHERE ""title"" = 'CarbonNetZero_SectionTitle';
+                END $$;
+            ");
         }
     }
 }
