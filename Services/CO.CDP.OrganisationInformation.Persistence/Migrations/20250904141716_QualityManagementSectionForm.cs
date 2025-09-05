77abf1c5d9c716c259d69ba736c2d31ca80acb00
Tony D <tony.dickinson@uk.ey.com>
2025-09-05 10:11:57 +0100

    CDPDEL-946: Added Quality Management Module and Migration.

---
+++ Services/CO.CDP.OrganisationInformation.Persistence/Migrations/20250904141716_QualityManagementSectionForm.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CO.CDP.OrganisationInformation.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class QualityManagementSectionForm : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($@"
+                DO $$
+                DECLARE
+                    form_id int;
+                    sectionId INT;
+                    previousQuestionId INT;
+                BEGIN
+                    SELECT id INTO form_id FROM forms WHERE name = 'Standard Questions';
+
+	                INSERT INTO form_sections (guid, title, form_id, allows_multiple_answer_sets, check_further_questions_exempted, type,display_order, configuration)
+                    VALUES ('{Guid.NewGuid()}', 'QualityManagement_SectionTitle', form_id, FALSE, FALSE, 3,1, '{{""AddAnotherAnswerLabel"": ""QualityManagement_Configuration_AddAnotherAnswerLabel"", ""SingularSummaryHeading"": ""QualityManagement_Configuration_SingularSummaryHeading"", ""SummaryRenderFormatter"": {{""KeyParams"": [""_QualityManagementQuestion02"", ""_QualityManagementQuestion04""], ""ValueParams"": [""_QualityManagementQuestion02"", ""_QualityManagementQuestion04""], ""KeyExpression"": ""{{0}}"", ""ValueExpression"": ""{{1}}"", ""KeyExpressionOperation"": ""StringFormat"", ""ValueExpressionOperation"": ""StringFormat""}}, ""RemoveConfirmationCaption"": ""QualityManagement_SectionTitle"", ""RemoveConfirmationHeading"": ""QualityManagement_Configuration_RemoveConfirmationHeading"", ""PluralSummaryHeadingFormat"": ""QualityManagement_Configuration_PluralSummaryHeadingFormat"", ""SingularSummaryHeadingHint"": ""QualityManagement_Configuration_SingularSummaryHeadingHint"", ""FurtherQuestionsExemptedHint"": ""QualityManagement_Configuration_FurtherQuestionsExemptedHint"", ""PluralSummaryHeadingHintFormat"": ""QualityManagement_Configuration_PluralSummaryHeadingHintFormat"", ""FurtherQuestionsExemptedHeading"": ""QualityManagement_Configuration_FurtherQuestionsExemptedHeading""}}');
+
+                    SELECT id INTO sectionId FROM form_sections where title = 'QualityManagement_SectionTitle';
+
+                    INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"",  caption, summary_title, ""name"", sort_order)
+                    VALUES('{Guid.NewGuid()}', NULL, NULL, sectionId, 6, true, 'Global_CheckYourAnswers', '', '{{}}', NULL, NULL, '_QualityManagementQuestion04', 4)
+                    RETURNING id INTO previousQuestionId;
+
+                    INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"",  caption, summary_title, ""name"", sort_order)
+                    VALUES('{Guid.NewGuid()}', previousQuestionId, NULL, sectionId, 3, false, 'QualityManagementQuestion_03_Title', NULL, '{{}}', NULL, 'QualityManagementQuestion_03_SummaryTitle', '_QualityManagementQuestion03', 3)
+                    RETURNING id INTO previousQuestionId;
+
+                    INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"",  caption, summary_title, ""name"", sort_order)
+                    VALUES('{Guid.NewGuid()}', previousQuestionId, NULL, sectionId, 2, false, 'QualityManagementQuestion_02_Title', NULL, '{{""layout"": {{""button"" : {{""beforeButtonContent"": ""QualityManagementQuestion_02_CustomInsetText""}}}}}}', NULL, 'QualityManagementQuestion_02_SummaryTitle', '_QualityManagementQuestion02', 2)
+                    RETURNING id INTO previousQuestionId;
+
+                    INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"",  caption, summary_title, ""name"", sort_order)
+                    VALUES('{Guid.NewGuid()}', previousQuestionId, NULL, sectionId, 0, true, 'QualityManagementQuestion_01_Title', 'QualityManagementQuestion_01_Description', '{{}}', NULL, '', '_QualityManagementQuestion01', 1)
+                    RETURNING id INTO previousQuestionId;
+
+                END $$;
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($@"
+                DO $$
+                DECLARE
+                    sectionId INT;
+                BEGIN
+                    SELECT id INTO sectionId FROM form_sections WHERE title = 'QualityManagement_SectionTitle';
+                    DELETE FROM form_questions WHERE section_id = sectionId;
+                    DELETE FROM form_sections WHERE id = sectionId;
+                END $$;
+            ");
+        }
+    }
+}
