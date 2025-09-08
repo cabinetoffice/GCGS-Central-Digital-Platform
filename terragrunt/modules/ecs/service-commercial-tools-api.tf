846578c17581f1fc96b8f5f7f581b40d62fbc301
eraninventur <eran@inventur.uk>
2025-09-08 15:37:19 +0100

    CDPDEL-702 Add Comercial Tools Services (#1852)
    * Creating ECS Services Definitions and task definition templates for new services
    
    * Add support for Commercial Tools API and update related secrets management
    
    - Introduced new variables for OData API base URL and API key.
    - Updated task definition templates for Commercial Tools API and App to include new secrets.
    - Modified ECS service definitions to utilize new secrets and environment variables.
    - Enhanced README and manage-secrets documentation for clarity on ODI Data Platform secrets.
    
    * Update secrets management documentation and fix typos in ECS service configuration
    
    * commiting changes for story CDPDEL-702
    
    * removal of redundent files
    
    * updates to variables
    
    * update documentation
    
    * adding support for cognito login for commercial_tools_app service
    
    * adding missing service_versoin params to new services
    
    * CDPDEL-702 Tidy-up
    * CDPDEL-702 Remove AWS creds meant for LocalStack setup, in live accounts we must user service roles instead
    * CDPDEL-702 Remove AWS Service URL
    * CDPDEL-702 Remove AWS Cognito details being injected to the service
    * CDPDEL-702 Remove AWS SystemManager Data Protection Prefix
    * CDPDEL-702 Map ports (this might be improved)
    * CDPDEL-702 Scale down to 0 in all accounts, except development
    * CDPDEL-702 Correct Cognito URL
    
    ---------
    
    Co-authored-by: ezlgh <ezl@currencies.co.uk>
    Co-authored-by: Ali Bahman <abn@webit4.me>

---
+++ terragrunt/modules/ecs/service-commercial-tools-api.tf
@@ -0,0 +1,42 @@
+module "ecs_service_commercial_tools_api" {
+  source = "../ecs-service"
+
+  container_definitions = templatefile(
+    "${path.module}/templates/task-definitions/${var.service_configs.commercial_tools_api.name}.json.tftpl",
+    {
+      aspcore_environment = local.aspcore_environment
+      container_port      = var.service_configs.commercial_tools_api.port
+      cpu                 = var.service_configs.commercial_tools_api.cpu
+      host_port           = var.service_configs.commercial_tools_api.port
+      image               = local.ecr_urls[var.service_configs.commercial_tools_api.name]
+      lg_name             = aws_cloudwatch_log_group.tasks[var.service_configs.commercial_tools_api.name].name
+      lg_prefix           = "app"
+      lg_region           = data.aws_region.current.name
+      memory              = var.service_configs.commercial_tools_api.memory
+      name                = var.service_configs.commercial_tools_api.name
+      odataapi_baseurl    = "${data.aws_secretsmanager_secret.odi_data_platform_secret.arn}:BaseUrl::"
+      odataapi_apikey     = "${data.aws_secretsmanager_secret.odi_data_platform_secret.arn}:ApiKey::"
+      public_domain       = var.public_domain
+      service_version     = local.service_version_sirsi
+      vpc_cidr            = var.vpc_cider
+    }
+  )
+  cluster_id             = aws_ecs_cluster.this.id
+  container_port         = var.service_configs.commercial_tools_api.port
+  cpu                    = var.service_configs.commercial_tools_api.cpu
+  desired_count          = var.environment == "development" ? 1 : 0 // var.service_configs.commercial_tools_api.desired_count
+  ecs_alb_sg_id          = var.alb_sg_id
+  ecs_listener_arn       = aws_lb_listener.ecs.arn
+  ecs_service_base_sg_id = var.ecs_sg_id
+  family                 = "app"
+  host_port              = var.service_configs.commercial_tools_api.port_host
+  memory                 = var.service_configs.commercial_tools_api.memory
+  name                   = var.service_configs.commercial_tools_api.name
+  private_subnet_ids     = var.private_subnet_ids
+  product                = var.product
+  public_domain          = var.public_domain
+  role_ecs_task_arn      = var.role_ecs_task_arn
+  role_ecs_task_exec_arn = var.role_ecs_task_exec_arn
+  tags                   = var.tags
+  vpc_id                 = var.vpc_id
+}
---
+++ terragrunt/modules/ecs/service-commercial-tools-app.tf
@@ -0,0 +1,50 @@
+module "ecs_service_commercial_tools_app" {
+  source = "../ecs-service"
+
+  container_definitions = templatefile(
+    "${path.module}/templates/task-definitions/${var.service_configs.commercial_tools_app.name}.json.tftpl",
+    {
+      aspcore_environment            = local.aspcore_environment
+      container_port                 = var.service_configs.commercial_tools_app.port
+      cpu                            = var.service_configs.commercial_tools_app.cpu
+      host_port                      = var.service_configs.commercial_tools_app.port
+      image                          = local.ecr_urls[var.service_configs.commercial_tools_app.name]
+      lg_name                        = aws_cloudwatch_log_group.tasks[var.service_configs.organisation_app.name].name
+      lg_prefix                      = "app"
+      lg_region                      = data.aws_region.current.name
+      memory                         = var.service_configs.commercial_tools_app.memory
+      name                           = var.service_configs.commercial_tools_app.name
+      public_domain                  = var.public_domain
+      redis_auth_token_arn           = var.redis_auth_token_arn
+      redis_port                     = var.redis_port
+      redis_primary_endpoint_address = var.redis_primary_endpoint
+      service_version                = local.service_version_sirsi
+      sessiontimeoutinminutes        = var.commercial_tools_session_timeout
+      vpc_cidr                       = var.vpc_cider
+    }
+  )
+
+  cluster_id                    = aws_ecs_cluster.this.id
+  container_port                = var.service_configs.commercial_tools_app.port
+  cpu                           = var.service_configs.commercial_tools_app.cpu
+  desired_count                 = var.environment == "development" ? 2 : 0 // var.service_configs.commercial_tools_app.desired_count
+  ecs_alb_sg_id                 = var.alb_sg_id
+  ecs_listener_arn              = aws_lb_listener.ecs.arn
+  ecs_service_base_sg_id        = var.ecs_sg_id
+  family                        = "app"
+  host_port                     = var.service_configs.commercial_tools_app.port # this needs to stay and can't be same as host, 80
+  is_frontend_app               = true
+  memory                        = var.service_configs.commercial_tools_app.memory
+  name                          = var.service_configs.commercial_tools_app.name
+  private_subnet_ids            = var.private_subnet_ids
+  product                       = var.product
+  public_domain                 = var.public_domain
+  role_ecs_task_arn             = var.role_ecs_task_arn
+  role_ecs_task_exec_arn        = var.role_ecs_task_exec_arn
+  tags                          = var.tags
+  allowed_unauthenticated_paths = ["/one-login/back-channel-sign-out", "/assets/*", "/css/*", "/manifest.json"]
+  user_pool_arn                 = local.cognito_enabled ? var.user_pool_arn : null
+  user_pool_client_id           = local.cognito_enabled ? var.user_pool_client_id : null
+  user_pool_domain              = local.cognito_enabled ? var.user_pool_domain : null
+  vpc_id                        = var.vpc_id
+}
