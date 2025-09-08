b1d6ea7ad4c8c12403e0fd8379ef3b113e240a3f
Ali Bahman <abn@webit4.me>
2025-09-08 15:09:00 +0100

    CDPDEL-702 Correct Cognito URL

--- terragrunt/modules/auth/locals.tf
+++ terragrunt/modules/auth/locals.tf
@@ -40,7 +40,7 @@ locals {
   organisation_app_url    = "https://${var.public_domain}"
 
   commercial_tools_app_domain = "${local.auth_domain}-commercial_tools_app"
-  commercial_tools_app_url    = "https://${var.public_domain}"
+  commercial_tools_app_url    = "https://commercial-tools-api.${var.public_domain}"
 
   pgadmin_domain = "${local.auth_domain}-pgadmin"
   pgadmin_url    = "https://pgadmin.${var.public_domain}"
