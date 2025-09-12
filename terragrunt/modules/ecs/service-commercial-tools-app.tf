23dcc23c7720d8ead9ddf35f70610876a56b2402
Ali Bahman <abn@webit4.me>
2025-09-12 11:49:36 +0100

    CDPDEL-702 Fix deployment, by including missing variables

--- terragrunt/modules/ecs/service-commercial-tools-app.tf
+++ terragrunt/modules/ecs/service-commercial-tools-app.tf
@@ -4,31 +4,33 @@ module "ecs_service_commercial_tools_app" {
   container_definitions = templatefile(
     "${path.module}/templates/task-definitions/${var.service_configs.commercial_tools_app.name}.json.tftpl",
     {
-      aspcore_environment            = local.aspcore_environment
-      container_port                 = var.service_configs.commercial_tools_app.port
-      cpu                            = var.service_configs.commercial_tools_app.cpu
-      host_port                      = var.service_configs.commercial_tools_app.port
-      image                          = local.ecr_urls[var.service_configs.commercial_tools_app.name]
-      lg_name                        = aws_cloudwatch_log_group.tasks[var.service_configs.commercial_tools_app.name].name
-      lg_prefix                      = "app"
-      lg_region                      = data.aws_region.current.name
-      memory                         = var.service_configs.commercial_tools_app.memory
-      name                           = var.service_configs.commercial_tools_app.name
-      public_domain                  = var.public_domain
-      redis_auth_token_arn           = var.redis_auth_token_arn
-      redis_port                     = var.redis_port
-      redis_primary_endpoint_address = var.redis_primary_endpoint
-      service_version                = local.service_version_sirsi
-      sessiontimeoutinminutes        = var.commercial_tools_session_timeout
-      shared_sessions_enabled        = local.shared_sessions_enabled
-      ssm_data_protection_prefix     = local.ssm_data_protection_prefix
-      vpc_cidr                       = var.vpc_cider
+      aspcore_environment               = local.aspcore_environment
+      container_port                    = var.service_configs.commercial_tools_app.port
+      cpu                               = var.service_configs.commercial_tools_app.cpu
+      diagnostic_page_enabled           = !var.is_production || var.environment == "integration"
+      diagnostic_page_path_arn          = aws_secretsmanager_secret.cdp_sirsi_diagnostic_path.arn
+      host_port                         = var.service_configs.commercial_tools_app.port
+      image                             = local.ecr_urls[var.service_configs.commercial_tools_app.name]
+      lg_name                           = aws_cloudwatch_log_group.tasks[var.service_configs.commercial_tools_app.name].name
+      lg_prefix                         = "app"
+      lg_region                         = data.aws_region.current.name
+      memory                            = var.service_configs.commercial_tools_app.memory
+      name                              = var.service_configs.commercial_tools_app.name
       onelogin_account_url              = local.one_login.credential_locations.account_url
       onelogin_authority                = local.one_login.credential_locations.authority
       onelogin_client_id                = local.one_login.credential_locations.client_id
       onelogin_fln_api_key_arn          = data.aws_secretsmanager_secret.one_login_forward_logout_notification_api_key.arn
       onelogin_logout_notification_urls = local.onelogin_logout_notification_urls
       onelogin_private_key              = local.one_login.credential_locations.private_key
+      public_domain                     = var.public_domain
+      redis_auth_token_arn              = var.redis_auth_token_arn
+      redis_port                        = var.redis_port
+      redis_primary_endpoint_address    = var.redis_primary_endpoint
+      service_version                   = local.service_version_sirsi
+      sessiontimeoutinminutes           = var.commercial_tools_session_timeout
+      shared_sessions_enabled           = local.shared_sessions_enabled
+      ssm_data_protection_prefix        = local.ssm_data_protection_prefix
+      vpc_cidr                          = var.vpc_cider
     }
   )
 
