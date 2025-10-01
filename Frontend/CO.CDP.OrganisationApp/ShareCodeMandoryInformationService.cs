a5318b54a2b48c110ed426c15fe896ae6db88633
Tony D <tony.dickinson@uk.ey.com>
2025-10-01 11:57:24 +0100

    Include Exclusions form type in mandatory check

--- Frontend/CO.CDP.OrganisationApp/ShareCodeMandoryInformationService.cs
+++ Frontend/CO.CDP.OrganisationApp/ShareCodeMandoryInformationService.cs
@@ -32,7 +32,7 @@ public class ShareCodeMandatoryInformationService : IShareCodeMandatoryInformati
             return SupplierInformationStatus.GetBasicInfoStepStatus(supplierInfo) == SupplierInformationStatus.StepStatus.Completed
                 && SupplierInformationStatus.GetConnectedPersonStepStatus(supplierInfo, connectedEntities.Count) == SupplierInformationStatus.StepStatus.Completed
                 && formSections
-                    .Where(s => s.Type == FormSectionType.Standard)
+                    .Where(s => s.Type == FormSectionType.Standard || s.Type == FormSectionType.Exclusions)
                     .All(section => (section.AnswerSetCount != 0 || section.AnswerSetWithFurtherQuestionExemptedExists));
         }
         catch (Exception ex)
