f9819f11d373994c9f628e9aa110d25d1358d7e3
Tony D <tony.dickinson@uk.ey.com>
2025-10-02 15:59:47 +0100

    Updated sections to separate Multi Question and Single question pages on the summary page

--- Services/CO.CDP.OrganisationInformation.Persistence/Migrations/20250925133856_WelshHealthAndSafetySectionForm.cs
+++ Services/CO.CDP.OrganisationInformation.Persistence/Migrations/20250925133856_WelshHealthAndSafetySectionForm.cs
@@ -11,40 +11,46 @@ namespace CO.CDP.OrganisationInformation.Persistence.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             var healthAndSafety01GroupId = new Guid("12345678-1234-1234-1234-123456789001"); // Group 1: HealthAndSafety_02-03
-            var healthAndSafety02GroupId = new Guid("12345678-1234-1234-1234-123456789002"); // Group 2: HealthAndSafety_04-05
-            var healthAndSafety03GroupId = new Guid("12345678-1234-1234-1234-123456789003"); // Group 3: HealthAndSafety_06-07
-            var healthAndSafety04GroupId = new Guid("12345678-1234-1234-1234-123456789004"); // Group 4: HealthAndSafety_08-09
-            var healthAndSafety05GroupId = new Guid("12345678-1234-1234-1234-123456789005"); // Group 5: HealthAndSafety_10-11
+            var healthAndSafety02GroupId = new Guid("12345678-1234-1234-1234-123456789002"); // Group 2: HealthAndSafety_04
+            var healthAndSafety03GroupId = new Guid("12345678-1234-1234-1234-123456789003"); // Group 3: HealthAndSafety_05
+
+            var healthAndSafety04GroupId = new Guid("12345678-1234-1234-1234-123456789004"); // Group 4: HealthAndSafety_06
+            var healthAndSafety05GroupId = new Guid("12345678-1234-1234-1234-123456789005"); // Group 5: HealthAndSafety_07
+
+            var healthAndSafety06GroupId = new Guid("12345678-1234-1234-1234-123456789006"); // Group 6: HealthAndSafety_08-09
+            var healthAndSafety07GroupId = new Guid("12345678-1234-1234-1234-123456789007"); // Group 7: HealthAndSafety_10-11
 
             var healthAndSafety01GroupSummaryTitle = "WelshHealthAndSafetyQuestion_Group1_SummaryTitle";
             var healthAndSafety02GroupSummaryTitle = "WelshHealthAndSafetyQuestion_Group2_SummaryTitle";
             var healthAndSafety03GroupSummaryTitle = "WelshHealthAndSafetyQuestion_Group3_SummaryTitle";
             var healthAndSafety04GroupSummaryTitle = "WelshHealthAndSafetyQuestion_Group4_SummaryTitle";
             var healthAndSafety05GroupSummaryTitle = "WelshHealthAndSafetyQuestion_Group5_SummaryTitle";
+            var healthAndSafety06GroupSummaryTitle = "WelshHealthAndSafetyQuestion_Group6_SummaryTitle";
+            var healthAndSafety07GroupSummaryTitle = "WelshHealthAndSafetyQuestion_Group7_SummaryTitle";
 
             // Base JSON for grouping structures
             var healthAndSafety01GroupingJsonFragment = $"\"grouping\": {{ \"id\": \"{healthAndSafety01GroupId}\", \"page\": false, \"checkYourAnswers\": true, \"summaryTitle\": \"{healthAndSafety01GroupSummaryTitle}\" }}";
             var healthAndSafety02GroupingSingleQuestionJsonFragment = $"\"grouping\": {{ \"id\": \"{healthAndSafety02GroupId}\", \"page\": false, \"checkYourAnswers\": true, \"summaryTitle\": \"{healthAndSafety02GroupSummaryTitle}\" }}";
-            var healthAndSafety02GroupingMultiQuestionJsonFragment = $"\"grouping\": {{ \"id\": \"{healthAndSafety02GroupId}\", \"page\": true, \"checkYourAnswers\": true, \"summaryTitle\": \"{healthAndSafety02GroupSummaryTitle}\" }}";
-            var healthAndSafety03GroupingSingleQuestionJsonFragment = $"\"grouping\": {{ \"id\": \"{healthAndSafety03GroupId}\", \"page\": false, \"checkYourAnswers\": true, \"summaryTitle\": \"{healthAndSafety03GroupSummaryTitle}\" }}";
             var healthAndSafety03GroupingMultiQuestionJsonFragment = $"\"grouping\": {{ \"id\": \"{healthAndSafety03GroupId}\", \"page\": true, \"checkYourAnswers\": true, \"summaryTitle\": \"{healthAndSafety03GroupSummaryTitle}\" }}";
-            var healthAndSafety04GroupingJsonFragment = $"\"grouping\": {{ \"id\": \"{healthAndSafety04GroupId}\", \"page\": false, \"checkYourAnswers\": true, \"summaryTitle\": \"{healthAndSafety04GroupSummaryTitle}\" }}";
-            var healthAndSafety05GroupingJsonFragment = $"\"grouping\": {{ \"id\": \"{healthAndSafety05GroupId}\", \"page\": false, \"checkYourAnswers\": true, \"summaryTitle\": \"{healthAndSafety05GroupSummaryTitle}\" }}";
+            var healthAndSafety04GroupingSingleQuestionJsonFragment = $"\"grouping\": {{ \"id\": \"{healthAndSafety04GroupId}\", \"page\": false, \"checkYourAnswers\": true, \"summaryTitle\": \"{healthAndSafety04GroupSummaryTitle}\" }}";
+            var healthAndSafety05GroupingMultiQuestionJsonFragment = $"\"grouping\": {{ \"id\": \"{healthAndSafety05GroupId}\", \"page\": true, \"checkYourAnswers\": true, \"summaryTitle\": \"{healthAndSafety05GroupSummaryTitle}\" }}";
+            var healthAndSafety06GroupingJsonFragment = $"\"grouping\": {{ \"id\": \"{healthAndSafety06GroupId}\", \"page\": false, \"checkYourAnswers\": true, \"summaryTitle\": \"{healthAndSafety06GroupSummaryTitle}\" }}";
+            var healthAndSafety07GroupingJsonFragment = $"\"grouping\": {{ \"id\": \"{healthAndSafety07GroupId}\", \"page\": false, \"checkYourAnswers\": true, \"summaryTitle\": \"{healthAndSafety07GroupSummaryTitle}\" }}";
 
             // JSON for regular grouped questions
             var healthAndSafety01GroupedOptionsJson = $"'{{ {healthAndSafety01GroupingJsonFragment} }}'";
             var healthAndSafety02SingleQuestionGroupedOptionsJson = $"'{{ {healthAndSafety02GroupingSingleQuestionJsonFragment} }}'";
-            var healthAndSafety02MultiQuestionGroupedOptionsJson = $"'{{ {healthAndSafety02GroupingMultiQuestionJsonFragment} }}'";
-            var healthAndSafety03SingleQuestionGroupedOptionsJson  = $"'{{ {healthAndSafety03GroupingSingleQuestionJsonFragment} }}'";
             var healthAndSafety03MultiQuestionGroupedOptionsJson = $"'{{ {healthAndSafety03GroupingMultiQuestionJsonFragment} }}'";
-            var healthAndSafety04GroupedOptionsJson = $"'{{ {healthAndSafety04GroupingJsonFragment} }}'";
-            var healthAndSafety05GroupedOptionsJson = $"'{{ {healthAndSafety05GroupingJsonFragment} }}'";
+            var healthAndSafety04SingleQuestionGroupedOptionsJson  = $"'{{ {healthAndSafety04GroupingSingleQuestionJsonFragment} }}'";
+            var healthAndSafety05MultiQuestionGroupedOptionsJson = $"'{{ {healthAndSafety05GroupingMultiQuestionJsonFragment} }}'";
+            var healthAndSafety06GroupedOptionsJson = $"'{{ {healthAndSafety06GroupingJsonFragment} }}'";
+            var healthAndSafety07GroupedOptionsJson = $"'{{ {healthAndSafety07GroupingJsonFragment} }}'";
 
             // JSON for HealthAndSafety_08 question options with inset text
-            var healthAndSafety08OptionsWithInsetTextJson = $"'{{\"layout\": {{\"button\" : {{\"beforeButtonContent\": \"WelshHealthAndSafetyQuestion_08_CustomInsetText\"}}}}, {healthAndSafety04GroupingJsonFragment} }}'";
+            var healthAndSafety08OptionsWithInsetTextJson = $"'{{\"layout\": {{\"button\" : {{\"beforeButtonContent\": \"WelshHealthAndSafetyQuestion_08_CustomInsetText\"}}}}, {healthAndSafety06GroupingJsonFragment} }}'";
 
             // JSON for HealthAndSafety_11 question options with inset text
-            var healthAndSafety11OptionsWithInsetTextJson = $"'{{\"layout\": {{\"button\" : {{\"beforeButtonContent\": \"WelshHealthAndSafetyQuestion_11_CustomInsetText\"}}}}, {healthAndSafety05GroupingJsonFragment} }}'";
+            var healthAndSafety11OptionsWithInsetTextJson = $"'{{\"layout\": {{\"button\" : {{\"beforeButtonContent\": \"WelshHealthAndSafetyQuestion_11_CustomInsetText\"}}}}, {healthAndSafety07GroupingJsonFragment} }}'";
 
 migrationBuilder.Sql($@"
         DO $$
@@ -93,39 +99,39 @@ migrationBuilder.Sql($@"
                     RETURNING id INTO q4Id;
 
                     INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"",  caption, summary_title, ""name"", sort_order)
-                    VALUES('{Guid.NewGuid()}', NULL, NULL, sectionId, 0, true, 'WelshHealthAndSafetyQuestion_05_Title', NULL, " + healthAndSafety02MultiQuestionGroupedOptionsJson + $@", NULL, '', '_WelshHealthAndSafetyQuestion05', 5)
+                    VALUES('{Guid.NewGuid()}', NULL, NULL, sectionId, 0, true, 'WelshHealthAndSafetyQuestion_05_Title', NULL, " + healthAndSafety03MultiQuestionGroupedOptionsJson + $@", NULL, '', '_WelshHealthAndSafetyQuestion05', 5)
                     RETURNING id INTO q5Id;
 
                     INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"",  caption, summary_title, ""name"", sort_order)
-                    VALUES('{Guid.NewGuid()}', NULL, NULL, sectionId, 1, true, 'WelshHealthAndSafetyQuestion_05_01_Title', NULL, " + healthAndSafety02MultiQuestionGroupedOptionsJson + $@", NULL, 'WelshHealthAndSafetyQuestion_05_01_SummaryTitle', '_WelshHealthAndSafetyQuestion05_1', 6)
+                    VALUES('{Guid.NewGuid()}', NULL, NULL, sectionId, 1, true, 'WelshHealthAndSafetyQuestion_05_01_Title', NULL, " + healthAndSafety03MultiQuestionGroupedOptionsJson + $@", NULL, 'WelshHealthAndSafetyQuestion_05_01_SummaryTitle', '_WelshHealthAndSafetyQuestion05_1', 6)
                     RETURNING id INTO q5Id_1_Id;
 
                     INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"",  caption, summary_title, ""name"", sort_order)
-                    VALUES('{Guid.NewGuid()}', NULL, NULL, sectionId, 1, true, 'WelshHealthAndSafetyQuestion_05_02_Title', NULL, " + healthAndSafety02MultiQuestionGroupedOptionsJson + $@", NULL, 'WelshHealthAndSafetyQuestion_05_02_SummaryTitle', '_WelshHealthAndSafetyQuestion05_2', 7)
+                    VALUES('{Guid.NewGuid()}', NULL, NULL, sectionId, 1, true, 'WelshHealthAndSafetyQuestion_05_02_Title', NULL, " + healthAndSafety03MultiQuestionGroupedOptionsJson + $@", NULL, 'WelshHealthAndSafetyQuestion_05_02_SummaryTitle', '_WelshHealthAndSafetyQuestion05_2', 7)
                     RETURNING id INTO q5Id_2_Id;
 
                     INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"",  caption, summary_title, ""name"", sort_order)
-                    VALUES('{Guid.NewGuid()}', NULL, NULL, sectionId, 10, true, 'WelshHealthAndSafetyQuestion_05_03_Title', NULL, " + healthAndSafety02MultiQuestionGroupedOptionsJson + $@", NULL, 'WelshHealthAndSafetyQuestion_05_03_SummaryTitle', '_WelshHealthAndSafetyQuestion05_3', 8)
+                    VALUES('{Guid.NewGuid()}', NULL, NULL, sectionId, 10, true, 'WelshHealthAndSafetyQuestion_05_03_Title', NULL, " + healthAndSafety03MultiQuestionGroupedOptionsJson + $@", NULL, 'WelshHealthAndSafetyQuestion_05_03_SummaryTitle', '_WelshHealthAndSafetyQuestion05_3', 8)
                     RETURNING id INTO q5Id_3_Id;
 
                     INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"",  caption, summary_title, ""name"", sort_order)
-                    VALUES('{Guid.NewGuid()}', NULL, NULL, sectionId, 3, true, 'WelshHealthAndSafetyQuestion_06_Title', 'WelshHealthAndSafetyQuestion_06_Description', " + healthAndSafety03SingleQuestionGroupedOptionsJson + $@", NULL, 'WelshHealthAndSafetyQuestion_06_SummaryTitle', '_WelshHealthAndSafetyQuestion06', 9)
+                    VALUES('{Guid.NewGuid()}', NULL, NULL, sectionId, 3, true, 'WelshHealthAndSafetyQuestion_06_Title', 'WelshHealthAndSafetyQuestion_06_Description', " + healthAndSafety04SingleQuestionGroupedOptionsJson + $@", NULL, 'WelshHealthAndSafetyQuestion_06_SummaryTitle', '_WelshHealthAndSafetyQuestion06', 9)
                     RETURNING id INTO q6Id;
 
                     INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"",  caption, summary_title, ""name"", sort_order)
-                    VALUES('{Guid.NewGuid()}', NULL, NULL, sectionId, 0, true, 'WelshHealthAndSafetyQuestion_07_Title', NULL, " + healthAndSafety03MultiQuestionGroupedOptionsJson + $@", NULL, '', '_WelshHealthAndSafetyQuestion07', 10)
+                    VALUES('{Guid.NewGuid()}', NULL, NULL, sectionId, 0, true, 'WelshHealthAndSafetyQuestion_07_Title', NULL, " + healthAndSafety05MultiQuestionGroupedOptionsJson + $@", NULL, '', '_WelshHealthAndSafetyQuestion07', 10)
                     RETURNING id INTO q7Id;
 
                     INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"",  caption, summary_title, ""name"", sort_order)
-                    VALUES('{Guid.NewGuid()}', NULL, NULL, sectionId, 1, true, 'WelshHealthAndSafetyQuestion_07_01_Title', NULL, " + healthAndSafety03MultiQuestionGroupedOptionsJson + $@", NULL, 'WelshHealthAndSafetyQuestion_07_01_SummaryTitle', '_WelshHealthAndSafetyQuestion07_1', 11)
+                    VALUES('{Guid.NewGuid()}', NULL, NULL, sectionId, 1, true, 'WelshHealthAndSafetyQuestion_07_01_Title', NULL, " + healthAndSafety05MultiQuestionGroupedOptionsJson + $@", NULL, 'WelshHealthAndSafetyQuestion_07_01_SummaryTitle', '_WelshHealthAndSafetyQuestion07_1', 11)
                     RETURNING id INTO q7Id_1_Id;
 
                     INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"",  caption, summary_title, ""name"", sort_order)
-                    VALUES('{Guid.NewGuid()}', NULL, NULL, sectionId, 1, true, 'WelshHealthAndSafetyQuestion_07_02_Title', NULL, " + healthAndSafety03MultiQuestionGroupedOptionsJson + $@", NULL, 'WelshHealthAndSafetyQuestion_07_02_SummaryTitle', '_WelshHealthAndSafetyQuestion07_2', 12)
+                    VALUES('{Guid.NewGuid()}', NULL, NULL, sectionId, 1, true, 'WelshHealthAndSafetyQuestion_07_02_Title', NULL, " + healthAndSafety05MultiQuestionGroupedOptionsJson + $@", NULL, 'WelshHealthAndSafetyQuestion_07_02_SummaryTitle', '_WelshHealthAndSafetyQuestion07_2', 12)
                     RETURNING id INTO q7Id_2_Id;
 
                     INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"",  caption, summary_title, ""name"", sort_order)
-                    VALUES('{Guid.NewGuid()}', NULL, NULL, sectionId, 10, true, 'WelshHealthAndSafetyQuestion_07_03_Title', NULL, " + healthAndSafety03MultiQuestionGroupedOptionsJson + $@", NULL, 'WelshHealthAndSafetyQuestion_07_03_SummaryTitle', '_WelshHealthAndSafetyQuestion07_3', 13)
+                    VALUES('{Guid.NewGuid()}', NULL, NULL, sectionId, 10, true, 'WelshHealthAndSafetyQuestion_07_03_Title', NULL, " + healthAndSafety05MultiQuestionGroupedOptionsJson + $@", NULL, 'WelshHealthAndSafetyQuestion_07_03_SummaryTitle', '_WelshHealthAndSafetyQuestion07_3', 13)
                     RETURNING id INTO q7Id_3_Id;
 
                     INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"",  caption, summary_title, ""name"", sort_order)
@@ -133,11 +139,11 @@ migrationBuilder.Sql($@"
                     RETURNING id INTO q8Id;
 
                     INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"",  caption, summary_title, ""name"", sort_order)
-                    VALUES('{Guid.NewGuid()}', NULL, NULL, sectionId, 3, true, 'WelshHealthAndSafetyQuestion_09_Title', NULL, " + healthAndSafety04GroupedOptionsJson + $@", NULL, 'WelshHealthAndSafetyQuestion_09_SummaryTitle', '_WelshHealthAndSafetyQuestion09', 15)
+                    VALUES('{Guid.NewGuid()}', NULL, NULL, sectionId, 3, true, 'WelshHealthAndSafetyQuestion_09_Title', NULL, " + healthAndSafety06GroupedOptionsJson + $@", NULL, 'WelshHealthAndSafetyQuestion_09_SummaryTitle', '_WelshHealthAndSafetyQuestion09', 15)
                     RETURNING id INTO q9Id;
 
                     INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"",  caption, summary_title, ""name"", sort_order)
-                    VALUES('{Guid.NewGuid()}', NULL, NULL, sectionId, 2, false, 'WelshHealthAndSafetyQuestion_10_Title', 'WelshHealthAndSafetyQuestion_10_Description', " + healthAndSafety05GroupedOptionsJson + $@", NULL, 'WelshHealthAndSafetyQuestion_10_SummaryTitle', '_WelshHealthAndSafetyQuestion10', 16)
+                    VALUES('{Guid.NewGuid()}', NULL, NULL, sectionId, 2, false, 'WelshHealthAndSafetyQuestion_10_Title', 'WelshHealthAndSafetyQuestion_10_Description', " + healthAndSafety07GroupedOptionsJson + $@", NULL, 'WelshHealthAndSafetyQuestion_10_SummaryTitle', '_WelshHealthAndSafetyQuestion10', 16)
                     RETURNING id INTO q10Id;
 
                     INSERT INTO form_questions (guid, next_question_id, next_question_alternative_id, section_id, ""type"", is_required, title, description, ""options"",  caption, summary_title, ""name"", sort_order)
