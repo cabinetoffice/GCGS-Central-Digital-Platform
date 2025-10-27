3449542d010a561928eae37ba78058ea71e9d74f
Edward Dennis <98972365+EdwardDennis@users.noreply.github.com>
2025-10-27 12:11:31 +0000

    Update UK country codes handling in migration script

--- Services/CO.CDP.RegisterOfCommercialTools.Persistence/Migrations/20251024155601_UpdateUkCountryCodes.cs
+++ Services/CO.CDP.RegisterOfCommercialTools.Persistence/Migrations/20251024155601_UpdateUkCountryCodes.cs
@@ -14,8 +14,20 @@ namespace CO.CDP.RegisterOfCommercialTools.Persistence.Migrations
                 "ALTER TABLE commercial_tools_nuts_codes DROP CONSTRAINT IF EXISTS fk_commercial_tools_nuts_codes_commercial_tools_nuts_codes_par;");
 
             migrationBuilder.Sql(@"
+                UPDATE commercial_tools_nuts_codes
+                SET parent_code = 'GB'
+                WHERE parent_code IN ('SCT', 'WLS', 'NIR');
+            ");
+
+            migrationBuilder.Sql(@"
+                UPDATE commercial_tools_nuts_codes
+                SET parent_code = 'ENG'
+                WHERE code IN ('UKC', 'UKD', 'UKE', 'UKF', 'UKG', 'UKH', 'UKI', 'UKJ', 'UKK')
+                AND parent_code = 'GB';
+            ");
+
+            migrationBuilder.Sql(@"
                 DELETE FROM commercial_tools_nuts_codes WHERE code IN ('SCT', 'WLS', 'NIR');
-                UPDATE commercial_tools_nuts_codes SET parent_code = 'ENG', level = level + 1 WHERE code IN ('UKC', 'UKD', 'UKE', 'UKF', 'UKG', 'UKH', 'UKI', 'UKJ', 'UKK');
             ");
 
             migrationBuilder.Sql(
@@ -33,12 +45,19 @@ namespace CO.CDP.RegisterOfCommercialTools.Persistence.Migrations
                 "ALTER TABLE commercial_tools_nuts_codes DROP CONSTRAINT IF EXISTS fk_commercial_tools_nuts_codes_commercial_tools_nuts_codes_par;");
 
             migrationBuilder.Sql(@"
-                UPDATE commercial_tools_nuts_codes SET parent_code = 'GB', level = level - 1 WHERE code IN ('UKC', 'UKD', 'UKE', 'UKF', 'UKG', 'UKH', 'UKI', 'UKJ', 'UKK');
                 INSERT INTO commercial_tools_nuts_codes (code, description_en, description_cy, parent_code, level, is_active, is_selectable, created_on, updated_on)
                 VALUES
                     ('SCT', 'Scotland', 'Yr Alban', 'GB', 2, true, true, NOW(), NOW()),
                     ('WLS', 'Wales', 'Cymru', 'GB', 2, true, true, NOW(), NOW()),
-                    ('NIR', 'Northern Ireland', 'Gogledd Iwerddon', 'GB', 2, true, true, NOW(), NOW());
+                    ('NIR', 'Northern Ireland', 'Gogledd Iwerddon', 'GB', 2, true, true, NOW(), NOW())
+                ON CONFLICT (code) DO NOTHING;
+            ");
+
+            migrationBuilder.Sql(@"
+                UPDATE commercial_tools_nuts_codes
+                SET parent_code = 'GB'
+                WHERE code IN ('UKC', 'UKD', 'UKE', 'UKF', 'UKG', 'UKH', 'UKI', 'UKJ', 'UKK')
+                AND parent_code = 'ENG';
             ");
 
             migrationBuilder.Sql(
