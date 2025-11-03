1ec85eadc67de81610b9a9ed19a31354e7aa4efa
Edward Dennis <98972365+EdwardDennis@users.noreply.github.com>
2025-11-03 10:59:33 +0000

    Fix error page route to be case-sensitive

--- Frontend/CO.CDP.RegisterOfCommercialTools.App/Program.cs
+++ Frontend/CO.CDP.RegisterOfCommercialTools.App/Program.cs
@@ -151,7 +151,7 @@ using (var scope = app.Services.CreateScope())
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
