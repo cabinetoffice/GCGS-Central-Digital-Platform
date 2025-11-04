6232ae9edae8ffc5aced9671b7656d12fa76113d
Edward Dennis <98972365+EdwardDennis@users.noreply.github.com>
2025-11-04 10:37:29 +0000

    Update callback URL and user pool client ID for commercial tools

--- terragrunt/modules/ecs/variables.tf
+++ terragrunt/modules/ecs/variables.tf
@@ -352,6 +352,10 @@ variable "user_pool_client_id" {
   type = string
 }
 
+variable "user_pool_commercial_tools_client_id" {
+  type = string
+}
+
 variable "user_pool_domain" {
   type = string
 }
