79967283e81555e6a99714414e79b6f02df1788a
markgorman98 <markgorman1998@gmail.com>
2025-11-13 10:19:02 +0000

    CDPDEL-1449: Replace Continue button with Start button on Payments Module first page (#2012)

---
+++ Services/CO.CDP.OrganisationInformation.Persistence/Migrations/20251112135347_UpdatePaymentsSectionForm.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CO.CDP.OrganisationInformation.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatePaymentsSectionForm : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                DO $$
+                BEGIN
+
+                    IF EXISTS (SELECT 1 FROM form_questions WHERE title = 'Payments_01_Title') THEN
+                        UPDATE form_questions
+                        SET ""options"" = '{""layout"": {""button"": {""style"": ""Start"", ""text"": ""Global_Start""}}}'::jsonb
+                        WHERE title = 'Payments_01_Title';
+                    END IF;
+
+                END $$;
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                DO $$
+                BEGIN
+
+                    IF EXISTS (SELECT 1 FROM form_questions WHERE title = 'Payments_01_Title') THEN
+                        UPDATE form_questions
+                        SET ""options"" = '{}'::jsonb
+                        WHERE title = 'Payments_01_Title';
+                    END IF;
+
+                END $$;
+            ");
+        }
+    }
+}
