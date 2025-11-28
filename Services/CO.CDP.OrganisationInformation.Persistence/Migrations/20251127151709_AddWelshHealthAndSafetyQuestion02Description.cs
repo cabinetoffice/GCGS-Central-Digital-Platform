4539235ecd0915366d3019e1d8c10be1aaee38c0
Mark Gorman <markgorman1998@gmail.com>
2025-11-28 12:11:31 +0000

    Add description for WelshHealthAndSafetyQuestion_02 and update link in WelshHealthAndSafetyQuestion_10_Description

---
+++ Services/CO.CDP.OrganisationInformation.Persistence/Migrations/20251127151709_AddWelshHealthAndSafetyQuestion02Description.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CO.CDP.OrganisationInformation.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddWelshHealthAndSafetyQuestion02Description : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            {
+                migrationBuilder.Sql(@"
+                UPDATE form_questions
+                SET description = 'WelshHealthAndSafetyQuestion_02_Description'
+                WHERE title = 'WelshHealthAndSafetyQuestion_02_Title';
+            ");
+            }
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                UPDATE form_questions
+                SET description = NULL
+                WHERE title = 'WelshHealthAndSafetyQuestion_02_Title';
+            ");
+        }
+    }
+}
