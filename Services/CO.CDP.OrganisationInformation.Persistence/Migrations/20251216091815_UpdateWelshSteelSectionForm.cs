aa51535f1f1818d4a998a81889b458e030116fea
Tony D <165309439+tony-739573@users.noreply.github.com>
2025-12-17 11:55:06 +0000

    Updates to the Welsh Steel Module following updated designs (#2062)

---
+++ Services/CO.CDP.OrganisationInformation.Persistence/Migrations/20251216091815_UpdateWelshSteelSectionForm.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CO.CDP.OrganisationInformation.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateWelshSteelSectionForm : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sql = $@"
+                DO $$
+                BEGIN
+                    UPDATE form_questions SET caption = 'WelshSteel_06_Caption' WHERE title = 'WelshSteel_06_Title';
+                    UPDATE form_questions SET caption = 'WelshSteel_07_Caption' WHERE title = 'WelshSteel_07_Title';
+                    UPDATE form_questions SET caption = 'WelshSteel_08_Caption' WHERE title = 'WelshSteel_08_Title';
+                END $$;
+            ";
+
+            migrationBuilder.Sql(sql);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            var sql = $@"
+                DO $$
+                BEGIN
+                    UPDATE form_questions SET caption = NULL WHERE title = 'WelshSteel_06_Title';
+                    UPDATE form_questions SET caption = NULL WHERE title = 'WelshSteel_07_Title';
+                    UPDATE form_questions SET caption = NULL WHERE title = 'WelshSteel_08_Title';
+                END $$;
+            ";
+
+            migrationBuilder.Sql(sql);
+        }
+    }
+}
