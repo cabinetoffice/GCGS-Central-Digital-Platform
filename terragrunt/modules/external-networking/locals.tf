d9d08cb0c0f74b68e4defd0c79f49b1023759fd7
Ali Bahman <abn@webit4.me>
2025-12-24 15:17:29 +0000

    BAU commercial-ai include orchestrator

--- terragrunt/modules/external-networking/locals.tf
+++ terragrunt/modules/external-networking/locals.tf
@@ -5,9 +5,10 @@ locals {
   commercial_ai_cloudfront_domain = "d1kry5jfpfm6f7.cloudfront.net"
 
   commercial_ai_cert_validator_records = {
-    development = null
-    staging     = null
-    integration = null
+    oechestrator = null
+    development  = null
+    staging      = null
+    integration  = null
     production = {
       name    = "_7fe1a72107ce559d9bf4cd43164da019"
       records = ["_1616d9f5d97e70042aafc1b4ecb98fab.xlfgrmvvlj.acm-validations.aws."]
