403aefc836a3d22bf38605948aaeae47dfb48c12
Ali Bahman <abn@webit4.me>
2026-01-15 15:40:26 +0000

    Commercial-AI: Add certificate's verifications for Commercial AI's development account (#2088)
    - Add CName to point to the loadbalancer (no CloudFront yet)

--- terragrunt/modules/external-networking/commercial-ai-ns-records.tf
+++ terragrunt/modules/external-networking/commercial-ai-ns-records.tf
@@ -1,8 +1,18 @@
 resource "aws_route53_record" "commercial_ai_cert" {
-  count = local.commercial_ai_cert_validator_records[var.environment] != null ? 1 : 0
+  count = local.commercial_ai_records[var.environment] != null ? 1 : 0
 
-  name    = local.commercial_ai_cert_validator_records[var.environment].name
-  records = local.commercial_ai_cert_validator_records[var.environment].records
+  name    = local.commercial_ai_records[var.environment].validator_name
+  records = local.commercial_ai_records[var.environment].validator_records
+  ttl     = 60
+  type    = "CNAME"
+  zone_id = var.core_hosted_zone_id
+}
+
+resource "aws_route53_record" "commercial_ai_cname" {
+  count = length(try(local.commercial_ai_records[var.environment].target_records, [])) > 0 ? 1 : 0
+
+  name    = "ai"
+  records = local.commercial_ai_records[var.environment].target_records
   ttl     = 60
   type    = "CNAME"
   zone_id = var.core_hosted_zone_id
