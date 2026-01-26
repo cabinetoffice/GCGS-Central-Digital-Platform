3bfffb6be6cffca57ce3bc020e6927b9585f2538
Ali Bahman <abn@webit4.me>
2026-01-26 20:36:24 +0000

    FM-167 Correct OS outputs (#2109)

--- terragrunt/modules/opensearch/outputs.tf
+++ terragrunt/modules/opensearch/outputs.tf
@@ -1,19 +1,19 @@
 output "dashboard_endpoint" {
-  value       = aws_opensearch_domain.this[0].dashboard_endpoint
+  value       = length(aws_opensearch_domain.this) > 0 ? aws_opensearch_domain.this[0].dashboard_endpoint : null
   description = "Dashboards endpoint (VPC reachable)."
 }
 
 output "domain_arn" {
-  value       = aws_opensearch_domain.this[0].arn
+  value       = length(aws_opensearch_domain.this) > 0 ? aws_opensearch_domain.this[0].arn : null
   description = "OpenSearch domain ARN."
 }
 
 output "domain_name" {
-  value       = aws_opensearch_domain.this[0].domain_name
+  value       = length(aws_opensearch_domain.this) > 0 ? aws_opensearch_domain.this[0].domain_name : null
   description = "OpenSearch domain name."
 }
 
 output "endpoint" {
-  value       = aws_opensearch_domain.this[0].endpoint
+  value       = length(aws_opensearch_domain.this) > 0 ? aws_opensearch_domain.this[0].endpoint : null
   description = "Domain endpoint (VPC reachable)."
 }
