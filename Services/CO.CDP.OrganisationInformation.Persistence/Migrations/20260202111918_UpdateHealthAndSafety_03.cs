ed62c20c55dd9158887ebc31bf9d062bf465f9c7
Mark Gorman <markgorman1998@gmail.com>
2026-02-02 16:07:41 +0000

    Update Health and Safety module with word limit hints

---
+++ Services/CO.CDP.OrganisationInformation.Persistence/Migrations/20260202111918_UpdateHealthAndSafety_03.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CO.CDP.OrganisationInformation.Persistence.Migrations
+{
+    public partial class UpdateHealthAndSafety_03 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                UPDATE form_questions
+                SET options = '{""layout"": {""button"": {""beforeButtonContent"": ""HealthAndSafetyQuestion_03_CustomInsetText""}}}'::jsonb
+                WHERE title = 'HealthAndSafetyQuestion_03_Title';
+            ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                UPDATE form_questions
+                SET options = '{}'::jsonb
+                WHERE title = 'HealthAndSafetyQuestion_03_Title';
+            ");
+        }
+    }
+}
