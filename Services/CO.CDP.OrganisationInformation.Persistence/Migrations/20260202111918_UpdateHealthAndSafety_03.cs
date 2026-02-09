f22a4597d248310a962cdc8eb2b8e2276b839cf3
markgorman98 <markgorman1998@gmail.com>
2026-02-09 13:21:19 +0000

    CDPDEL-1806: Update Health and Safety module with word limit hints (#2125)
    * Update Health and Safety module with word limit hints
    
    * Update layout of hint text for HealthAndSafetyQuestion_02_CustomInsetText and HealthAndSafetyQuestion_03_Description

---
+++ Services/CO.CDP.OrganisationInformation.Persistence/Migrations/20260202111918_UpdateHealthAndSafety_03.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CO.CDP.OrganisationInformation.Persistence.Migrations
+{
+    public partial class UpdateHealthAndSafety_03 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.Sql(@"
+                UPDATE form_questions
+                SET options = '{""layout"":{""button"":{""beforeButtonContent"":""HealthAndSafetyQuestion_02_CustomInsetText""}}}'::jsonb
+                WHERE title = 'HealthAndSafetyQuestion_02_Title';
+            ");
+
+
+            migrationBuilder.Sql(@"
+                UPDATE form_questions
+                SET description = 'HealthAndSafetyQuestion_03_Description'
+                WHERE title = 'HealthAndSafetyQuestion_03_Title';
+            ");
+
+            // Q03: Remove previously-set beforeButtonContent
+            migrationBuilder.Sql(@"
+                UPDATE form_questions
+                SET options = COALESCE(options, '{}'::jsonb) #- '{layout,button,beforeButtonContent}'
+                WHERE title = 'HealthAndSafetyQuestion_03_Title';
+            ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                UPDATE form_questions
+                SET description = NULL
+                WHERE title = 'HealthAndSafetyQuestion_03_Title';
+            ");
+
+
+            migrationBuilder.Sql(@"
+                UPDATE form_questions
+                SET options = COALESCE(options, '{}'::jsonb) #- '{layout,button,beforeButtonContent}'
+                WHERE title IN ('HealthAndSafetyQuestion_02_Title', 'HealthAndSafetyQuestion_03_Title');
+            ");
+        }
+    }
+}
