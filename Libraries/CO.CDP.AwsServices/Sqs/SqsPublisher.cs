6308c32a13cdd7ae0656639876e1954cff26c2c1
Edward Dennis <98972365+EdwardDennis@users.noreply.github.com>
2026-02-20 12:13:51 +0000

    Refactor SqsPublisher to simplify QueueUrl and MessageGroupId methods; add unit test for OutboxMessage publishing

--- Libraries/CO.CDP.AwsServices/Sqs/SqsPublisher.cs
+++ Libraries/CO.CDP.AwsServices/Sqs/SqsPublisher.cs
@@ -68,23 +68,13 @@ public class SqsPublisher(
     {
     }
 
-    private string QueueUrlFor<TM>(TM message) where TM : notnull
+    private string QueueUrlFor<TM>(TM _) where TM : notnull
     {
-        if (message is OutboxMessage outboxMessage)
-        {
-            return outboxMessage.QueueUrl;
-        }
-
         return configuration.QueueUrl;
     }
 
-    private string? MessageGroupIdFor<TM>(TM message) where TM : notnull
+    private string? MessageGroupIdFor<TM>(TM _) where TM : notnull
     {
-        if (message is OutboxMessage outboxMessage)
-        {
-            return outboxMessage.MessageGroupId;
-        }
-
         return configuration.MessageGroupId;
     }
 }
