5adaada7f614807a3dd41519ef120dcc13732d39
Ali Bahman <abn@webit4.me>
2026-03-06 16:19:08 +0000

    DP-1483 Add traffic dashboards

--- terragrunt/tools/grafana/configs/entrypoint.sh
+++ terragrunt/tools/grafana/configs/entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 sed -i "s/GRAFANA_CLOUDWATCH_ACCOUNT_ID/${GRAFANA_CLOUDWATCH_ACCOUNT_ID}/g" /etc/grafana/provisioning/dashboards/application/*
 sed -i "s/GRAFANA_CLOUDWATCH_ACCOUNT_ID/${GRAFANA_CLOUDWATCH_ACCOUNT_ID}/g" /etc/grafana/provisioning/dashboards/infrastructure/*
 sed -i "s/GRAFANA_CLOUDWATCH_ACCOUNT_ID/${GRAFANA_CLOUDWATCH_ACCOUNT_ID}/g" /etc/grafana/provisioning/dashboards/overview/*
+sed -i "s/GRAFANA_CLOUDWATCH_ACCOUNT_ID/${GRAFANA_CLOUDWATCH_ACCOUNT_ID}/g" /etc/grafana/provisioning/dashboards/traffic/*
 sed -i "s/CDP_SIRSI_ENVIRONMENT/${CDP_SIRSI_ENVIRONMENT}/g" /etc/grafana/provisioning/dashboards/application/*
 sed -i "s/CDP_SIRSI_ENVIRONMENT/${CDP_SIRSI_ENVIRONMENT}/g" /etc/grafana/provisioning/dashboards/infrastructure/*
 sed -i "s/CDP_SIRSI_ENVIRONMENT/${CDP_SIRSI_ENVIRONMENT}/g" /etc/grafana/provisioning/dashboards/overview/*
