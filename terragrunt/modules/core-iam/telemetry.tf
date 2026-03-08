394d3708f49a6781b0f98b03013afdc0b3e42878
Ali Bahman <abn@webit4.me>
2026-03-08 08:57:11 +0000

    DP-2570 Add WAF triage dashboard

--- terragrunt/modules/core-iam/telemetry.tf
+++ terragrunt/modules/core-iam/telemetry.tf
@@ -9,3 +9,19 @@ resource "aws_iam_role_policy_attachment" "grafana_role_cloudwatch_read_only_acc
   policy_arn = data.aws_iam_policy.cloudwatch_read_only_access.arn
   role       = aws_iam_role.telemetry.id
 }
+
+resource "aws_iam_policy" "grafana_generic" {
+  name   = "${local.name_prefix}-grafana-generic"
+  policy = data.aws_iam_policy_document.grafana_generic.json
+  tags   = var.tags
+}
+
+resource "aws_iam_role_policy_attachment" "grafana_generic" {
+  policy_arn = aws_iam_policy.grafana_generic.arn
+  role       = aws_iam_role.telemetry.name
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_grafana_generic" {
+  policy_arn = aws_iam_policy.grafana_generic.arn
+  role       = aws_iam_role.ecs_task.name
+}
