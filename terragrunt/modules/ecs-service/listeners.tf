968bcc12faebe81575153af77fe7455ca27ed3e5
Ali Bahman <abn@webit4.me>
2026-03-22 17:50:36 +0000

    DP-0000 BAU Address terraform provider issue, nested forward on cognito rules

--- terragrunt/modules/ecs-service/listeners.tf
+++ terragrunt/modules/ecs-service/listeners.tf
@@ -91,31 +91,30 @@ resource "aws_lb_listener_rule" "external" {
   priority     = local.service_listener_rule_priority
 
   dynamic "action" {
-    for_each = local.external_listener_actions
+    for_each = var.user_pool_arn != null && var.user_pool_client_id != null && var.user_pool_domain != null ? [1] : []
 
     content {
-      type  = action.value.type
-      order = action.value.order
+      type  = "authenticate-cognito"
+      order = 1
 
-      dynamic "authenticate_cognito" {
-        for_each = action.value.type == "authenticate-cognito" ? [1] : []
-        content {
-          user_pool_arn              = var.user_pool_arn
-          user_pool_client_id        = var.user_pool_client_id
-          user_pool_domain           = var.user_pool_domain
-          session_cookie_name        = "AWSELBAuthSessionCookie"
-          scope                      = "openid"
-          on_unauthenticated_request = "authenticate"
-        }
+      authenticate_cognito {
+        user_pool_arn              = var.user_pool_arn
+        user_pool_client_id        = var.user_pool_client_id
+        user_pool_domain           = var.user_pool_domain
+        session_cookie_name        = "AWSELBAuthSessionCookie"
+        scope                      = "openid"
+        on_unauthenticated_request = "authenticate"
       }
+    }
+  }
 
-      dynamic "forward" {
-        for_each = action.value.type == "forward" ? [1] : []
-        content {
-          target_group {
-            arn = aws_lb_target_group.external[0].arn
-          }
-        }
+  action {
+    type  = "forward"
+    order = var.user_pool_arn != null && var.user_pool_client_id != null && var.user_pool_domain != null ? 2 : 1
+
+    forward {
+      target_group {
+        arn = aws_lb_target_group.external[0].arn
       }
     }
   }
