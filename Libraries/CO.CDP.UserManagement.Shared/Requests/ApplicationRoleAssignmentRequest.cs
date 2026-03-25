d9c858fb8e398888b3368680554f116fe0e84616
Edward Dennis <98972365+EdwardDennis@users.noreply.github.com>
2026-03-25 17:29:41 +0000

    Create new shared request types

---
+++ Libraries/CO.CDP.UserManagement.Shared/Requests/ApplicationRoleAssignmentRequest.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace CO.CDP.UserManagement.Shared.Requests;
+
+public sealed record ApplicationRoleAssignmentRequest(
+    int OrganisationApplicationId,
+    int ApplicationId,
+    bool GiveAccess,
+    int? SelectedRoleId,
+    IReadOnlyList<int>? SelectedRoleIds
+);
