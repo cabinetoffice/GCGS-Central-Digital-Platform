7badbff67541530ce7b2fc3cffcf2eff85623d5a
Ali Bahman <abn@webit4.me>
2026-04-09 09:08:33 +0100

    DP-2643 Add prod payments DNS aliases and ACM validation records

--- terragrunt/modules/external-networking/payments-ns-records.tf
+++ terragrunt/modules/external-networking/payments-ns-records.tf
@@ -1,23 +1,23 @@
 resource "aws_route53_record" "payments_cert" {
-  count = local.payments_records[var.environment] != null ? 1 : 0
+  for_each = local.payments_records[var.environment] != null ? local.payments_records[var.environment] : {}
 
-  name    = local.payments_records[var.environment].validator_name
-  records = local.payments_records[var.environment].validator_records
+  name    = each.value.validator_name
+  records = each.value.validator_records
   ttl     = 60
   type    = "CNAME"
   zone_id = var.core_hosted_zone_id
 }
 
 resource "aws_route53_record" "payments_alias" {
-  count = length(try(local.payments_records[var.environment].records, [])) > 0 ? 1 : 0
+  for_each = local.payments_records[var.environment] != null ? local.payments_records[var.environment] : {}
 
-  name    = local.payments_records[var.environment].name
+  name    = each.value.name
   type    = "A"
   zone_id = var.core_hosted_zone_id
 
   alias {
     evaluate_target_health = false
-    name                   = local.payments_records[var.environment].records[0]
-    zone_id                = local.payments_records[var.environment].lb_zone_id
+    name                   = each.value.records[0]
+    zone_id                = each.value.lb_zone_id
   }
 }
