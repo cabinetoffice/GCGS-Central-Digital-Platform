9d5858a4dcc48f4d549abd2cb98b1174f7d97a50
Ali Bahman <abn@webit4.me>
2026-04-18 14:25:27 +0100

    DP-2611 Grant terraform permission over us-east-1 resources (#2355)
    - Configure log bucket with no versioning and 90 days expiry
    - Split policies to keep in line with AWS policy per role limit

--- terragrunt/modules/core-iam/terraform-global-datasource.tf
+++ terragrunt/modules/core-iam/terraform-global-datasource.tf
@@ -93,60 +93,6 @@ data "aws_iam_policy_document" "terraform_global" {
 
   statement {
     actions = [
-      "cloudfront:CreateDistribution",
-      "cloudfront:CreateRealtimeLogConfig",
-      "cloudfront:CreateOriginAccessControl",
-      "cloudfront:CreateResponseHeadersPolicy",
-      "cloudfront:DeleteDistribution",
-      "cloudfront:DeleteRealtimeLogConfig",
-      "cloudfront:DeleteOriginAccessControl",
-      "cloudfront:DeleteResponseHeadersPolicy",
-      "cloudfront:GetDistribution",
-      "cloudfront:GetDistributionConfig",
-      "cloudfront:GetOriginAccessControl",
-      "cloudfront:GetRealtimeLogConfig",
-      "cloudfront:GetResponseHeadersPolicy",
-      "cloudfront:GetResponseHeadersPolicyConfig",
-      "cloudfront:ListDistributions",
-      "cloudfront:ListOriginAccessControls",
-      "cloudfront:ListRealtimeLogConfigs",
-      "cloudfront:ListResponseHeadersPolicies",
-      "cloudfront:ListTagsForResource",
-      "cloudfront:TagResource",
-      "cloudfront:UntagResource",
-      "cloudfront:UpdateDistribution",
-      "cloudfront:UpdateOriginAccessControl",
-      "cloudfront:UpdateRealtimeLogConfig",
-      "cloudfront:UpdateResponseHeadersPolicy",
-    ]
-    effect = "Allow"
-    resources = [
-      "*"
-    ]
-    sid = "ManageCloudfront"
-  }
-
-  statement {
-    actions = [
-      "kinesis:AddTagsToStream",
-      "kinesis:CreateStream",
-      "kinesis:DeleteStream",
-      "kinesis:DescribeStreamSummary",
-      "kinesis:IncreaseStreamRetentionPeriod",
-      "kinesis:DescribeStream",
-      "kinesis:ListStreams",
-      "kinesis:ListTagsForStream",
-      "kinesis:RemoveTagsFromStream"
-    ]
-    effect = "Allow"
-    resources = [
-      "*"
-    ]
-    sid = "ManageKinesis"
-  }
-
-  statement {
-    actions = [
       "kms:*",
       "kms:Decrypt",
       "kms:ReEncrypt*",
@@ -334,47 +280,6 @@ data "aws_iam_policy_document" "terraform_global" {
       "arn:aws:states:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:stateMachine:*"
     ]
     sid = "ManageStateMachines"
-  }
-
-  statement {
-    actions = [
-      "wafv2:CreateWebACL",
-      "wafv2:GetWebACLForResource",
-      "wafv2:UpdateWebACL",
-    ]
-    effect = "Allow"
-    resources = [
-      "arn:aws:wafv2:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:regional/managedruleset/*/*",
-      "arn:aws:wafv2:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:regional/webacl/*/*",
-    ]
-    sid = "ManageWAF"
-  }
-
-  statement {
-    actions = [
-      "wafv2:DeleteLoggingConfiguration",
-      "wafv2:PutLoggingConfiguration",
-    ]
-    effect = "Allow"
-    resources = [
-      "*",
-    ]
-    sid = "ManageWAFLoggingConfigurationAPI"
-  }
-
-  statement {
-    actions = [
-      "logs:CreateLogDelivery",
-      "logs:DeleteLogDelivery",
-      "logs:PutResourcePolicy",
-      "logs:DescribeResourcePolicies",
-      "logs:DescribeLogGroups"
-    ]
-    effect = "Allow"
-    resources = [
-      "*",
-    ]
-    sid = "ManageWAFWebACLLoggingCWL"
   }
 
 }
