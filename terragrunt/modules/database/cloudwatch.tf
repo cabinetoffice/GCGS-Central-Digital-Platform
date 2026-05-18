6fddc510e7efe63e51c149a6ad5aa16a1f9dba24
Ali Bahman <abn@webit4.me>
2026-05-18 21:20:12 +0100

    FC-1020 Remove import blocks

--- terragrunt/modules/database/cloudwatch.tf
+++ terragrunt/modules/database/cloudwatch.tf
@@ -1,23 +1,3 @@
-import {
-  to = aws_cloudwatch_log_group.fts_db["audit"]
-  id = "/aws/rds/cluster/${local.fts_cluster_name}/audit"
-}
-
-import {
-  to = aws_cloudwatch_log_group.fts_db["error"]
-  id = "/aws/rds/cluster/${local.fts_cluster_name}/error"
-}
-
-import {
-  to = aws_cloudwatch_log_group.fts_db["general"]
-  id = "/aws/rds/cluster/${local.fts_cluster_name}/general"
-}
-
-import {
-  to = aws_cloudwatch_log_group.fts_db["slowquery"]
-  id = "/aws/rds/cluster/${local.fts_cluster_name}/slowquery"
-}
-
 resource "aws_cloudwatch_log_group" "fts_db" {
   for_each = toset(local.fts_log_exports)
 
