979e3d90421b3219d8712e8cc94ed6ad09152c3c
Ali Bahman <abn@webit4.me>
2026-05-19 10:12:42 +0100

    FC-1038 Split FTS DB Parameter groups
    - Enabling extra logs in dev and prod only

--- terragrunt/modules/database/locals.tf
+++ terragrunt/modules/database/locals.tf
@@ -30,24 +30,30 @@ locals {
   fts_cluster_name = "${local.name_prefix}-fts-cluster"
   fts_log_exports  = contains(["development", "production"], var.environment) ? ["audit", "error", "general", "slowquery"] : []
 
-  fts_db_parameters_cluster = {
+  fts_db_parameters_cluster_base = {
     character_set_database          = "latin1"
     character_set_server            = "latin1"
     collation_server                = "latin1_swedish_ci"
     explicit_defaults_for_timestamp = 0
-    general_log                     = 1
     group_concat_max_len            = 200000
     innodb_ft_enable_stopword       = "0"
     innodb_ft_min_token_size        = 1
     local_infile                    = 1
-    log_output                      = "FILE"
-    log_queries_not_using_indexes   = 1
-    long_query_time                 = 1
     max_allowed_packet              = "293601280"
-    slow_query_log                  = 1
     sql_mode                        = "NO_ENGINE_SUBSTITUTION"
     time_zone                       = "Europe/Dublin"
   }
+  fts_db_parameters_cluster_logging = {
+    general_log                   = 1
+    log_output                    = "FILE"
+    log_queries_not_using_indexes = 1
+    long_query_time               = 1
+    slow_query_log                = 1
+  }
+  fts_db_parameters_cluster = merge(
+    local.fts_db_parameters_cluster_base,
+    contains(["development", "production"], var.environment) ? local.fts_db_parameters_cluster_logging : {}
+  )
   fts_db_parameters_instance = {
     group_concat_max_len      = 200000
     innodb_ft_enable_stopword = "0"
