c35e6bab40061d7ec1988f88e963f9da96e309bf
Ali Bahman <abn@webit4.me>
2026-06-02 20:44:35 +0100

    FC-991 Enable Grafana's native sign-up

--- terragrunt/modules/telemetry/service-grafana.tf
+++ terragrunt/modules/telemetry/service-grafana.tf
@@ -43,8 +43,8 @@ module "ecs_service_grafana" {
   role_ecs_task_exec_arn = var.role_ecs_task_exec_arn
   service_port           = var.grafana_config.port
   tags                   = var.tags
-  # user_pool_arn          = var.is_production ? var.user_pool_arn_grafana : null
-  # user_pool_client_id    = var.is_production ? var.user_pool_client_id_grafana : null
-  # user_pool_domain       = var.is_production ? var.user_pool_domain_grafana : null
+  user_pool_arn          = null
+  user_pool_client_id    = null
+  user_pool_domain       = null
   vpc_id = var.vpc_id
 }
