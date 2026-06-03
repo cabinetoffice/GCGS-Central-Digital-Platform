1212406977533312221151b05210b2a6e2445886
Ali Bahman <abn@webit4.me>
2026-06-03 11:37:41 +0100

    FC-991 Adjust permission

--- terragrunt/tools/grafana/terraform/secrets.tf
+++ terragrunt/tools/grafana/terraform/secrets.tf
@@ -1,9 +1,5 @@
-data "aws_secretsmanager_secret" "grafana_alerting" {
-  name = "cdp-sirsi-grafana-alerting-webhook"
-}
-
 data "aws_secretsmanager_secret_version" "grafana_alerting" {
-  secret_id = data.aws_secretsmanager_secret.grafana_alerting.id
+  secret_id = "cdp-sirsi-grafana-alerting-webhook"
 }
 
 locals {
