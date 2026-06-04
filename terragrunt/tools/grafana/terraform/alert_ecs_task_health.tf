644e2d5aa4be44f23189cfb7debb7c279af33339
Ali Bahman <abn@webit4.me>
2026-06-04 18:14:05 +0100

    FC-991 Add more alerts around inactive tasks

--- terragrunt/tools/grafana/terraform/alert_ecs_task_health.tf
+++ terragrunt/tools/grafana/terraform/alert_ecs_task_health.tf
@@ -344,6 +344,121 @@ resource "grafana_rule_group" "ecs_task_health" {
     }
   }
 
+  rule {
+    name           = "ECS tasks stopping/restarting"
+    condition      = "C"
+    for            = "5m"
+    no_data_state  = "OK"
+    exec_err_state = "Error"
+    is_paused      = false
+
+    annotations = {}
+    labels = {
+      severity = "warning"
+    }
+
+    data {
+      ref_id         = "A"
+      datasource_uid = grafana_data_source.cloudwatch.uid
+      relative_time_range {
+        from = 300
+        to   = 0
+      }
+
+      model = jsonencode({
+        dimensions = {
+          ClusterName = "*"
+          ServiceName = "*"
+        }
+        expression        = ""
+        id                = ""
+        intervalMs        = 1000
+        label             = ""
+        logGroups         = []
+        matchExact        = true
+        maxDataPoints     = 43200
+        metricEditorMode  = 0
+        metricName        = "TaskStoppedCount"
+        metricQueryType   = 0
+        namespace         = "ECS/ContainerInsights"
+        period            = "1m"
+        queryLanguage     = "CWLI"
+        queryMode         = "Metrics"
+        refId             = "A"
+        region            = "default"
+        sqlExpression     = ""
+        statistic         = "Sum"
+      })
+    }
+
+    data {
+      ref_id         = "B"
+      datasource_uid = "__expr__"
+      relative_time_range {
+        from = 0
+        to   = 0
+      }
+
+      model = jsonencode({
+        datasource = {
+          type = "__expr__"
+          uid  = "__expr__"
+        }
+        expression    = "A"
+        intervalMs    = 1000
+        maxDataPoints = 43200
+        reducer       = "last"
+        refId         = "B"
+        type          = "reduce"
+      })
+    }
+
+    data {
+      ref_id         = "C"
+      datasource_uid = "__expr__"
+      relative_time_range {
+        from = 0
+        to   = 0
+      }
+
+      model = jsonencode({
+        conditions = [{
+          evaluator = {
+            params = [var.ecs_task_stopped_threshold]
+            type   = "gt"
+          }
+          operator = {
+            type = "and"
+          }
+          query = {
+            params = []
+          }
+          reducer = {
+            params = []
+            type   = "last"
+          }
+          type = "query"
+        }]
+        datasource = {
+          type = "__expr__"
+          uid  = "__expr__"
+        }
+        expression    = "B"
+        intervalMs    = 1000
+        maxDataPoints = 43200
+        refId         = "C"
+        type          = "threshold"
+      })
+    }
+
+    dynamic "notification_settings" {
+      for_each = local.teams_webhook_url != "" ? [1] : []
+      content {
+        contact_point = grafana_contact_point.teams[0].name
+      }
+    }
+  }
+
   depends_on = [
     grafana_data_source.cloudwatch,
     grafana_dashboard.dashboards,
--- terragrunt/tools/grafana/terraform/env/integration.tfvars
+++ terragrunt/tools/grafana/terraform/env/integration.tfvars
@@ -1,3 +1,10 @@
 environment = "integration"
 ecs_cpu_threshold = 50
 ecs_memory_threshold = 60
+ecs_task_stopped_threshold = 0
+rds_cpu_threshold = 80
+rds_free_storage_threshold_bytes = 5368709120
+rds_freeable_memory_threshold_bytes = 536870912
+rds_connections_threshold = 200
+rds_read_latency_threshold_seconds = 0.2
+rds_write_latency_threshold_seconds = 0.2
