3968201c4400912844a4ed806ba9fac9491f996d
Marek Matulka <mareg@users.noreply.github.com>
2026-06-05 12:58:55 +0100

    FC-1292: Add feature flag for notice render cache feature

--- terragrunt/modules/ecs/locals-fts.tf
+++ terragrunt/modules/ecs/locals-fts.tf
@@ -85,6 +85,7 @@ locals {
     notice_render_cache_bucket            = module.s3_bucket_fts_notice_render_cache.bucket
     notice_render_cache_cdn_url           = var.cloudfront_downloads_enabled ? "https://${module.cloudfront_fts_notice_render_cache.cloudfront_domain_name}" : ""
     notice_render_cache_debug_marker      = contains(["development", "staging"], var.environment)
+    notice_render_cache_enabled           = contains(["development"], var.environment)
     session_name_default                  = "SRSI_FT_AUTH"
     site_domain                           = local.fts_site_domains[var.environment]
     site_tag                              = "TEST"
