b32a40911f062ba389649c6d008dbfc01cafc07a
Paul Bunkham <paul@dobit.co.uk>
2026-06-17 20:30:14 +0100

    Revert "Set SAVED_SEARCH_IGNORE_TIME_WINDOW in Dev and Staging (#2540)"
    This reverts commit ca08a914295296f6610480a520ad70e832e8d6bf.

--- terragrunt/modules/ecs/locals-fts.tf
+++ terragrunt/modules/ecs/locals-fts.tf
@@ -80,7 +80,6 @@ locals {
     licenced_to                           = "No-one"
     local_version                         = 1100
     modernised_landing_page               = contains(["development", "staging"], var.environment)
-    saved_search_ignore_time_window       = contains(["development", "staging"], var.environment)
     summarised_search_enabled             = contains(["development", "staging"], var.environment)
     pa23_enabled                          = contains(["development"], var.environment)
     notice_publish_internal_key           = local.fts_notice_publish_internal_key_arn
