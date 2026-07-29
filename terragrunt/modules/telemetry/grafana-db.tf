60e2b3ba2cf465f6cd36382dab30250cce16ca06
Ali Bahman <abn@webit4.me>
2026-07-29 08:16:27 +0100

    FC-1870 Match the code with latest Grafana DB minor update

--- terragrunt/modules/telemetry/grafana-db.tf
+++ terragrunt/modules/telemetry/grafana-db.tf
@@ -4,7 +4,7 @@ module "grafana_db" {
   db_name              = "${local.name_prefix}-grafana"
   db_sg_id             = var.db_postgres_sg_id
   engine               = "postgres"
-  engine_version       = "16.6"
+  engine_version       = "16.13"
   family               = "postgres16"
   instance_type        = local.grafana_db_instance_type
   multi_az             = local.grafana_db_multi_az
