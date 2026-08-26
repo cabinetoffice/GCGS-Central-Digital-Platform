ea1b68634a6c782857f0b940fe229943c0016518
JBaigGoaco <jawwad.baig@goaco.com>
2026-08-26 19:17:34 +0100

    Update `Nationalities` list and improve related tests
    The `Country` class `Nationalities` array was updated to include new entries, correct existing ones, and ensure consistency in naming conventions. The array was reformatted for better readability.
    
    The `CountryTests` class was updated to include additional assertions for the `Nationalities` property, ensuring the list is in ascending order and contains only unique items. The `expectedNationalities` list in the tests was updated to reflect the changes in the `Country` class.
    
    Unused `using` directives were removed from `CountryTests.cs`. These changes improve the correctness, readability, and maintainability of the code.

--- Frontend/CO.CDP.OrganisationApp/Constants/Country.cs
+++ Frontend/CO.CDP.OrganisationApp/Constants/Country.cs
@@ -5,7 +5,41 @@ public static class Country
     public const string UnitedKingdom = "United Kingdom";
     public const string UKCountryCode = "GB";
 
-    public static readonly string[] Nationalities = ["Afghan", "Albanian", "Algerian", "American", "Andorran", "Angolan", "Antiguans", "Argentinean", "Armenian", "Australian", "Austrian", "Azerbaijani", "Bahamian", "Bahraini", "Bangladeshi", "Barbadian", "Barbudans", "Batswana", "Belarusian", "Belgian", "Belizean", "Beninese", "Bhutanese", "Bolivian", "Bosnian", "Brazilian", "British", "Bruneian", "Bulgarian", "Burkinabe", "Burmese", "Burundian", "Cambodian", "Cameroonian", "Canadian", "Cape Verdean", "Central African", "Chadian", "Chilean", "Chinese", "Colombian", "Comoran", "Congolese", "Costa Rican", "Croatian", "Cuban", "Cypriot", "Czech", "Danish", "Djibouti", "Dominican", "Dutch", "East Timorese", "Ecuadorean", "Egyptian", "Emirian", "Equatorial Guinean", "Eritrean", "Estonian", "Ethiopian", "Fijian", "Filipino", "Finnish", "French", "Gabonese", "Gambian", "Georgian", "German", "Ghanaian", "Greek", "Grenadian", "Guatemalan", "Guinea-Bissauan", "Guinean", "Guyanese", "Haitian", "Herzegovinian", "Honduran", "Hungarian", "I-Kiribati", "Icelander", "Indian", "Indonesian", "Iranian", "Iraqi", "Irish", "Israeli", "Italian", "Ivorian", "Jamaican", "Japanese", "Jordanian", "Kazakhstani", "Kenyan", "Kittian and Nevisian", "Kuwaiti", "Kyrgyz", "Laotian", "Latvian", "Lebanese", "Liberian", "Libyan", "Liechtensteiner", "Lithuanian", "Luxembourger", "Macedonian", "Malagasy", "Malawian", "Malaysian", "Maldivan", "Malian", "Maltese", "Marshallese", "Mauritanian", "Mauritian", "Mexican", "Micronesian", "Moldovan", "Monacan", "Mongolian", "Moroccan", "Mosotho", "Motswana", "Mozambican", "Namibian", "Nauruan", "Nepalese", "New Zealander", "Nicaraguan", "Nigerian", "Nigerien", "North Korean", "Northern Irish", "Norwegian", "Omani", "Pakistani", "Palauan", "Panamanian", "Papua New Guinean", "Paraguayan", "Peruvian", "Polish", "Portuguese", "Qatari", "Romanian", "Russian", "Rwandan", "Saint Lucian", "Salvadoran", "Samoan", "San Marinese", "Sao Tomean", "Saudi", "Scottish", "Senegalese", "Serbian", "Seychellois", "Sierra Leonean", "Singaporean", "Slovakian", "Slovenian", "Solomon Islander", "Somali", "South African", "South Korean", "Spanish", "Sri Lankan", "Sudanese", "Surinamer", "Swazi", "Swedish", "Swiss", "Syrian", "Taiwanese", "Tajik", "Tanzanian", "Thai", "Togolese", "Tongan", "Trinidadian or Tobagonian", "Tunisian", "Turkish", "Tuvaluan", "Ugandan", "Ukrainian", "Uruguayan", "Uzbekistani", "Venezuelan", "Vietnamese", "Welsh", "Yemenite", "Zambian", "Zimbabwean"];
+    public static readonly string[] Nationalities =
+    [
+        "Afghan", "Albanian", "Algerian", "American", "Andorran", "Angolan", "Anguillan", "Antiguans",
+        "Argentine", "Argentinean", "Armenian", "Australian", "Austrian", "Azerbaijani", "Bahamian", "Bahraini",
+        "Bangladeshi", "Barbadian", "Barbudans", "Batswana", "Belarusian", "Belgian", "Belizean", "Beninese",
+        "Bermudian", "Bhutanese", "Bolivian", "Bosnian", "Botswanan", "Brazilian", "British", "British Virgin Islander",
+        "Bruneian", "Bulgarian", "Burkinabe", "Burkinan", "Burmese", "Burundian", "Cambodian", "Cameroonian",
+        "Canadian", "Cape Verdean", "Cayman Islander", "Central African", "Chadian", "Chilean", "Chinese", "Citizen of Antigua and Barbuda",
+        "Citizen of Bosnia and Herzegovina", "Citizen of Guinea-Bissau", "Citizen of Kiribati", "Citizen of Seychelles", "Citizen of Vanuatu", "Citizen of the Dominican Republic", "Colombian", "Comoran",
+        "Congolese", "Congolese (Congo)", "Congolese (DRC)", "Cook Islander", "Costa Rican", "Croatian", "Cuban", "Cymraes",
+        "Cymro", "Cypriot", "Czech", "Danish", "Djibouti", "Djiboutian", "Dominican", "Dutch",
+        "East Timorese", "Ecuadorean", "Egyptian", "Emirati", "Emirian", "English", "Equatorial Guinean", "Eritrean",
+        "Estonian", "Ethiopian", "Faroese", "Fijian", "Filipino", "Finnish", "French", "Gabonese",
+        "Gambian", "Georgian", "German", "Ghanaian", "Gibraltarian", "Greek", "Greenlandic", "Grenadian",
+        "Guamanian", "Guatemalan", "Guinea-Bissauan", "Guinean", "Guyanese", "Haitian", "Herzegovinian", "Honduran",
+        "Hong Konger", "Hungarian", "I-Kiribati", "Icelander", "Icelandic", "Indian", "Indonesian", "Iranian",
+        "Iraqi", "Irish", "Israeli", "Italian", "Ivorian", "Jamaican", "Japanese", "Jordanian",
+        "Kazakh", "Kazakhstani", "Kenyan", "Kittian and Nevisian", "Kittitian", "Kosovan", "Kuwaiti", "Kyrgyz",
+        "Lao", "Laotian", "Latvian", "Lebanese", "Liberian", "Libyan", "Liechtenstein citizen", "Liechtensteiner",
+        "Lithuanian", "Luxembourger", "Macanese", "Macedonian", "Malagasy", "Malawian", "Malaysian", "Maldivan",
+        "Maldivian", "Malian", "Maltese", "Marshallese", "Martiniquais", "Mauritanian", "Mauritian", "Mexican",
+        "Micronesian", "Moldovan", "Monacan", "Monegasque", "Mongolian", "Montenegrin", "Montserratian", "Moroccan",
+        "Mosotho", "Motswana", "Mozambican", "Namibian", "Nauruan", "Nepalese", "New Zealander", "Nicaraguan",
+        "Nigerian", "Nigerien", "Niuean", "North Korean", "Northern Irish", "Norwegian", "Omani", "Pakistani",
+        "Palauan", "Palestinian", "Panamanian", "Papua New Guinean", "Paraguayan", "Peruvian", "Pitcairn Islander", "Polish",
+        "Portuguese", "Prydeinig", "Puerto Rican", "Qatari", "Romanian", "Russian", "Rwandan", "Saint Lucian",
+        "Salvadoran", "Salvadorean", "Sammarinese", "Samoan", "San Marinese", "Sao Tomean", "Saudi", "Saudi Arabian",
+        "Scottish", "Senegalese", "Serbian", "Seychellois", "Sierra Leonean", "Singaporean", "Slovak", "Slovakian",
+        "Slovenian", "Solomon Islander", "Somali", "South African", "South Korean", "South Sudanese", "Spanish", "Sri Lankan",
+        "St Helenian", "St Lucian", "Stateless", "Sudanese", "Surinamer", "Surinamese", "Swazi", "Swedish",
+        "Swiss", "Syrian", "Taiwanese", "Tajik", "Tanzanian", "Thai", "Togolese", "Tongan",
+        "Trinidadian", "Trinidadian or Tobagonian", "Tristanian", "Tunisian", "Turkish", "Turkmen", "Turks and Caicos Islander", "Tuvaluan",
+        "Ugandan", "Ukrainian", "Uruguayan", "Uzbek", "Uzbekistani", "Vatican citizen", "Venezuelan", "Vietnamese",
+        "Vincentian", "Wallisian", "Welsh", "Yemeni", "Yemenite", "Zambian", "Zimbabwean"
+    ];
 
     public static Dictionary<string, string> NonUKCountries =
         new()
