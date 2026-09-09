b103045ab638576c2b3daa17a0208f41b25d44fa
Ali Bahman <abn@webit4.me>
2026-09-09 19:33:34 +0100

    FC-2137 More env-vars, many to be removed once app is refactored

--- terragrunt/modules/ecs/task-e2e-nightly-dev.tf
+++ terragrunt/modules/ecs/task-e2e-nightly-dev.tf
@@ -11,12 +11,17 @@ module "ecs_task_e2e_nightly_dev" {
       e2e_env_secret_arn         = data.aws_secretsmanager_secret.e2e_nightly_dev_env[0].arn
       fts_secrets_arn            = data.aws_secretsmanager_secret.fts_secrets.arn
       image                      = local.ecr_urls[var.service_configs.e2e_nightly_dev.name]
+      kill_after_seconds         = "30"
       lg_name                    = aws_cloudwatch_log_group.tasks[var.service_configs.e2e_nightly_dev.name].name
       lg_prefix                  = "app"
       lg_region                  = data.aws_region.current.region
       memory                     = var.service_configs.e2e_nightly_dev.memory
       name                       = var.service_configs.e2e_nightly_dev.name
+      public_domain              = var.public_domain
+      run_once                   = "true"
       service_version            = local.service_version_fts
+      test_settings_headed       = "0"
+      test_timeout_seconds       = "5400"
       teams_notification_enabled = "true"
     }
   )
