f5f0ce651784c8ca6ab35bccade9d7b0dd599ea1
Ali Bahman <abn@webit4.me>
2026-09-15 11:54:52 +0100

    FC-2219 Add report's bucket for E2E nightly task

--- terragrunt/modules/ecs/s3.tf
+++ terragrunt/modules/ecs/s3.tf
@@ -53,15 +53,15 @@ module "s3_bucket_fts" {
 }
 
 module "s3_bucket_fts_notice_render_cache" {
-  source             = "../s3-bucket"
-  bucket_name        = "${local.name_prefix}-${var.environment}-fts-notice-render-cache-${data.aws_caller_identity.current.account_id}"
+  source                 = "../s3-bucket"
+  bucket_name            = "${local.name_prefix}-${var.environment}-fts-notice-render-cache-${data.aws_caller_identity.current.account_id}"
   cloudfront_read_access = true
-  create_kms_key     = false
-  enable_encryption  = true
-  kms_key_admin_role = var.role_terraform_arn
-  read_roles         = [var.role_ecs_task_exec_arn]
-  sse_algorithm      = "AES256"
-  write_roles        = [var.role_ecs_task_arn]
+  create_kms_key         = false
+  enable_encryption      = true
+  kms_key_admin_role     = var.role_terraform_arn
+  read_roles             = [var.role_ecs_task_exec_arn]
+  sse_algorithm          = "AES256"
+  write_roles            = [var.role_ecs_task_arn]
 
   tags = var.tags
 }
@@ -73,6 +73,18 @@ module "s3_bucket_ocds_exports" {
   enable_encryption  = false
   kms_key_admin_role = var.role_terraform_arn
   read_roles         = [var.role_ecs_task_exec_arn]
+  write_roles        = [var.role_ecs_task_arn]
+
+  tags = var.tags
+}
+
+module "s3_bucket_e2e_nightly_dev_reports" {
+  source = "../s3-bucket"
+  count  = local.e2e_nightly_dev_enabled ? 1 : 0
+
+  bucket_name        = "${local.name_prefix}-${var.environment}-e2e-nightly-dev-reports-${data.aws_caller_identity.current.account_id}"
+  kms_key_admin_role = var.role_terraform_arn
+  read_roles         = [var.role_terraform_arn]
   write_roles        = [var.role_ecs_task_arn]
 
   tags = var.tags
